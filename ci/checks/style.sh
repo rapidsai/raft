db38255a2e1928bd2e062c84cee39a42ab23beb8
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-05-05 02:51:03 -0700

    CI updated style.sh with the latest checks

--- ci/checks/style.sh
+++ ci/checks/style.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (c) 2020, NVIDIA CORPORATION.
 #####################
-# cuML Style Tester #
+# RAFT Style Tester #
 #####################
 
 # Ignore errors and set path
@@ -41,15 +41,9 @@ else
 fi
 
 # Check for a consistent #include syntax
-# TODO: keep adding more dirs as and when we update the syntax
 HASH_INCLUDE=`python cpp/scripts/include_checker.py \
-                     cpp/bench \
-                     cpp/comms/mpi/include \
-                     cpp/comms/mpi/src \
-                     cpp/comms/std/include \
-                     cpp/comms/std/src \
                      cpp/include \
-                     cpp/examples \
+                     cpp/test \
                      2>&1`
 HASH_RETVAL=$?
 if [ "$RETVAL" = "0" ]; then
@@ -66,7 +60,6 @@ else
 fi
 
 # Check for a consistent code format
-# TODO: keep adding more dirs when we add more source folders in cuml
 FORMAT=`python cpp/scripts/run-clang-format.py 2>&1`
 FORMAT_RETVAL=$?
 if [ "$RETVAL" = "0" ]; then
