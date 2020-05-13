43f0d69f5275dd81d5f1794dcb5917133e1770cc
Corey J. Nolet <cjnolet@gmail.com>
2020-05-13 15:20:56 -0400

    Adding copyright headers

--- python/raft/dask/common/__init__.py
+++ python/raft/dask/common/__init__.py
@@ -1,8 +1,22 @@
-
+# Copyright (c) 2020, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from raft.dask.common.comms import CommsContext, worker_state
 
-from raft.dask.common.comms_utils import inject_comms_on_handle, \
-    perform_test_comms_allreduce, perform_test_comms_send_recv, \
-    inject_comms_on_handle_coll_only
+from raft.dask.common.comms_utils import inject_comms_on_handle
+from raft.dask.common.comms_utils import inject_comms_on_handle_coll_only
+from raft.dask.common.comms_utils import perform_test_comms_allreduce
+from raft.dask.common.comms_utils import perform_test_comms_send_recv
 
