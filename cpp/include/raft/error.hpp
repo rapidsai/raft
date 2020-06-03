a92846874481c947f345b2073b3dc7a58927788d
Seunghwa Kang <seunghwak@nvidia.com>
2020-06-03 11:06:20 -0400

    copy error.hpp from cuDF, add license statement, and initial update

---
+++ cpp/include/raft/error.hpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <stdexcept>
+#include <string>
+
+
+namespace raft {
+
+/**
+ * @brief Exception thrown when logical precondition is violated.
+ *
+ * This exception should not be thrown directly and is instead thrown by the
+ * RAFT_EXPECTS macro.
+ *
+ */
+struct logic_error : public std::logic_error {
+  logic_error(char const* const message) : std::logic_error(message) {}
+
+  logic_error(std::string const& message) : std::logic_error(message) {}
+};
+
+/**
+ * @brief Exception thrown when a CUDA error is encountered.
+ */
+struct cuda_error : public std::runtime_error {
+  cuda_error(std::string const& message) : std::runtime_error(message) {}
+};
+
+}  // namespace raft
+
+#define STRINGIFY_DETAIL(x) #x
+#define RAFT_STRINGIFY(x) STRINGIFY_DETAIL(x)
+
+/**
+ * @brief Macro for checking (pre-)conditions that throws an exception when
+ * a condition is violated.
+ *
+ * @param[in] cond Expression that evaluates to true or false
+ * @param[in] reason String literal description of the reason that cond is
+ * expected to be true
+ * @throw raft::logic_error if the condition evaluates to false.
+ **/
+#define RAFT_EXPECTS(cond, reason)                                  \
+  (!!(cond)) ? static_cast<void>(0)                                 \
+             : throw raft::logic_error("RAFT failure at: " __FILE__ \
+                                       ":" RAFT_STRINGIFY(__LINE__) ": " reason)
+
+/**
+ * @brief Indicates that an erroneous code path has been taken.
+ *
+ * In host code, throws a `raft::logic_error`.
+ *
+ * @param[in] reason String literal description of the reason
+ **/
+#define RAFT_FAIL(reason) \
+  throw raft::logic_error("RAFT failure at: " __FILE__ ":" RAFT_STRINGIFY(__LINE__) ": " reason)
+
+namespace raft {
+namespace detail {
+
+inline void throw_cuda_error(cudaError_t error, const char* file, unsigned int line)
+{
+  throw raft::cuda_error(std::string{"CUDA error encountered at: " + std::string{file} + ":" +
+                                     std::to_string(line) + ": " + std::to_string(error) + " " +
+                                     cudaGetErrorName(error) + " " + cudaGetErrorString(error)});
+}
+
+}  // namespace detail
+}  // namespace raft
+
+/**
+ * @brief Error checking macro for CUDA runtime API functions.
+ *
+ * Invokes a CUDA runtime API function call, if the call does not return
+ * cudaSuccess, invokes cudaGetLastError() to clear the error and throws an
+ * exception detailing the CUDA error that occurred
+ *
+ **/
+#define CUDA_TRY(call)                                            \
+  do {                                                            \
+    cudaError_t const status = (call);                            \
+    if (cudaSuccess != status) {                                  \
+      cudaGetLastError();                                         \
+      raft::detail::throw_cuda_error(status, __FILE__, __LINE__); \
+    }                                                             \
+  } while (0);
+
+/**
+ * @brief Debug macro to check for CUDA errors
+ *
+ * In a non-release build, this macro will synchronize the specified stream
+ * before error checking. In both release and non-release builds, this macro
+ * checks for any pending CUDA errors from previous calls. If an error is
+ * reported, an exception is thrown detailing the CUDA error that occurred.
+ *
+ * The intent of this macro is to provide a mechanism for synchronous and
+ * deterministic execution for debugging asynchronous CUDA execution. It should
+ * be used after any asynchronous CUDA call, e.g., cudaMemcpyAsync, or an
+ * asynchronous kernel launch.
+ *
+ **/
+#ifndef NDEBUG
+#define CHECK_CUDA(stream) CUDA_TRY(cudaStreamSynchronize(stream));
+#else
+#define CHECK_CUDA(stream) CUDA_TRY(cudaPeekAtLastError());
+#endif
