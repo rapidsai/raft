dc537ce137f5e8df5b48c06e8758099d3b3092ad
divyegala <divyegala@gmail.com>
2020-10-22 13:00:26 -0500

    building, still couple issues

--- cpp/test/mst.cu
+++ cpp/test/mst.cu
@@ -19,6 +19,7 @@
 #include <gtest/gtest.h>
 #include <rmm/thrust_rmm_allocator.h>
 #include <iostream>
+#include <vector>
 
 #include <raft/handle.hpp>
 #include <raft/cudart_utils.h>
@@ -32,7 +33,7 @@ struct csr_t {
   value_t *weights;
   vertex_t n_vertices;
   edge_t n_edges;
-}
+};
 
 namespace raft {
 namespace mst {
@@ -41,22 +42,23 @@ namespace mst {
 // Returns total weight of MST
 template <typename vertex_t, typename edge_t, typename value_t>
 value_t prims(csr_t<vertex_t, edge_t, value_t> &csr_h) {
+  std::cout << csr_h.n_vertices;
   bool active_vertex[csr_h.n_vertices];
-  bool mst_set[csr_h.n_edges];
+  // bool mst_set[csr_h.n_edges];
   value_t curr_edge[csr_h.n_vertices];
 
-  for (auto i = 0, i < csr_h.n_vertices; i++) {
+  for (auto i = 0; i < csr_h.n_vertices; i++) {
     active_vertex[i] = false;
     curr_edge[i] = INT_MAX;
   }
   curr_edge[0] = 0;
 
-  for (auto i = 0; i < csr_h.n_edges; i++) {
-    mst_set[i] = false;
-  }
+  // for (auto i = 0; i < csr_h.n_edges; i++) {
+  //   mst_set[i] = false;
+  // }
 
   // function to pick next min vertex-edge
-  auto min_vertex_edge = [](auto &curr_edge, auto &active_vertex, auto n_vertices) {
+  auto min_vertex_edge = [](auto *curr_edge, auto *active_vertex, auto n_vertices) {
     value_t min = INT_MAX;
     vertex_t min_vertex;
 
@@ -71,7 +73,7 @@ value_t prims(csr_t<vertex_t, edge_t, value_t> &csr_h) {
   };
 
   // iterate over n vertices
-  for (int v = 0; v < n_vertices - 1; v++) {
+  for (auto v = 0; v < csr_h.n_vertices - 1; v++) {
     // pick min vertex-edge
     vertex_t curr_v = min_vertex_edge(curr_edge, active_vertex, csr_h.n_vertices);
 
@@ -85,7 +87,7 @@ value_t prims(csr_t<vertex_t, edge_t, value_t> &csr_h) {
 
       // put edges to be considered for next iteration
       auto neighbor_idx = csr_h.indices[e];
-      if (!active_vertex[neighbor_idx] && && csr_h.weights[e]) {
+      if (!active_vertex[neighbor_idx] && csr_h.weights[e]) {
         curr_edge[neighbor_idx] = csr_h.weights[e];
       }
 
@@ -95,7 +97,7 @@ value_t prims(csr_t<vertex_t, edge_t, value_t> &csr_h) {
 
   // find sum of MST
   value_t total_weight = 0;
-  for(int v = 1; v < n_vertices; v++) {
+  for(auto v = 1; v < csr_h.n_vertices; v++) {
     total_weight += curr_edge[v];
   }
 
@@ -112,15 +114,14 @@ class MSTTest : public ::testing::TestWithParam<csr_t<vertex_t, edge_t, value_t>
       rmm::device_vector<vertex_t> mst_src;
       rmm::device_vector<vertex_t> mst_dst;
 
-      MST_solver<vertex_t, edge_t, value_t> solver(h, offsets, indices, weights, v,
-                                                  e);
+      MST_solver<vertex_t, edge_t, value_t> solver(handle, csr_d.offsets, csr_d.indices, csr_d.weights, csr_d.n_vertices,
+                                                  csr_d.n_edges);
 
       //nullptr expected to trigger exceptions
       EXPECT_ANY_THROW(solver.solve(mst_src, mst_dst));
     }
 
     void SetUp() override {
-      csr_t csr_d;
       csr_d.n_vertices = csr_h.n_vertices;
       csr_d.n_edges = csr_h.n_edges;
 
@@ -139,22 +140,10 @@ class MSTTest : public ::testing::TestWithParam<csr_t<vertex_t, edge_t, value_t>
       CUDA_CHECK(cudaFree(csr_d.weights));
     }
   
-  private:
-    csr_t csr_h;
+  protected:
+    csr_t<vertex_t, edge_t, value_t> csr_h, csr_d;
 
     raft::handle_t handle;
-};
-
-const csr_t<int, int, int> csr_null_h = {
-  nullptr, nullptr, nullptr, 0, 0;
-};
-
-const csr_t<int, int, int> csr_graph1_h = {
-  {0, 3, 5, 7, 8},
-  {1, 2, 3, 0, 3, 0, 0, 1},
-  {2, 3, 4, 2, 1, 3, 4, 1},
-  4,
-  8
 };
 
 /*
@@ -165,17 +154,28 @@ Graph 1:
 (2)   (3)
 
 */
+constexpr csr_t<int, int, int> graph1() {
+  int offsets[5] = {0, 3, 5, 7, 8};
+  int indices[8] = {1, 2, 3, 0, 3, 0, 0, 1};
+  int weights[8] = {2, 3, 4, 2, 1, 3, 4, 1};
+  return csr_t<int, int, int> {offsets, indices, weights, 4, 8};
+}
+
+const std::vector<csr_t<int, int, int>> csr_in_h = {
+  // {nullptr, nullptr, nullptr, 0, 0},
+  graph1()
+};
 
 typedef MSTTest<int, int, int> MSTTestSequential;
-TEST_P(MSTTest, Sequential) {
-  this->mst_sequential();
+TEST_P(MSTTestSequential, Sequential) {
+  // this->mst_sequential();
 
   // do assertions here
   // in this case, running sequential MST
+  std::cout << prims(csr_h);
 }
 
-INSTANTIATE_TEST_SUITE_P(MSTTest, MSTTestSequential, ::testing::ValuesIn(csr_null_h));
-INSTANTIATE_TEST_SUITE_P(MSTTest, MSTTestSequential, ::testing::ValuesIn(csr_graph1_h));
+INSTANTIATE_TEST_SUITE_P(MSTTests, MSTTestSequential, ::testing::ValuesIn(csr_in_h));
 
 }  // namespace mst
 }  // namespace raft
