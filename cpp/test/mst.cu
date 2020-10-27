01c671a1890775ecf46f8e6619e604b9699e5838
Alex Fender <afender@nvidia.com>
2020-10-27 17:18:21 -0500

    Update mst.cu

--- cpp/test/mst.cu
+++ cpp/test/mst.cu
@@ -160,8 +160,13 @@ Graph 1:
 */
 
 const std::vector<CSRHost<int, int, float>> csr_in_h = {
-  // {nullptr, nullptr, nullptr, 0, 0},
   {{0, 3, 5, 7, 8}, {1, 2, 3, 0, 3, 0, 0, 1}, {2, 3, 4, 2, 1, 3, 4, 1}}};
+
+const std::vector<CSRHost<int, int, float>> csr_in2_h = {
+  {{0, 4, 6, 9, 12, 15, 17, 20},
+   {2, 4, 5, 6, 3, 6, 0, 4, 5, 1, 4, 6, 0, 2, 3, 0, 2, 0, 1, 3},
+   {5.0, 9.0, 1.0, 4.0, 8.0, 7.0, 5.0, 2.0, 6.0, 8.0,
+    3.0, 4.0, 9.0, 2.0, 3.0, 1.0, 6.0, 4.0, 7.0, 10.0}}};
 
 typedef MSTTest<int, int, float> MSTTestSequential;
 TEST_P(MSTTestSequential, Sequential) {
