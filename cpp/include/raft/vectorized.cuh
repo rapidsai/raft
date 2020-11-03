08f85354334cf84075704dd6cc1f8bdbc86d7356
Thejaswi Rao <rao.thejaswi@gmail.com>
2020-11-03 09:54:54 -0800

    FIX added no-lint to the vectorized.cuh for now

--- cpp/include/raft/vectorized.cuh
+++ cpp/include/raft/vectorized.cuh
@@ -21,245 +21,247 @@
 
 namespace raft {
 
-template <typename math_, int VecLen>
-struct IOType {};
+template <typename Math, int VecLen>
+struct IOType {};  // NOLINT
 template <>
 struct IOType<bool, 1> {
   static_assert(sizeof(bool) == sizeof(int8_t),
                 "IOType bool size assumption failed");
-  typedef int8_t Type;
+  using Type = int8_t;  // NOLINT
 };
 template <>
 struct IOType<bool, 2> {
-  typedef int16_t Type;
+  using Type = int16_t;  // NOLINT
 };
 template <>
 struct IOType<bool, 4> {
-  typedef int32_t Type;
+  using Type = int32_t;  // NOLINT
 };
 template <>
 struct IOType<bool, 8> {
-  typedef int2 Type;
+  using Type = int2;  // NOLINT
 };
 template <>
 struct IOType<bool, 16> {
-  typedef int4 Type;
+  using Type = int4;  // NOLINT
 };
 template <>
 struct IOType<int8_t, 1> {
-  typedef int8_t Type;
+  using Type = int8_t;  // NOLINT
 };
 template <>
 struct IOType<int8_t, 2> {
-  typedef int16_t Type;
+  using Type = int16_t;  // NOLINT
 };
 template <>
 struct IOType<int8_t, 4> {
-  typedef int32_t Type;
+  using Type = int32_t;  // NOLINT
 };
 template <>
 struct IOType<int8_t, 8> {
-  typedef int2 Type;
+  using Type = int2;  // NOLINT
 };
 template <>
 struct IOType<int8_t, 16> {
-  typedef int4 Type;
+  using Type = int4;  // NOLINT
 };
 template <>
 struct IOType<uint8_t, 1> {
-  typedef uint8_t Type;
+  using Type = uint8_t;  // NOLINT
 };
 template <>
 struct IOType<uint8_t, 2> {
-  typedef uint16_t Type;
+  using Type = uint16_t;  // NOLINT
 };
 template <>
 struct IOType<uint8_t, 4> {
-  typedef uint32_t Type;
+  using Type = uint32_t;  // NOLINT
 };
 template <>
 struct IOType<uint8_t, 8> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<uint8_t, 16> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<int16_t, 1> {
-  typedef int16_t Type;
+  using Type = int16_t;  // NOLINT
 };
 template <>
 struct IOType<int16_t, 2> {
-  typedef int32_t Type;
+  using Type = int32_t;  // NOLINT
 };
 template <>
 struct IOType<int16_t, 4> {
-  typedef int2 Type;
+  using Type = int2;  // NOLINT
 };
 template <>
 struct IOType<int16_t, 8> {
-  typedef int4 Type;
+  using Type = int4;  // NOLINT
 };
 template <>
 struct IOType<uint16_t, 1> {
-  typedef uint16_t Type;
+  using Type = uint16_t;  // NOLINT
 };
 template <>
 struct IOType<uint16_t, 2> {
-  typedef uint32_t Type;
+  using Type = uint32_t;  // NOLINT
 };
 template <>
 struct IOType<uint16_t, 4> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<uint16_t, 8> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<__half, 1> {
-  typedef __half Type;
+  using Type = __half;  // NOLINT
 };
 template <>
 struct IOType<__half, 2> {
-  typedef __half2 Type;
+  using Type = __half2;  // NOLINT
 };
 template <>
 struct IOType<__half, 4> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<__half, 8> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<__half2, 1> {
-  typedef __half2 Type;
+  using Type = __half2;  // NOLINT
 };
 template <>
 struct IOType<__half2, 2> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<__half2, 4> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<int32_t, 1> {
-  typedef int32_t Type;
+  using Type = int32_t;  // NOLINT
 };
 template <>
 struct IOType<int32_t, 2> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<int32_t, 4> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<uint32_t, 1> {
-  typedef uint32_t Type;
+  using Type = uint32_t;  // NOLINT
 };
 template <>
 struct IOType<uint32_t, 2> {
-  typedef uint2 Type;
+  using Type = uint2;  // NOLINT
 };
 template <>
 struct IOType<uint32_t, 4> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<float, 1> {
-  typedef float Type;
+  using Type = float;  // NOLINT
 };
 template <>
 struct IOType<float, 2> {
-  typedef float2 Type;
+  using Type = float2;  // NOLINT
 };
 template <>
 struct IOType<float, 4> {
-  typedef float4 Type;
+  using Type = float4;  // NOLINT
 };
 template <>
 struct IOType<int64_t, 1> {
-  typedef int64_t Type;
+  using Type = int64_t;  // NOLINT
 };
 template <>
 struct IOType<int64_t, 2> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<uint64_t, 1> {
-  typedef uint64_t Type;
+  using Type = uint64_t;  // NOLINT
 };
 template <>
 struct IOType<uint64_t, 2> {
-  typedef uint4 Type;
+  using Type = uint4;  // NOLINT
 };
+// there's no guarantee that uint64_t and unsigned long long will be same types!
 template <>
-struct IOType<unsigned long long, 1> {
-  typedef unsigned long long Type;
+struct IOType<unsigned long long, 1> {  // NOLINT
+  using Type = unsigned long long;  // NOLINT
 };
+// there's no guarantee that uint64_t and unsigned long long will be same types!
 template <>
-struct IOType<unsigned long long, 2> {
-  typedef uint4 Type;
+struct IOType<unsigned long long, 2> {  // NOLINT
+  using Type = uint4;  // NOLINT
 };
 template <>
 struct IOType<double, 1> {
-  typedef double Type;
+  using Type = double;  // NOLINT
 };
 template <>
 struct IOType<double, 2> {
-  typedef double2 Type;
+  using Type = double2;  // NOLINT
 };
 
 /**
-     * @struct TxN_t
-     *
-     * @brief Internal data structure that is used to define a facade for vectorized
-     * loads/stores across the most common POD types. The goal of his file is to
-     * provide with CUDA programmers, an easy way to have compiler issue vectorized
-     * load or store instructions to memory (either global or shared). Vectorized
-     * accesses to memory are important as they'll utilize its resources
-     * efficiently,
-     * when compared to their non-vectorized counterparts. Obviously, for whatever
-     * reasons if one is unable to issue such vectorized operations, one can always
-     * fallback to using POD types.
-     *
-     * Example demonstrating the use of load operations, performing math on such
-     * loaded data and finally storing it back.
-     * @code{.cu}
-     * TxN_t<uint8_t,8> mydata1, mydata2;
-     * int idx = (threadIdx.x + (blockIdx.x * blockDim.x)) * mydata1.Ratio;
-     * mydata1.load(ptr1, idx);
-     * mydata2.load(ptr2, idx);
-     * #pragma unroll
-     * for(int i=0;i<mydata1.Ratio;++i) {
-     *     mydata1.val.data[i] += mydata2.val.data[i];
-     * }
-     * mydata1.store(ptr1, idx);
-     * @endcode
-     *
-     * By doing as above, the interesting thing is that the code effectively remains
-     * almost the same, in case one wants to upgrade to TxN_t<uint16_t,16> type.
-     * Only change required is to replace variable declaration appropriately.
-     *
-     * Obviously, it's caller's responsibility to take care of pointer alignment!
-     *
-     * @tparam math_ the data-type in which the compute/math needs to happen
-     * @tparam veclen_ the number of 'math_' types to be loaded/stored per
-     * instruction
-     */
-template <typename math_, int veclen_>
-struct TxN_t {
+ * @struct TxN_t
+ *
+ * @brief Internal data structure that is used to define a facade for vectorized
+ * loads/stores across the most common POD types. The goal of his file is to
+ * provide with CUDA programmers, an easy way to have compiler issue vectorized
+ * load or store instructions to memory (either global or shared). Vectorized
+ * accesses to memory are important as they'll utilize its resources
+ * efficiently,
+ * when compared to their non-vectorized counterparts. Obviously, for whatever
+ * reasons if one is unable to issue such vectorized operations, one can always
+ * fallback to using POD types.
+ *
+ * Example demonstrating the use of load operations, performing math on such
+ * loaded data and finally storing it back.
+ * @code{.cu}
+ * TxN_t<uint8_t,8> mydata1, mydata2;
+ * int idx = (threadIdx.x + (blockIdx.x * blockDim.x)) * mydata1.Ratio;
+ * mydata1.load(ptr1, idx);
+ * mydata2.load(ptr2, idx);
+ * #pragma unroll
+ * for(int i=0;i<mydata1.Ratio;++i) {
+ *     mydata1.val.data[i] += mydata2.val.data[i];
+ * }
+ * mydata1.store(ptr1, idx);
+ * @endcode
+ *
+ * By doing as above, the interesting thing is that the code effectively remains
+ * almost the same, in case one wants to upgrade to TxN_t<uint16_t,16> type.
+ * Only change required is to replace variable declaration appropriately.
+ *
+ * Obviously, it's caller's responsibility to take care of pointer alignment!
+ *
+ * @tparam math_ the data-type in which the compute/math needs to happen
+ * @tparam veclen_ the number of 'math_' types to be loaded/stored per
+ * instruction
+ */
+template <typename Math, int VecLen>
+struct TxN_t {  // NOLINT
   /** underlying math data type */
-  typedef math_ math_t;
+  using math_t = Matht;  // NOLINT
   /** internal storage data type */
-  typedef typename IOType<math_t, veclen_>::Type io_t;
+  using io_t = typename IOType<math_t, VecLen>::Type;  // NOLINT
 
   /** defines the number of 'math_t' types stored by this struct */
-  static const int Ratio = veclen_;
+  static const int Ratio = VecLen;  // NOLINT
 
   union {
     /** the vectorized data that is used for subsequent operations */
@@ -298,20 +300,20 @@ struct TxN_t {
    * case of stores, the data in the val.data will be stored to that location.
    * @{
    */
-  template <typename idx_t = int>
-  DI void load(const math_t *ptr, idx_t idx) {
+  template <typename IdxT = int>
+  DI void load(const math_t *ptr, IdxT idx) {
     const io_t *bptr = reinterpret_cast<const io_t *>(&ptr[idx]);
     val.internal = __ldg(bptr);
   }
 
-  template <typename idx_t = int>
-  DI void load(math_t *ptr, idx_t idx) {
+  template <typename IdxT = int>
+  DI void load(math_t *ptr, IdxT idx) {
     io_t *bptr = reinterpret_cast<io_t *>(&ptr[idx]);
     val.internal = *bptr;
   }
 
-  template <typename idx_t = int>
-  DI void store(math_t *ptr, idx_t idx) {
+  template <typename IdxT = int>
+  DI void store(math_t *ptr, IdxT idx) {
     io_t *bptr = reinterpret_cast<io_t *>(&ptr[idx]);
     *bptr = val.internal;
   }
@@ -319,22 +321,22 @@ struct TxN_t {
 };
 
 /** this is just to keep the compiler happy! */
-template <typename math_>
-struct TxN_t<math_, 0> {
-  typedef math_ math_t;
-  static const int Ratio = 1;
+template <typename Math>
+struct TxN_t<Math, 0> {
+  using math_t = Math;  // NOLINT
+  static const int Ratio = 1;  // NOLINT
 
   union {
-    math_t data[1];
+    math_t data[1];  // NOLINT
   } val;
 
   DI void fill(math_t _val) {}
-  template <typename idx_t = int>
-  DI void load(const math_t *ptr, idx_t idx) {}
-  template <typename idx_t = int>
-  DI void load(math_t *ptr, idx_t idx) {}
-  template <typename idx_t = int>
-  DI void store(math_t *ptr, idx_t idx) {}
+  template <typename IdxT = int>
+  DI void load(const math_t *ptr, IdxT idx) {}
+  template <typename IdxT = int>
+  DI void load(math_t *ptr, IdxT idx) {}
+  template <typename IdxT = int>
+  DI void store(math_t *ptr, IdxT idx) {}
 };
 
 }  // namespace raft
