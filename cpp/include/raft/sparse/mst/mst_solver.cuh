8f5621882022625c91f7cfb6a8d8a94458ceb2f0
divyegala <divyegala@gmail.com>
2021-02-17 12:44:31 -0800

    fixing

--- cpp/include/raft/sparse/mst/mst_solver.cuh
+++ cpp/include/raft/sparse/mst/mst_solver.cuh
@@ -42,7 +42,7 @@ class MST_solver {
   MST_solver(const raft::handle_t& handle_, const edge_t* offsets_,
              const vertex_t* indices_, const weight_t* weights_,
              const vertex_t v_, const edge_t e_, vertex_t* color_,
-             cudaStream_t stream_, bool initialize_colors_);
+             cudaStream_t stream_, bool initialize_colors_=true);
 
   raft::Graph_COO<vertex_t, edge_t, weight_t> solve();
 
