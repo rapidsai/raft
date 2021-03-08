ede30e0ad39b4dfc5a434efeb4166e44650868d3
Corey J. Nolet <cjnolet@gmail.com>
2021-03-08 17:37:24 -0500

    Updating style

--- cpp/test/sparse/connect_components.cu
+++ cpp/test/sparse/connect_components.cu
@@ -113,7 +113,8 @@ class ConnectComponentsTest : public ::testing::TestWithParam<
       handle, indptr.data(), knn_graph_coo.cols(), knn_graph_coo.vals(),
       params.n_row, knn_graph_coo.nnz, colors.data(), stream, false);
 
-    raft::print_device_vector("colors", colors.data(), colors.size(), std::cout);
+    raft::print_device_vector("colors", colors.data(), colors.size(),
+                              std::cout);
 
     /**
      * 3. connect_components to fix connectivities
