50d1cdc100bc31e649399ad1ab883b8334a94703
Corey J. Nolet <cjnolet@gmail.com>
2021-05-19 22:27:36 -0400

    Getting test to build

--- cpp/test/sparse/connect_components.cu
+++ cpp/test/sparse/connect_components.cu
@@ -99,9 +99,10 @@ class ConnectComponentsTest : public ::testing::TestWithParam<
     /**
      * 3. connect_components to fix connectivities
      */
+    raft::linkage::FixConnectivitiesRedOp<value_idx, value_t> red_op(colors.data(), params.n_row);
     raft::linkage::connect_components<value_idx, value_t>(
       handle, out_edges, data.data(), colors.data(), params.n_row,
-      params.n_col);
+      params.n_col, red_op);
 
     /**
      * Construct final edge list
