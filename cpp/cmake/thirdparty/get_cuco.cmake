7e727dcdd9118d668522fe75e019858601affa27
ptaylor <paul.e.taylor@me.com>
2021-06-23 19:28:23 -0500

    test with my cuco branch

--- cpp/cmake/thirdparty/get_cuco.cmake
+++ cpp/cmake/thirdparty/get_cuco.cmake
@@ -25,8 +25,8 @@ function(find_and_configure_cuco VERSION)
       BUILD_EXPORT_SET    raft-exports
       INSTALL_EXPORT_SET  raft-exports
       CPM_ARGS
-        GIT_REPOSITORY https://github.com/NVIDIA/cuCollections.git
-        GIT_TAG        e5e2abe55152608ef449ecf162a1ef52ded19801
+        GIT_REPOSITORY https://github.com/trxcllnt/cuCollections.git
+        GIT_TAG        dev
         OPTIONS        "BUILD_TESTS OFF"
                        "BUILD_BENCHMARKS OFF"
                        "BUILD_EXAMPLES OFF"
