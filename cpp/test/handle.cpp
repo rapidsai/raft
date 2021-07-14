e9c88dfcd5a4cbc15269404b9d65cc003bdd31d2
divyegala <divyegala@gmail.com>
2021-07-14 12:24:21 -0700

    working handle cpp tests

--- cpp/test/handle.cpp
+++ cpp/test/handle.cpp
@@ -20,69 +20,45 @@
 #include <memory>
 #include <raft/handle.hpp>
 
-// namespace raft {
+namespace raft {
 
-// TEST(Raft, HandleDefault) {
-//   handle_t h;
-//   ASSERT_EQ(0, h.get_num_internal_streams());
-//   ASSERT_EQ(0, h.get_device());
-//   ASSERT_EQ(nullptr, h.get_stream());
-//   ASSERT_NE(nullptr, h.get_cublas_handle());
-//   ASSERT_NE(nullptr, h.get_cusolver_dn_handle());
-//   ASSERT_NE(nullptr, h.get_cusolver_sp_handle());
-//   ASSERT_NE(nullptr, h.get_cusparse_handle());
-// }
+TEST(Raft, HandleDefault) {
+  handle_t h;
+  ASSERT_EQ(0, h.get_stream_pool().get_pool_size());
+  ASSERT_EQ(0, h.get_device());
+  ASSERT_EQ(rmm::cuda_stream_default, h.get_stream());
+  ASSERT_NE(nullptr, h.get_cublas_handle());
+  ASSERT_NE(nullptr, h.get_cusolver_dn_handle());
+  ASSERT_NE(nullptr, h.get_cusolver_sp_handle());
+  ASSERT_NE(nullptr, h.get_cusparse_handle());
+}
 
-// TEST(Raft, Handle) {
-//   handle_t h(4);
-//   ASSERT_EQ(4, h.get_num_internal_streams());
-//   cudaStream_t stream;
-//   CUDA_CHECK(cudaStreamCreate(&stream));
-//   h.set_stream(stream);
-//   ASSERT_EQ(stream, h.get_stream());
-//   CUDA_CHECK(cudaStreamSynchronize(stream));
-//   CUDA_CHECK(cudaStreamDestroy(stream));
-// }
+TEST(Raft, Handle) {
+  // test stream pool creation
+  rmm::cuda_stream_pool stream_pool {4};
+  handle_t h(rmm::cuda_stream_default, stream_pool);
+  ASSERT_EQ(4, h.get_stream_pool().get_pool_size());
 
-// TEST(Raft, GetInternalStreams) {
-//   handle_t h(4);
-//   auto streams = h.get_internal_streams();
-//   ASSERT_EQ(4U, streams.size());
-// }
+  // test non default stream handle
+  cudaStream_t stream;
+  CUDA_CHECK(cudaStreamCreate(&stream));
+  rmm::cuda_stream_view stream_view(stream);
+  handle_t handle(stream_view);
+  ASSERT_EQ(stream_view, handle.get_stream());
+  CUDA_CHECK(cudaStreamSynchronize(stream));
+  CUDA_CHECK(cudaStreamDestroy(stream));
+}
 
-// TEST(Raft, GetHandleFromPool) {
-//   handle_t parent(4);
+TEST(Raft, GetHandleFromPool) {
+  constexpr std::size_t n_streams = 4;
+  rmm::cuda_stream_pool stream_pool(n_streams);
+  handle_t parent(rmm::cuda_stream_default, stream_pool);
 
-//   handle_t child(parent, 2);
-//   ASSERT_EQ(parent.get_internal_stream(2), child.get_stream());
-//   ASSERT_EQ(0, child.get_num_internal_streams());
+  for (std::size_t i = 0; i < n_streams; i++) {
+      auto worker_stream = parent.get_stream_pool().get_stream(i);
+      handle_t child(worker_stream);
+      ASSERT_EQ(parent.get_stream_pool().get_stream(i), child.get_stream());
+  }
+}
 
-//   child.set_stream(parent.get_internal_stream(3));
-//   ASSERT_EQ(parent.get_internal_stream(3), child.get_stream());
-//   ASSERT_NE(parent.get_internal_stream(2), child.get_stream());
-
-//   ASSERT_EQ(parent.get_device(), child.get_device());
-// }
-
-// TEST(Raft, GetHandleFromPoolPerf) {
-//   handle_t parent(100);
-//   auto start = curTimeMillis();
-//   for (int i = 0; i < parent.get_num_internal_streams(); i++) {
-//     handle_t child(parent, i);
-//     ASSERT_EQ(parent.get_internal_stream(i), child.get_stream());
-//     child.wait_on_user_stream();
-//   }
-//   // upperbound on 0.1ms per child handle
-//   ASSERT_LE(curTimeMillis() - start, 10);
-// }
-
-// TEST(Raft, GetHandleStreamViews) {
-//   handle_t parent(4);
-
-//   handle_t child(parent, 2);
-//   ASSERT_EQ(parent.get_internal_stream_view(2), child.get_stream_view());
-//   ASSERT_EQ(parent.get_internal_stream_view(2).value(),
-//             child.get_stream_view().value());
-//   EXPECT_FALSE(child.get_stream_view().is_default());
-// }
-// }  // namespace raft
+}
