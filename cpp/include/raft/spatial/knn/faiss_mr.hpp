2a7517db321eaaead8b5f38e2831befcc0ea9bf0
viclafargue <viclafargue@nvidia.com>
2021-10-25 17:11:59 +0200

    Move faiss_mr.hpp and add tests

--- cpp/include/raft/spatial/knn/faiss_mr.hpp
+++ cpp/include/raft/spatial/knn/faiss_mr.hpp
@@ -424,10 +424,13 @@ class RmmGpuResourcesImpl : public GpuResources {
     }
 
     if (req.space == MemorySpace::Temporary) {
+      std::cout << "dealloc Temporary" << std::endl;
       tempMemory_[device]->deallocMemory(device, req.stream, req.size, p);
     } else if (req.space == MemorySpace::Device) {
+      std::cout << "dealloc Device" << std::endl;
       cmr->deallocate(p, req.size, req.stream);
     } else if (req.space == MemorySpace::Unified) {
+      std::cout << "dealloc Unified" << std::endl;
       mmr->deallocate(p, req.size, req.stream);
     } else {
       FAISS_ASSERT_FMT(false, "unknown MemorySpace %d", (int)req.space);
