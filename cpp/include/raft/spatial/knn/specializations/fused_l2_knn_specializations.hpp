e5568d6c4a2923c59519f1a2ea1b32c0c400fbf9
Corey J. Nolet <cjnolet@gmail.com>
2021-12-06 10:45:03 -0500

    Fixes after clang-format changes

--- cpp/include/raft/spatial/knn/specializations/fused_l2_knn_specializations.hpp
+++ cpp/include/raft/spatial/knn/specializations/fused_l2_knn_specializations.hpp
@@ -22,132 +22,57 @@ namespace spatial {
 namespace knn {
 namespace detail {
 
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2SqrtUnexpanded, std::int64_t, float, true>(
-  size_t D,
-  std::int64_t* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
+extern template void fusedL2Knn<long, float, true>(size_t D,
+                                                   long* out_inds,
+                                                   float* out_dists,
+                                                   const float* index,
+                                                   const float* query,
+                                                   size_t n_index_rows,
+                                                   size_t n_query_rows,
+                                                   int k,
+                                                   bool rowMajorIndex,
+                                                   bool rowMajorQuery,
+                                                   cudaStream_t stream,
+                                                   raft::distance::DistanceType metric);
 
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2Unexpanded, std::int64_t, float, true>(
-  size_t D,
-  std::int64_t* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
+extern template void fusedL2Knn<long, float, false>(size_t D,
+                                                    long* out_inds,
+                                                    float* out_dists,
+                                                    const float* index,
+                                                    const float* query,
+                                                    size_t n_index_rows,
+                                                    size_t n_query_rows,
+                                                    int k,
+                                                    bool rowMajorIndex,
+                                                    bool rowMajorQuery,
+                                                    cudaStream_t stream,
+                                                    raft::distance::DistanceType metric);
 
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2SqrtUnexpanded, int, float, true>(
-  size_t D,
-  int* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
+extern template void fusedL2Knn<int, float, true>(size_t D,
+                                                  int* out_inds,
+                                                  float* out_dists,
+                                                  const float* index,
+                                                  const float* query,
+                                                  size_t n_index_rows,
+                                                  size_t n_query_rows,
+                                                  int k,
+                                                  bool rowMajorIndex,
+                                                  bool rowMajorQuery,
+                                                  cudaStream_t stream,
+                                                  raft::distance::DistanceType metric);
 
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2Unexpanded, int, float, true>(size_t D,
-                                                                                int* out_inds,
-                                                                                float* out_dists,
-                                                                                const float* index,
-                                                                                const float* query,
-                                                                                size_t n_index_rows,
-                                                                                size_t n_query_rows,
-                                                                                int k,
-                                                                                bool rowMajorIndex,
-                                                                                bool rowMajorQuery,
-                                                                                cudaStream_t stream,
-                                                                                void* workspace,
-                                                                                size_t& worksize);
-
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2SqrtUnexpanded, std::int64_t, float, false>(
-  size_t D,
-  std::int64_t* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
-
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2Unexpanded, std::int64_t, float, false>(
-  size_t D,
-  std::int64_t* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
-
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2SqrtUnexpanded, int, float, false>(
-  size_t D,
-  int* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
-
-extern template void
-l2_unexpanded_knn<raft::distance::DistanceType::L2Unexpanded, int, float, false>(
-  size_t D,
-  int* out_inds,
-  float* out_dists,
-  const float* index,
-  const float* query,
-  size_t n_index_rows,
-  size_t n_query_rows,
-  int k,
-  bool rowMajorIndex,
-  bool rowMajorQuery,
-  cudaStream_t stream,
-  void* workspace,
-  size_t& worksize);
+extern template void fusedL2Knn<int, float, false>(size_t D,
+                                                   int* out_inds,
+                                                   float* out_dists,
+                                                   const float* index,
+                                                   const float* query,
+                                                   size_t n_index_rows,
+                                                   size_t n_query_rows,
+                                                   int k,
+                                                   bool rowMajorIndex,
+                                                   bool rowMajorQuery,
+                                                   cudaStream_t stream,
+                                                   raft::distance::DistanceType metric);
 
 };  // namespace detail
 };  // namespace knn
