51734071b627fb787943a2284b23c47ea982999c
Corey J. Nolet <cjnolet@gmail.com>
2021-12-08 16:40:28 -0500

    Fixing style

--- cpp/test/spatial/ball_cover.cu
+++ cpp/test/spatial/ball_cover.cu
@@ -17,8 +17,8 @@
 #include <raft/cudart_utils.h>
 #include <raft/linalg/distance_type.h>
 #include <raft/spatial/knn/ball_cover.hpp>
-#include <raft/spatial/specializations.hpp>
 #include <raft/spatial/knn/detail/knn_brute_force_faiss.cuh>
+#include <raft/spatial/specializations.hpp>
 #include <rmm/device_uvector.hpp>
 #include "../test_utils.h"
 #include "spatial_data.h"
