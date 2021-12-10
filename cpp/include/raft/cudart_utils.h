rapidsai/raft

commit 2ecdd34a6f0b878fe91312bf0156a3e70240fc7f

Consistent renaming of CHECK_CUDA and *_TRY macros (#410)
This PR makes the naming of cuda status checks/logs consistent with other RAPIDS libraries. 

1. `RAFT_CHECK_CUDA` is used to check if a CUDA error has been thrown (and synchronize if in debug mode) given a stream
2. `RAFT_{lib_name}_TRY` evaluates the return code of a library function
3. `{lib_name}_CHECK` invokes its corresponding `RAFT_{lib_name}_TRY` macro for now. Once these are removed from all consuming projects, we can simply remove them.

Authors:
  - Corey J. Nolet (https://github.com/cjnolet)

Approvers:
  - Seunghwa Kang (https://github.com/seunghwak)
  - Chuck Hastings (https://github.com/ChuckHastings)

URL: https://github.com/rapidsai/raft/pull/410

cpp/include/raft/cudart_utils.h
@@ -53,8 +53,7 @@ struct cuda_error : public raft::exception {
  * exception detailing the CUDA error that occurred
  *
  */
-#ifndef CUDA_TRY
-#define CUDA_TRY(call)                             \
+#define RAFT_CUDA_TRY(call)                        \
   do {                                             \
     cudaError_t const status = call;               \
     if (status != cudaSuccess) {                   \
@@ -69,7 +68,12 @@ struct cuda_error : public raft::exception {
       throw raft::cuda_error(msg);                 \
     }                                              \
   } while (0)
+
+// FIXME: Remove after consumers rename
+#ifndef CUDA_TRY
+#define CUDA_TRY(call) RAFT_CUDA_TRY(call)
 #endif
+
 /**
  * @brief Debug macro to check for CUDA errors
  *
@@ -84,23 +88,26 @@ struct cuda_error : public raft::exception {
  * asynchronous kernel launch.
  */
 #ifndef NDEBUG
-#define CHECK_CUDA(stream) CUDA_TRY(cudaStreamSynchronize(stream));
+#define RAFT_CHECK_CUDA(stream) RAFT_CUDA_TRY(cudaStreamSynchronize(stream));
 #else
-#define CHECK_CUDA(stream) CUDA_TRY(cudaPeekAtLastError());
+#define RAFT_CHECK_CUDA(stream) RAFT_CUDA_TRY(cudaPeekAtLastError());
 #endif
 
-/** FIXME: temporary alias for cuML compatibility */
+// FIXME: Remove after consumers rename
+#ifndef CHECK_CUDA
+#define CHECK_CUDA(call) RAFT_CHECK_CUDA(call)
+#endif
+
+/** FIXME: remove after cuml rename */
 #ifndef CUDA_CHECK
-#define CUDA_CHECK(call) CUDA_TRY(call)
+#define CUDA_CHECK(call) RAFT_CUDA_TRY(call)
 #endif
 
-///@todo: enable this only after we have added logging support in raft
 // /**
 //  * @brief check for cuda runtime API errors but log error instead of raising
 //  *        exception.
 //  */
-#ifndef CUDA_CHECK_NO_THROW
-#define CUDA_CHECK_NO_THROW(call)                                  \
+#define RAFT_CUDA_TRY_NO_THROW(call)                               \
   do {                                                             \
     cudaError_t const status = call;                               \
     if (cudaSuccess != status) {                                   \
@@ -111,6 +118,10 @@ struct cuda_error : public raft::exception {
              cudaGetErrorString(status));                          \
     }                                                              \
   } while (0)
+
+// FIXME: Remove after cuml rename
+#ifndef CUDA_CHECK_NO_THROW
+#define CUDA_CHECK_NO_THROW(call) RAFT_CHECK_CUDA_NO_THROW(call)
 #endif
 
 /**
@@ -118,8 +129,6 @@ struct cuda_error : public raft::exception {
  * TODO: Rename original implementations in 22.04 to fix
  * https://github.com/rapidsai/raft/issues/128
  */
-#define RAFT_CUDA_CHECK(call)          CUDA_CHECK(call)
-#define RAFT_CUDA_CHECK_NO_THROW(call) CUDA_CHECK_NO_THROW(call)
 
 namespace raft {
 
@@ -328,9 +337,9 @@ inline void deallocate_all(rmm::cuda_stream_view stream)
 inline int getSharedMemPerBlock()
 {
   int devId;
-  CUDA_CHECK(cudaGetDevice(&devId));
+  RAFT_CUDA_TRY(cudaGetDevice(&devId));
   int smemPerBlk;
-  CUDA_CHECK(cudaDeviceGetAttribute(&smemPerBlk, cudaDevAttrMaxSharedMemoryPerBlock, devId));
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&smemPerBlk, cudaDevAttrMaxSharedMemoryPerBlock, devId));
   return smemPerBlk;
 }
 
@@ -338,9 +347,9 @@ inline int getSharedMemPerBlock()
 inline int getMultiProcessorCount()
 {
   int devId;
-  CUDA_CHECK(cudaGetDevice(&devId));
+  RAFT_CUDA_TRY(cudaGetDevice(&devId));
   int mpCount;
-  CUDA_CHECK(cudaDeviceGetAttribute(&mpCount, cudaDevAttrMultiProcessorCount, devId));
+  RAFT_CUDA_TRY(cudaDeviceGetAttribute(&mpCount, cudaDevAttrMultiProcessorCount, devId));
   return mpCount;
 }
 
@@ -352,7 +361,7 @@ std::string arr2Str(const T* arr, int size, std::string name, cudaStream_t strea
 
   T* arr_h = (T*)malloc(size * sizeof(T));
   update_host(arr_h, arr, size, stream);
-  CUDA_CHECK(cudaStreamSynchronize(stream));
+  RAFT_CUDA_TRY(cudaStreamSynchronize(stream));
 
   ss << name << " = [ ";
   for (int i = 0; i < size; i++) {

commit a4bf6eaac8437c6e377ff97a96ae62db6a4232d6

Fix CUDA_CHECK_NO_THROW compatibility define
cuML builds broke after 2ecdd34a6f.

cpp/include/raft/cudart_utils.h
@@ -121,7 +121,7 @@ struct cuda_error : public raft::exception {
 
 // FIXME: Remove after cuml rename
 #ifndef CUDA_CHECK_NO_THROW
-#define CUDA_CHECK_NO_THROW(call) RAFT_CHECK_CUDA_NO_THROW(call)
+#define CUDA_CHECK_NO_THROW(call) RAFT_CUDA_TRY_NO_THROW(call)
 #endif
 
 /**