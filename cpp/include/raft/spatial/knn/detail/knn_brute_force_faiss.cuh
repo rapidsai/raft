2d52f2b0d70692a9250a03e807dc36deee966e55
Corey J. Nolet <cjnolet@gmail.com>
2021-12-13 13:34:18 -0500

    Disabling fused l2 knn again. Not sure how this got added back.

--- cpp/include/raft/spatial/knn/detail/knn_brute_force_faiss.cuh
+++ cpp/include/raft/spatial/knn/detail/knn_brute_force_faiss.cuh
@@ -294,24 +294,24 @@ void brute_force_knn_impl(
     cudaStream_t stream = raft::select_stream(userStream, internalStreams, n_int_streams, i);
 
     //    // TODO: Enable this once we figure out why it's causing pytest failures in cuml.
-    if (k <= 64 && rowMajorQuery == rowMajorIndex && rowMajorQuery == true &&
-        (metric == raft::distance::DistanceType::L2Unexpanded ||
-         metric == raft::distance::DistanceType::L2SqrtUnexpanded ||
-         metric == raft::distance::DistanceType::L2Expanded ||
-         metric == raft::distance::DistanceType::L2SqrtExpanded)) {
-      fusedL2Knn(D,
-                 out_i_ptr,
-                 out_d_ptr,
-                 input[i],
-                 search_items,
-                 sizes[i],
-                 n,
-                 k,
-                 rowMajorIndex,
-                 rowMajorQuery,
-                 stream,
-                 metric);
-    } else {
+//    if (k <= 64 && rowMajorQuery == rowMajorIndex && rowMajorQuery == true &&
+//        (metric == raft::distance::DistanceType::L2Unexpanded ||
+//         metric == raft::distance::DistanceType::L2SqrtUnexpanded ||
+//         metric == raft::distance::DistanceType::L2Expanded ||
+//         metric == raft::distance::DistanceType::L2SqrtExpanded)) {
+//      fusedL2Knn(D,
+//                 out_i_ptr,
+//                 out_d_ptr,
+//                 input[i],
+//                 search_items,
+//                 sizes[i],
+//                 n,
+//                 k,
+//                 rowMajorIndex,
+//                 rowMajorQuery,
+//                 stream,
+//                 metric);
+//    } else {
       switch (metric) {
         case raft::distance::DistanceType::Haversine:
 
@@ -350,7 +350,7 @@ void brute_force_knn_impl(
            */
           bfKnn(&gpu_res, args);
       }
-    }
+//    }
 
     RAFT_CUDA_TRY(cudaPeekAtLastError());
   }
