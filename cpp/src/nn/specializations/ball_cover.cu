d1a70a4d6e8a0a6d9f18ba1addf11df07ce22baf
Artem M. Chirkin <9253178+achirkin@users.noreply.github.com>
2021-12-17 11:10:49 +0100

    Update cpp/src/nn/specializations/ball_cover.cu
    Co-authored-by: Matt Joux <matthias.kohl@hotmail.de>

--- cpp/src/nn/specializations/ball_cover.cu
+++ cpp/src/nn/specializations/ball_cover.cu
@@ -17,7 +17,16 @@
 #include <cstdint>
 #include <raft/spatial/knn/ball_cover.hpp>
 #include <raft/spatial/knn/ball_cover_common.h>
+#include <raft/spatial/knn/ball_cover.hpp>
+#include <raft/spatial/knn/ball_cover_common.h>
 
+// Ignore upstream specializations to avoid unnecessary recompiling
+#include <raft/distance/specializations.hpp>
+#include <raft/spatial/knn/specializations/detail/ball_cover_lowdim.hpp>
+#include <raft/spatial/knn/specializations/fused_l2_knn.hpp>
+#include <raft/spatial/knn/specializations/knn.hpp>
+
+#include <cstdint>
 // Ignore upstream specializations to avoid unnecessary recompiling
 #include <raft/distance/specializations.hpp>
 #include <raft/spatial/knn/specializations/detail/ball_cover_lowdim.hpp>
