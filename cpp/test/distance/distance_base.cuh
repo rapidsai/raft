754cc889ef5b292caaba83676f95aa3f11f80308
Artem M. Chirkin <9253178+achirkin@users.noreply.github.com>
2021-12-17 15:45:28 +0100

    NVTX range helpers (#416)
    Moves the helpers for NVTX ranges from cuml with minor additions.
    
    Also adds the helpers to couple places in the codebase - as a way of testing the new feature.
    
    Authors:
      - Artem M. Chirkin (https://github.com/achirkin)
    
    Approvers:
      - William Hicks (https://github.com/wphicks)
      - Corey J. Nolet (https://github.com/cjnolet)
    
    URL: https://github.com/rapidsai/raft/pull/416

--- cpp/test/distance/distance_base.cuh
+++ cpp/test/distance/distance_base.cuh
@@ -16,6 +16,7 @@
 
 #include "../test_utils.h"
 #include <gtest/gtest.h>
+#include <raft/common/nvtx.hpp>
 #include <raft/cuda_utils.cuh>
 #include <raft/cudart_utils.h>
 #include <raft/distance/distance.hpp>
@@ -416,6 +417,9 @@ class DistanceTest : public ::testing::TestWithParam<DistanceInputs<DataType>> {
 
   void SetUp() override
   {
+    auto testInfo = testing::UnitTest::GetInstance()->current_test_info();
+    common::nvtx::range fun_scope("test::%s/%s", testInfo->test_suite_name(), testInfo->name());
+
     raft::random::Rng r(params.seed);
     int m               = params.m;
     int n               = params.n;
