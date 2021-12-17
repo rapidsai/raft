9efcf33959d94f25bac64e48cad3de2febc8e2fd
achirkin <achirkin@users.noreply.github.com>
2021-12-17 10:39:02 +0100

    Add doxygen target to CI

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020, NVIDIA CORPORATION.
+# Copyright (c) 2020-2021, NVIDIA CORPORATION.
 #########################################
 # cuML GPU build and test script for CI #
 #########################################
@@ -88,7 +88,7 @@ export LD_LIBRARY_PATH_CACHED=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 gpuci_logger "Build C++ and Python targets"
-"$WORKSPACE/build.sh" cppraft pyraft -v
+"$WORKSPACE/build.sh" cppraft pyraft cppdocs -v
 
 gpuci_logger "Resetting LD_LIBRARY_PATH"
 
