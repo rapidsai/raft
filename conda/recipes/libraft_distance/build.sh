5381ccdcfc673f78c06b35f92d24bc219df44d4b
Corey J. Nolet <cjnolet@gmail.com>
2022-01-11 12:49:59 -0500

    Separating out raft dependencies.

---
+++ conda/recipes/libraft_distance/build.sh
@@ -0,0 +1,7 @@
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # C++11 requires 10.9
+    # but cudatoolkit 8 is build for 10.11
+    export MACOSX_DEPLOYMENT_TARGET=10.11
+fi
+
+./build.sh clean libraft -v --allgpuarch # TODO: Compile only distance package instantiations
