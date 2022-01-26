511fafa773ca86b4f1ba858497a5af62daddb23b
fis <jm.yuan@outlook.com>
2022-01-26 18:06:09 +0800

    pragma

--- cpp/include/raft/detail/mdarray.hpp
+++ cpp/include/raft/detail/mdarray.hpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma once
 #include <experimental/mdspan>
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/device_uvector.hpp>
