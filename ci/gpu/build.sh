f434053f68f29898719f2dd2bb2f1bd812c7058b
Corey J. Nolet <cjnolet@gmail.com>
2022-02-04 16:56:08 -0500

    Removing additional libraft in gpu built d

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -96,9 +96,9 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 gpuci_logger "Build C++ and Python targets"
 # These should link against the existing shared libs
 if hasArg --skip-tests; then
-  "$WORKSPACE/build.sh" libraft pyraft libraft -v --nogtest
+  "$WORKSPACE/build.sh" pyraft libraft -v --nogtest
 else
-  "$WORKSPACE/build.sh" libraft pyraft libraft -v
+  "$WORKSPACE/build.sh" pyraft libraft -v
 fi
 
 gpuci_logger "sccache stats"
