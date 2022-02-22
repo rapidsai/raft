9a23e8417fead1c75fc1d97e26d3e5cd8880e0f8
akifcorduk <akifcorduk@gmail.com>
2022-02-22 00:54:09 -0800

    Use shfl_xor

--- cpp/include/raft/cuda_utils.cuh
+++ cpp/include/raft/cuda_utils.cuh
@@ -653,19 +653,18 @@ DI T shfl_xor(T val, int laneMask, int width = WarpSize, uint32_t mask = 0xfffff
  * @brief Warp-level sum reduction
  * @param val input value
  * @tparam T Value type to be reduced
- * @tparam Broadcast Boolean flag to broadcast reduction value to all lanes
- * @return only the lane0 will contain valid reduced result unless Broadcast is true
+ * @return Reduction result. All lanes will have the valid result.
  * @note Why not cub? Because cub doesn't seem to allow working with arbitrary
  *       number of warps in a block. All threads in the warp must enter this
  *       function together
  * @todo Expand this to support arbitrary reduction ops
  */
-template <typename T, bool Broadcast = false>
+template <typename T>
 DI T warpReduce(T val)
 {
 #pragma unroll
   for (int i = WarpSize / 2; i > 0; i >>= 1) {
-    T tmp = Broadcast ? shfl_xor(val, i) : shfl(val, laneId() + i);
+    T tmp = shfl_xor(val, i);
     val += tmp;
   }
   return val;
