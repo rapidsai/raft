7ec1100951c0b288688eb49c123aeed6dc6a7776
Corey J. Nolet <cjnolet@gmail.com>
2022-03-10 18:06:41 -0500

    Exorting rmm through raft-headers-exports

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -19,8 +19,8 @@ function(find_and_configure_rmm)
     include(${rapids-cmake-dir}/cpm/rmm.cmake)
     rapids_cpm_rmm(
         GLOBAL_TARGETS      rmm::rmm
-        BUILD_EXPORT_SET    raft-exports
-        INSTALL_EXPORT_SET  raft-exports
+        BUILD_EXPORT_SET    raft-headers-exports
+        INSTALL_EXPORT_SET  raft-headers-exports
         EXCLUDE_FROM_ALL TRUE
     )
 
