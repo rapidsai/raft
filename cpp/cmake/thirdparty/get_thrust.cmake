339b8b67afc11dae6d2d626f0acb4c904621ace8
Corey J. Nolet <cjnolet@gmail.com>
2022-03-10 20:11:38 -0500

    Adding headers to test-raft links

--- cpp/cmake/thirdparty/get_thrust.cmake
+++ cpp/cmake/thirdparty/get_thrust.cmake
@@ -16,11 +16,9 @@
 function(find_and_configure_thrust)
     include(${rapids-cmake-dir}/cpm/thrust.cmake)
 
-    rapids_cpm_thrust(
-            NAMESPACE raft
-            BUILD_EXPORT_SET raft-headers-exports
-            INSTALL_EXPORT_SET raft-headers-exports
-    )
+    rapids_cpm_thrust(NAMESPACE raft)
+    rapids_export_package(BUILD thrust raft-headers-exports)
+    rapids_export_package(INSTALL thrust raft-headers-exports)
 
 endfunction()
 
