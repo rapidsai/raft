e9fc98eb80a13f2f11843c59bd0eeac6556595ce
Corey J. Nolet <cjnolet@gmail.com>
2022-03-22 10:23:39 -0400

    install export set for mdspan and libcucxx

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -17,7 +17,6 @@
 function(find_and_configure_rmm)
     include(${rapids-cmake-dir}/cpm/rmm.cmake)
     rapids_cpm_rmm(BUILD_EXPORT_SET raft-exports)
-    rapids_cpm_rmm(INSTALL_EXPORT_SET raft-exports)
 endfunction()
 
 find_and_configure_rmm()
