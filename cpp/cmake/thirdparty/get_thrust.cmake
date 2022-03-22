2ff3536767fc36861d473db655750b7eb6449157
Corey J. Nolet <cjnolet@gmail.com>
2022-03-22 13:02:08 -0400

    Calling single thrust function instead of two

--- cpp/cmake/thirdparty/get_thrust.cmake
+++ cpp/cmake/thirdparty/get_thrust.cmake
@@ -16,9 +16,9 @@
 function(find_and_configure_thrust)
     include(${rapids-cmake-dir}/cpm/thrust.cmake)
 
-    rapids_cpm_thrust( NAMESPACE raft )
-    rapids_export_package(BUILD Thrust raft-exports)
-    rapids_export_package(INSTALL Thrust raft-exports)
+    rapids_cpm_thrust( NAMESPACE raft
+                       BUILD_EXPORT_SET raft-exports
+                       INSTALL_EXPORT_SET raft-exports)
 endfunction()
 
 find_and_configure_thrust()
