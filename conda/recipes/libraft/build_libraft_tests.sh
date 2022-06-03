b68004e96a8e3b71205566dd0a4ea539d8e5ccd4
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-06-03 13:00:48 +0200

    Fix libraft-tests install
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- conda/recipes/libraft/build_libraft_tests.sh
+++ conda/recipes/libraft/build_libraft_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022, NVIDIA CORPORATION.
 
-./build.sh tests --install -v --allgpuarch --no-nvtx
+./build.sh tests bench -v --allgpuarch --no-nvtx
+cmake --install cpp/build --component testing
