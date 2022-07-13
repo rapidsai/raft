1085fefdd085ed8e9d41bc523f3342eeb675c6d1
ayodeawe <jawe@nvidia.com>
2022-07-13 14:13:22 -0500

    debug

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -13,6 +13,15 @@ export CMAKE_GENERATOR="Ninja"
 # ucx-py version
 export UCX_PY_VERSION='0.27.*'
 
+export CUDAHOSTCXX=${CXX}
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
+
+if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
+  export VERSION_SUFFIX=$(date +%y%m%d)
+else
+  export VERSION_SUFFIX=""
+fi
+
 ################################################################################
 # BUILD - Conda package builds (LIBCUGRAPH)
 ################################################################################
