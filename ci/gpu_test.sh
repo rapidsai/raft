97d40d61be5c25bf80713d1f341be7af64d38ab1
ayodeawe <jawe@nvidia.com>
2022-07-14 05:08:14 -0500

    set cuda_rel

--- ci/gpu_test.sh
+++ ci/gpu_test.sh
@@ -16,7 +16,7 @@ function hasArg {
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
 export PARALLEL_LEVEL=${PARALLEL_LEVEL:-8}
-export CUDA_REL=${CUDA_VERSION%.*}
+export CUDA_REL=${CUDA%.*}
 
 # Set home to the job's workspace
 export HOME="$WORKSPACE"
