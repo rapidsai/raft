598f77b16ec48ae34b65490ac33028fb27961bfd
Allard Hendriksen <ahendriksen@nvidia.com>
2022-07-18 11:01:33 +0200

    sparse: Deprecate .hpp headers

--- cpp/include/raft/sparse/convert/csr.hpp
+++ cpp/include/raft/sparse/convert/csr.hpp
@@ -13,67 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * This file is deprecated and will be removed in release 22.06.
  * Please use the cuh version instead.
  */
 
-#ifndef __CSR_H
-#define __CSR_H
+/**
+ * DISCLAIMER: this file is deprecated: use csr.cuh instead
+ */
 
 #pragma once
 
-#include <raft/sparse/convert/detail/csr.cuh>
-#include <raft/sparse/csr.hpp>
+#pragma message(__FILE__                                                  \
+                " is deprecated and will be removed in a future release." \
+                " Please use the cuh version instead.")
 
-namespace raft {
-namespace sparse {
-namespace convert {
-
-template <typename value_t>
-void coo_to_csr(const raft::handle_t& handle,
-                const int* srcRows,
-                const int* srcCols,
-                const value_t* srcVals,
-                int nnz,
-                int m,
-                int* dst_offsets,
-                int* dstCols,
-                value_t* dstVals)
-{
-  detail::coo_to_csr(handle, srcRows, srcCols, srcVals, nnz, m, dst_offsets, dstCols, dstVals);
-}
-
-/**
- * @brief Generate the row indices array for a sorted COO matrix
- *
- * @param rows: COO rows array
- * @param nnz: size of COO rows array
- * @param row_ind: output row indices array
- * @param m: number of rows in dense matrix
- * @param stream: cuda stream to use
- */
-template <typename T>
-void sorted_coo_to_csr(const T* rows, int nnz, T* row_ind, int m, cudaStream_t stream)
-{
-  detail::sorted_coo_to_csr(rows, nnz, row_ind, m, stream);
-}
-
-/**
- * @brief Generate the row indices array for a sorted COO matrix
- *
- * @param coo: Input COO matrix
- * @param row_ind: output row indices array
- * @param stream: cuda stream to use
- */
-template <typename T>
-void sorted_coo_to_csr(COO<T>* coo, int* row_ind, cudaStream_t stream)
-{
-  detail::sorted_coo_to_csr(coo->rows(), coo->nnz, row_ind, coo->n_rows, stream);
-}
-
-};  // end NAMESPACE convert
-};  // end NAMESPACE sparse
-};  // end NAMESPACE raft
-
-#endif
+#include "csr.cuh"
