7f640a98217eca04bfc8c1b87feb998232464697
achirkin <achirkin@users.noreply.github.com>
2022-07-21 09:13:04 +0200

    Improve the docs

--- cpp/include/raft/spatial/knn/ivf_flat.cuh
+++ cpp/include/raft/spatial/knn/ivf_flat.cuh
@@ -31,9 +31,22 @@ namespace raft::spatial::knn::ivf_flat {
  * @brief Build the index from the dataset for efficient search.
  *
  * NB: Currently, the following distance metrics are supported:
- *   L2Expanded
- *   L2Unexpanded
- *   InnerProduct
+ * - L2Expanded
+ * - L2Unexpanded
+ * - InnerProduct
+ *
+ * Usage example:
+ * @code{.cpp}
+ *   using namespace raft::spatial::knn;
+ *   // use default index parameters
+ *   ivf_flat::index_params index_params;
+ *   // create and fill the index from a [N, D] dataset
+ *   auto index = ivf_flat::build(handle, index_params, dataset, N, D);
+ *   // use default search parameters
+ *   ivf_flat::search_params search_params;
+ *   // search K nearest neighbours for each of the N queries
+ *   ivf_flat::search(handle, search_params, index, queries, N, K, out_inds, out_dists);
+ * @endcode
  *
  * @tparam T data element type
  * @tparam IdxT type of the indices in the source dataset
@@ -62,13 +75,27 @@ inline auto build(
  *    The new data is clustered according to existing kmeans clusters, then the cluster
  *    centers are adjusted to match the newly labeled data.
  *
+ * Usage example:
+ * @code{.cpp}
+ *   using namespace raft::spatial::knn;
+ *   ivf_flat::index_params index_params;
+ *   index_params.add_data_on_build = false;      // don't populate index on build
+ *   index_params.kmeans_trainset_fraction = 1.0; // use whole dataset for kmeans training
+ *   // train the index from a [N, D] dataset
+ *   auto index_empty = ivf_flat::build(handle, index_params, dataset, N, D);
+ *   // fill the index with the data
+ *   auto index = ivf_flat::extend(handle, index_empty, dataset, nullptr, N);
+ * @endcode
+ *
  * @tparam T data element type
  * @tparam IdxT type of the indices in the source dataset
  *
  * @param handle
- * @param index original index
+ * @param orig_index original index
  * @param[in] new_vectors a device pointer to a row-major matrix [n_rows, index.dim()]
- * @param[in] new_indices a device pointer to a vector of indices [n_rows]
+ * @param[in] new_indices a device pointer to a vector of indices [n_rows].
+ *    If the original index is empty (`orig_index.size() == 0`), you can pass `nullptr`
+ *    here to imply a continuous range `[0...n_rows)`.
  * @param n_rows the number of samples
  *
  * @return the constructed extended ivf-flat index
@@ -86,6 +113,8 @@ inline auto extend(const handle_t& handle,
 
 /**
  * @brief Search ANN using the constructed index.
+ *
+ * See the [ivf_flat::build](#ivf_flat::build) documentation for a usage example.
  *
  * @tparam T data element type
  * @tparam IdxT type of the indices
