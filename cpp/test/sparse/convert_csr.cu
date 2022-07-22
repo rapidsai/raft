362f91c2113ec73763fe87aaef2ebcfcd71b2026
Allard Hendriksen <hendriksen.a.a@gmail.com>
2022-07-22 16:46:01 +0200

    Replace csr_adj_graph functions with faster equivalent (#746)
    The `csr_adj_graph` functions are a performance bottleneck in the DBSCAN implementation in cuML. They are not used anywhere  else.
    
    This PR replaces the `csr_adj_graph` functions with the faster `dense_bool_to_unsorted_csr` function. It has the same functionality, *but*
    
    1. It requires the input adjacency matrix to be in row-major order (rather than column-major).
    2. The output column indices are not guaranteed to be in ascending order (hence unsorted).
    
    Authors:
      - Allard Hendriksen (https://github.com/ahendriksen)
    
    Approvers:
      - Tamas Bela Feher (https://github.com/tfeher)
    
    URL: https://github.com/rapidsai/raft/pull/746

--- cpp/test/sparse/convert_csr.cu
+++ cpp/test/sparse/convert_csr.cu
@@ -16,7 +16,7 @@
 
 #include "../test_utils.h"
 #include <gtest/gtest.h>
-#include <raft/cudart_utils.h>
+#include <raft/cuda_utils.cuh>
 
 #include <raft/sparse/convert/csr.cuh>
 #include <raft/sparse/coo.hpp>
@@ -88,55 +88,108 @@ INSTANTIATE_TEST_CASE_P(SparseConvertCSRTest, SortedCOOToCSR, ::testing::ValuesI
 
 /******************************** adj graph ********************************/
 
-template <typename Index_>
+template <typename index_t>
+__global__ void init_adj_kernel(bool* adj, index_t num_rows, index_t num_cols, index_t divisor)
+{
+  index_t r = blockDim.y * blockIdx.y + threadIdx.y;
+  index_t c = blockDim.x * blockIdx.x + threadIdx.x;
+
+  for (; r < num_rows; r += gridDim.y * blockDim.y) {
+    for (; c < num_cols; c += gridDim.x * blockDim.x) {
+      adj[r * num_cols + c] = c % divisor == 0;
+    }
+  }
+}
+
+template <typename index_t>
+void init_adj(bool* adj, index_t num_rows, index_t num_cols, index_t divisor, cudaStream_t stream)
+{
+  // adj matrix: element a_ij is set to one if j is divisible by divisor.
+  dim3 block(32, 32);
+  const index_t max_y_grid_dim = 65535;
+  dim3 grid(num_cols / 32 + 1, (int)min(num_rows / 32 + 1, max_y_grid_dim));
+  init_adj_kernel<index_t><<<grid, block, 0, stream>>>(adj, num_rows, num_cols, divisor);
+  RAFT_CHECK_CUDA(stream);
+}
+
+template <typename index_t>
 struct CSRAdjGraphInputs {
-  Index_ n_rows;
-  Index_ n_cols;
-  std::vector<Index_> row_ind;
-  std::vector<uint8_t> adj;  // To avoid vector<bool> optimization
-  std::vector<Index_> verify;
+  index_t n_rows;
+  index_t n_cols;
+  index_t divisor;
 };
 
-template <typename Index_>
-class CSRAdjGraphTest : public ::testing::TestWithParam<CSRAdjGraphInputs<Index_>> {
+template <typename index_t>
+class CSRAdjGraphTest : public ::testing::TestWithParam<CSRAdjGraphInputs<index_t>> {
  public:
   CSRAdjGraphTest()
-    : params(::testing::TestWithParam<CSRAdjGraphInputs<Index_>>::GetParam()),
-      stream(handle.get_stream()),
-      row_ind(params.n_rows, stream),
+    : stream(handle.get_stream()),
+      params(::testing::TestWithParam<CSRAdjGraphInputs<index_t>>::GetParam()),
       adj(params.n_rows * params.n_cols, stream),
-      result(params.verify.size(), stream),
-      verify(params.verify.size(), stream)
+      row_ind(params.n_rows, stream),
+      row_counters(params.n_rows, stream),
+      col_ind(params.n_rows * params.n_cols, stream),
+      row_ind_host(params.n_rows)
   {
   }
 
  protected:
-  void SetUp() override { nnz = params.verify.size(); }
+  void SetUp() override
+  {
+    // Initialize adj matrix: element a_ij equals one if j is divisible by
+    // params.divisor.
+    init_adj(adj.data(), params.n_rows, params.n_cols, params.divisor, stream);
+    // Initialize row_ind
+    for (size_t i = 0; i < row_ind_host.size(); ++i) {
+      size_t nnz_per_row = raft::ceildiv(params.n_cols, params.divisor);
+      row_ind_host[i]    = nnz_per_row * i;
+    }
+    raft::update_device(row_ind.data(), row_ind_host.data(), row_ind.size(), stream);
+
+    // Initialize result to 1, so we can catch any errors.
+    RAFT_CUDA_TRY(cudaMemsetAsync(col_ind.data(), 1, col_ind.size() * sizeof(index_t), stream));
+  }
 
   void Run()
   {
-    raft::update_device(row_ind.data(), params.row_ind.data(), params.n_rows, stream);
-    raft::update_device(adj.data(),
-                        reinterpret_cast<bool*>(params.adj.data()),
-                        params.n_rows * params.n_cols,
-                        stream);
-    raft::update_device(verify.data(), params.verify.data(), nnz, stream);
+    convert::adj_to_csr<index_t>(handle,
+                                 adj.data(),
+                                 row_ind.data(),
+                                 params.n_rows,
+                                 params.n_cols,
+                                 row_counters.data(),
+                                 col_ind.data());
 
-    convert::csr_adj_graph_batched<Index_>(
-      row_ind.data(), params.n_cols, nnz, params.n_rows, adj.data(), result.data(), stream);
+    std::vector<index_t> col_ind_host(col_ind.size());
+    raft::update_host(col_ind_host.data(), col_ind.data(), col_ind.size(), stream);
+    std::vector<index_t> row_counters_host(params.n_rows);
+    raft::update_host(row_counters_host.data(), row_counters.data(), row_counters.size(), stream);
+    RAFT_CUDA_TRY(cudaStreamSynchronize(stream));
 
-    ASSERT_TRUE(
-      raft::devArrMatch<Index_>(verify.data(), result.data(), nnz, raft::Compare<Index_>()));
+    // 1. Check that each row contains enough values
+    index_t nnz_per_row = raft::ceildiv(params.n_cols, params.divisor);
+    for (index_t i = 0; i < params.n_rows; ++i) {
+      ASSERT_EQ(row_counters_host[i], nnz_per_row) << "where i = " << i;
+    }
+    // 2. Check that all column indices are divisble by divisor
+    for (index_t i = 0; i < params.n_rows; ++i) {
+      index_t row_base = row_ind_host[i];
+      for (index_t j = 0; j < nnz_per_row; ++j) {
+        ASSERT_EQ(0, col_ind_host[row_base + j] % params.divisor);
+      }
+    }
   }
 
  protected:
   raft::handle_t handle;
   cudaStream_t stream;
 
-  CSRAdjGraphInputs<Index_> params;
-  Index_ nnz;
-  rmm::device_uvector<Index_> row_ind, result, verify;
+  CSRAdjGraphInputs<index_t> params;
   rmm::device_uvector<bool> adj;
+  rmm::device_uvector<index_t> row_ind;
+  rmm::device_uvector<index_t> row_counters;
+  rmm::device_uvector<index_t> col_ind;
+  std::vector<index_t> row_ind_host;
 };
 
 using CSRAdjGraphTestI = CSRAdjGraphTest<int>;
@@ -145,19 +198,15 @@ TEST_P(CSRAdjGraphTestI, Result) { Run(); }
 using CSRAdjGraphTestL = CSRAdjGraphTest<int64_t>;
 TEST_P(CSRAdjGraphTestL, Result) { Run(); }
 
-const std::vector<CSRAdjGraphInputs<int>> csradjgraph_inputs_i = {
-  {3,
-   6,
-   {0, 3, 6},
-   {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 1, 2, 0, 1, 2, 0, 1, 2}},
-};
+const std::vector<CSRAdjGraphInputs<int>> csradjgraph_inputs_i     = {{10, 10, 2}};
 const std::vector<CSRAdjGraphInputs<int64_t>> csradjgraph_inputs_l = {
-  {3,
-   6,
-   {0, 3, 6},
-   {1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-   {0, 1, 2, 0, 1, 2, 0, 1, 2}},
+  {0, 0, 2},
+  {10, 10, 2},
+  {64 * 1024 + 10, 2, 3},  // 64K + 10 is slightly over maximum of blockDim.y
+  {16, 16, 3},             // No peeling-remainder
+  {17, 16, 3},             // Check peeling-remainder
+  {18, 16, 3},             // Check peeling-remainder
+  {32 + 9, 33, 2},         // Check peeling-remainder
 };
 
 INSTANTIATE_TEST_CASE_P(SparseConvertCSRTest,
