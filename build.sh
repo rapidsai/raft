374c91c28cf8a54654fd41b405f46ab265f36c76
Corey J. Nolet <cjnolet@gmail.com>
2022-10-06 06:21:02 -0400

    Reverting changes to build.sh

--- build.sh
+++ build.sh
@@ -322,7 +322,7 @@ if (( ${NUMARGS} == 0 )) || hasArg raft-dask || hasArg docs; then
 fi
 
 # Build and (optionally) install the pylibraft Python package
-if (( ${NUMARGS} == 0 )) || hasArg pylibraft || hasArg docs; then
+if (( ${NUMARGS} == 0 )) || hasArg pylibraft; then
 
     cd ${REPODIR}/python/pylibraft
     python setup.py build_ext --inplace -- -DCMAKE_PREFIX_PATH="${LIBRAFT_BUILD_DIR};${INSTALL_PREFIX}" -DCMAKE_LIBRARY_PATH=${LIBRAFT_BUILD_DIR} ${EXTRA_CMAKE_ARGS} -- -j${PARALLEL_LEVEL:-1}
