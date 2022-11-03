2a63aff614a0d7f417144d0a13bfa9acaeefface
William Hicks <whicks@nvidia.com>
2022-11-03 14:28:41 -0400

    Provide memory_type enum
    Provide an enum class used to distinguish among various memory types.
    This allows decoupling of the concept of memory type from a specific
    mdspan accessor policy and ensures that templates receive only a
    logically-consistent memory type.

---
+++ cpp/include/raft/core/memory_type.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+namespace raft {
+enum class memory_type {
+  host,
+  device,
+  managed,
+  pinned
+};
+
+auto constexpr is_device_accessible(memory_type mem_type) {
+  return (mem_type == memory_type::device || mem_type == memory_type::managed);
+}
+auto constexpr is_host_accessible(memory_type mem_type) {
+  return (mem_type == memory_type::host || mem_type == memory_type::managed || mem_type == memory_type::pinned);
+}
+auto constexpr is_host_device_accessible(memory_type mem_type) {
+  return mem_type == memory_type::managed;
+}
+
+}  // end namespace raft
