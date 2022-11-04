79334361caae2279e8a30315c018be96fb87859b
Mahesh Doijade <mdoijade@nvidia.com>
2022-11-04 15:09:11 +0530

    fix bugs in get_cutlass cmake to use cutlass provided properties correctly

--- cpp/cmake/thirdparty/get_cutlass.cmake
+++ cpp/cmake/thirdparty/get_cutlass.cmake
@@ -20,58 +20,47 @@ function(find_and_configure_cutlass)
                           "${multiValueArgs}" ${ARGN} )
 
     if(RAFT_ENABLE_DIST_DEPENDENCIES OR RAFT_COMPILE_LIBRARIES)
-      rapids_find_generate_module(cutlass
-          HEADER_NAMES  cutlass/include/*
-          INCLUDE_SUFFIXES cutlass
-      )
-      set(CUTLASS_ENABLE_HEADERS_ONLY ON)
-      set(RAFT_CUTLASS_NAMESPACE "raft_cutlass" CACHE STRING "Top level namespace of CUTLASS")
-#      set(BUILD_SHARED_LIBS OFF)
-      # if (PKG_BUILD_STATIC_LIBS)
-      #   set(BUILD_SHARED_LIBS OFF)
-      #   set(CPM_DOWNLOAD_cutlass ON)
-      # endif()
+      set(CUTLASS_ENABLE_HEADERS_ONLY ON CACHE BOOL "Enable only the header library")
+      set(CUTLASS_NAMESPACE "raft_cutlass" CACHE STRING "Top level namespace of CUTLASS")
 
       rapids_cpm_find(cutlass ${PKG_VERSION}
-          GLOBAL_TARGETS     cutlass::cutlass
+          GLOBAL_TARGETS     nvidia::cutlass::CUTLASS
           CPM_ARGS
             GIT_REPOSITORY   ${PKG_REPOSITORY}
             GIT_TAG          ${PKG_PINNED_TAG}
-            EXCLUDE_FROM_ALL ${PKG_EXCLUDE_FROM_ALL}
             OPTIONS
-              "CMAKE_INSTALL_INCLUDEDIR include"
               "CUDAToolkit_ROOT ${CUDAToolkit_LIBRARY_DIR}"
       )
 
-      if(TARGET cutlass AND NOT TARGET cutlass::cutlass)
-          add_library(cutlass::cutlass ALIAS cutlass)
+      if(TARGET cutlass AND NOT TARGET nvidia::cutlass::cutlass)
+          add_library(nvidia::cutlass::cutlass ALIAS CUTLASS)
       endif()
 
-      # if(cutlass_ADDED)
-      #   rapids_export(BUILD cutlass
-      #       EXPORT_SET cutlass-targets
-      #       GLOBAL_TARGETS cutlass
-      #       NAMESPACE cutlass::)
-      # endif()
+      if(cutlass_ADDED)
+        rapids_export(BUILD cutlass
+            EXPORT_SET NvidiaCutlass
+            GLOBAL_TARGETS nvidia::cutlass::CUTLASS
+            NAMESPACE nvidia::cutlass::)
+      endif()
     endif()
 
-    # We generate the faiss-config files when we built faiss locally, so always do `find_dependency`
-    rapids_export_package(BUILD cutlass raft-distance-lib-exports GLOBAL_TARGETS cutlass::cutlass cutlass)
-    rapids_export_package(INSTALL cutlass raft-distance-lib-exports GLOBAL_TARGETS cutlass::cutlass cutlass)
+    # We generate the cutlass-config files when we built cutlass locally, so always do `find_dependency`
+    rapids_export_package(BUILD cutlass raft-distance-exports GLOBAL_TARGETS nvidia::cutlass::CUTLASS)
+    rapids_export_package(INSTALL cutlass raft-distance-exports GLOBAL_TARGETS nvidia::cutlass::CUTLASS)
 
     # Tell cmake where it can find the generated cutlass-config.cmake we wrote.
     include("${rapids-cmake-dir}/export/find_package_root.cmake")
-    rapids_export_find_package_root(INSTALL cutlass [=[${CMAKE_CURRENT_LIST_DIR}]=] raft-distance-lib-exports)
+    rapids_export_find_package_root(INSTALL cutlass [=[${CMAKE_CURRENT_LIST_DIR}]=] raft-distance-exports)
 endfunction()
 
 if(NOT RAFT_CUTLASS_GIT_TAG)
-  set(RAFT_CUTLASS_GIT_TAG v2.9.0)
+  set(RAFT_CUTLASS_GIT_TAG v2.9.1)
 endif()
 
 if(NOT RAFT_CUTLASS_GIT_REPOSITORY)
   set(RAFT_CUTLASS_GIT_REPOSITORY https://github.com/NVIDIA/cutlass.git)
 endif()
 
-find_and_configure_cutlass(VERSION    2.9.0
+find_and_configure_cutlass(VERSION    2.9.1
                          REPOSITORY  ${RAFT_CUTLASS_GIT_REPOSITORY}
                          PINNED_TAG  ${RAFT_CUTLASS_GIT_TAG})
