f5c4a00e3ff832c311be5dd7309dd2a5af82a28d
Corey J. Nolet <cjnolet@gmail.com>
2022-11-10 17:33:01 -0500

    Updating docs for device_ndarray

--- python/pylibraft/pylibraft/common/device_ndarray.py
+++ python/pylibraft/pylibraft/common/device_ndarray.py
@@ -14,17 +14,23 @@
 #
 
 import numpy as np
+
 import rmm
 
+
 class device_ndarray:
+    """
+    raft.device_ndarray is meant to be a very lightweight
+    __cuda_array_interface__ wrapper around a numpy.ndarray.
+    """
 
     def __init__(self, np_ndarray):
         """
-        Construct a raft.device_ndarray wrapper around a numpy.ndarray (on host)
+        Construct a raft.device_ndarray wrapper around a numpy.ndarray
 
         Parameters
         ----------
-        ndarray : Any array that provides a valid __array_interface__ or __cuda_array_interface__
+        ndarray : A numpy.ndarray which will be copied and moved to the device
         """
         self.ndarray_ = np_ndarray
         order = "C" if self.c_contiguous else "F"
@@ -34,33 +40,58 @@ class device_ndarray:
 
     @property
     def c_contiguous(self):
+        """
+        Is the current device_ndarray laid out in row-major format?
+        """
         array_interface = self.ndarray_.__array_interface__
         strides = self.strides
-        return strides is None or \
-               array_interface["strides"][1] == self.dtype.itemsize
+        return (
+            strides is None
+            or array_interface["strides"][1] == self.dtype.itemsize
+        )
 
     @property
     def f_contiguous(self):
+        """
+        Is the current device_ndarray laid out in column-major format?
+        """
         return not self.c_contiguous
 
     @property
     def dtype(self):
+        """
+        Datatype of the current device_ndarray instance
+        """
         array_interface = self.ndarray_.__array_interface__
         return np.dtype(array_interface["typestr"])
 
     @property
     def shape(self):
+        """
+        Shape of the current device_ndarray instance
+        """
         array_interface = self.ndarray_.__array_interface__
         return array_interface["shape"]
 
     @property
     def strides(self):
+        """
+        Strides of the current device_ndarray instance
+        """
         array_interface = self.ndarray_.__array_interface__
-        return None if "strides" not in array_interface else \
-            array_interface["strides"]
+        return (
+            None
+            if "strides" not in array_interface
+            else array_interface["strides"]
+        )
 
     @property
     def __cuda_array_interface__(self):
+        """
+        Returns the __cuda_array_interface__ compliant dict for
+        integrating with other device-enabled libraries using
+        zero-copy semantics.
+        """
         device_cai = self.device_buffer_.__cuda_array_interface__
         host_cai = self.ndarray_.__array_interface__.copy()
         host_cai["data"] = (device_cai["data"][0], device_cai["data"][1])
@@ -68,13 +99,14 @@ class device_ndarray:
         return host_cai
 
     def copy_to_host(self):
-        ret = (
-            np.frombuffer(
-                self.device_buffer_.tobytes(),
-                dtype=self.dtype,
-                like=self.ndarray_,
-            )
-            .astype(self.dtype)
-        )
+        """
+        Returns a new numpy.ndarray object on host with the current contents of
+        this device_ndarray
+        """
+        ret = np.frombuffer(
+            self.device_buffer_.tobytes(),
+            dtype=self.dtype,
+            like=self.ndarray_,
+        ).astype(self.dtype)
         ret = np.lib.stride_tricks.as_strided(ret, self.shape, self.strides)
         return ret
