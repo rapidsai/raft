1224ffdfa62b203fa563f499c92172e8ecb5302e
Bradley Dice <bdice@bradleydice.com>
2022-12-07 14:52:45 -0600

    Add GitHub Actions Workflows.

---
+++ ci/build_python.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) 2022, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+source rapids-env-update
+
+export CMAKE_GENERATOR=Ninja
+
+rapids-print-env
+
+rapids-logger "Begin py build"
+
+CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+
+# TODO: Remove `--no-test` flag once importing on a CPU
+# node works correctly
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  conda/recipes/pylibraft
+
+# TODO: Remove `--no-test` flag once importing on a CPU
+# node works correctly
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  conda/recipes/raft-dask
+
+rapids-upload-conda-to-s3 python
