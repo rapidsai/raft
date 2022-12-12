e8393b750e609bee565d8d65500c23c311e73750
Bradley Dice <bdice@bradleydice.com>
2022-12-12 14:33:35 -0600

    Use ucx_py_version in conda_build_config.yaml.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -7,16 +7,6 @@ source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
 
-# If nightly build, append current YYMMDD to version
-if [[ "$RAPIDS_BUILD_TYPE" = "nightly" && "$RAPIDS_REF_NAME" = branch-* ]] ; then
-  export VERSION_SUFFIX=$(date +%y%m%d)
-else
-  export VERSION_SUFFIX=""
-fi
-
-# ucx-py version
-export UCX_PY_VERSION='0.30.*'
-
 rapids-print-env
 
 rapids-logger "Begin cpp build"
