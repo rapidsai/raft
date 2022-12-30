39ce0eaad5a873430ca7277b72fbb062e028e959
Mickael Ide <mide@nvidia.com>
2022-12-30 16:43:15 +0100

    Remove reconstruction test

--- cpp/test/linalg/randomized_svd.cu
+++ cpp/test/linalg/randomized_svd.cu
@@ -186,58 +186,10 @@ class randomized_svdTest : public ::testing::TestWithParam<randomized_svdInputs<
     handle.sync_stream(stream);
   }
 
-  void advancedTest()
-  {
-    int len    = params.n_row * params.n_col;
-    T data_h[] = {0.42120356, 0.55346701, 0.58788903, 0.75040157, 0.09853688, 0.49730508,
-                  0.15003893, 0.67740912, 0.12597932, 0.55363214, 0.40739539, 0.04186442,
-                  0.35645475, 0.13316199, 0.10088794, 0.39135527, 0.14173856, 0.11158198,
-                  0.78597058, 0.5228312,  0.1176523,  0.40416425, 0.18799533, 0.73968831,
-                  0.98123824, 0.82342543, 0.51029349, 0.43759839, 0.74817398, 0.82807957,
-                  0.94418196, 0.84631003, 0.88368781, 0.70672518, 0.64339536, 0.26589284,
-                  0.32476141, 0.93004274, 0.23253774, 0.64376609, 0.75940952, 0.79519889,
-                  0.14765252, 0.99161529, 0.82875801, 0.18182914, 0.22672471, 0.38118221,
-                  0.48865348, 0.24939353};
-    raft::update_device(data.data(), data_h, len, stream);
-
-    randomized_svd(handle,
-                   raft::make_device_matrix_view<const T, uint64_t, raft::col_major>(
-                     data.data(), params.n_row, params.n_col),
-                   raft::make_device_vector_view<T, uint64_t>(sing_vals_act.data(), params.k),
-                   std::make_optional(raft::make_device_matrix_view<T, uint64_t, raft::col_major>(
-                     left_eig_vectors_act.data(), params.n_row, params.k)),
-                   std::make_optional(raft::make_device_matrix_view<T, uint64_t, raft::col_major>(
-                     right_eig_vectors_act.data(), params.k, params.n_col)),
-                   params.k,
-                   1,
-                   2);
-
-    auto diag = raft::make_device_matrix<T, uint64_t, raft::col_major>(handle, params.k, params.k);
-    raft::matrix::set_diagonal(
-      handle,
-      raft::make_device_vector_view<const T, uint64_t>(sing_vals_act.data(), params.k),
-      diag.view());
-    raft::linalg::svd_reconstruction(
-      handle,
-      raft::make_device_matrix_view<const T, uint64_t, raft::col_major>(
-        left_eig_vectors_act.data(), params.n_row, params.k),
-      raft::make_device_matrix_view<const T, uint64_t, raft::col_major>(
-        diag.data_handle(), params.k, params.k),
-      raft::make_device_matrix_view<const T, uint64_t, raft::col_major>(
-        right_eig_vectors_act.data(), params.k, params.n_col),
-      raft::make_device_matrix_view<T, uint64_t, raft::col_major>(
-        reconst.data(), params.n_row, params.n_col));
-    handle.sync_stream(stream);
-  }
-
   void SetUp() override
   {
-    if (params.n_row == 5 && params.n_col == 5) {
-      apiTest();
-      basicTest();
-    } else {
-      advancedTest();
-    }
+    apiTest();
+    basicTest();
   }
 
  protected:
@@ -251,10 +203,6 @@ class randomized_svdTest : public ::testing::TestWithParam<randomized_svdInputs<
 
 const std::vector<randomized_svdInputs<float>> inputsf1  = {{0.0001f, 5, 5, 2, 1234ULL}};
 const std::vector<randomized_svdInputs<double>> inputsd1 = {{0.0001, 5, 5, 2, 1234ULL}};
-const std::vector<randomized_svdInputs<float>> inputsf2  = {{0.5f, 10, 5, 3, 1234ULL}};
-const std::vector<randomized_svdInputs<double>> inputsd2 = {{0.5, 10, 5, 3, 1234ULL}};
-const std::vector<randomized_svdInputs<float>> inputsf3  = {{0.5f, 5, 10, 2, 1234ULL}};
-const std::vector<randomized_svdInputs<double>> inputsd3 = {{0.5, 5, 10, 2, 1234ULL}};
 
 typedef randomized_svdTest<float> randomized_svdTestF;
 TEST_P(randomized_svdTestF, Result)
@@ -290,37 +238,7 @@ TEST_P(randomized_svdTestD, Result)
                                 raft::CompareApproxAbs<double>(params.tolerance)));
 }
 
-typedef randomized_svdTest<float> randomized_svdTestReconstructionF;
-TEST_P(randomized_svdTestReconstructionF, Result)
-{
-  ASSERT_TRUE(devArrMatch(data.data(),
-                          reconst.data(),
-                          params.n_row * params.n_col,
-                          raft::CompareApproxAbs<float>(params.tolerance)));
-}
-
-typedef randomized_svdTest<double> randomized_svdTestReconstructionD;
-TEST_P(randomized_svdTestReconstructionD, Result)
-{
-  ASSERT_TRUE(devArrMatch(data.data(),
-                          reconst.data(),
-                          params.n_row * params.n_col,
-                          raft::CompareApproxAbs<double>(params.tolerance)));
-}
-
 INSTANTIATE_TEST_SUITE_P(randomized_svdTests1, randomized_svdTestF, ::testing::ValuesIn(inputsf1));
 INSTANTIATE_TEST_SUITE_P(randomized_svdTests1, randomized_svdTestD, ::testing::ValuesIn(inputsd1));
-INSTANTIATE_TEST_SUITE_P(randomized_svdTests2,
-                         randomized_svdTestReconstructionF,
-                         ::testing::ValuesIn(inputsf2));
-INSTANTIATE_TEST_SUITE_P(randomized_svdTests2,
-                         randomized_svdTestReconstructionD,
-                         ::testing::ValuesIn(inputsd2));
-INSTANTIATE_TEST_SUITE_P(randomized_svdTests3,
-                         randomized_svdTestReconstructionF,
-                         ::testing::ValuesIn(inputsf3));
-INSTANTIATE_TEST_SUITE_P(randomized_svdTests3,
-                         randomized_svdTestReconstructionD,
-                         ::testing::ValuesIn(inputsd3));
 }  // end namespace linalg
 }  // end namespace raft
