dff50ad6fb1a2027940d2627cc4a406ea83217e4
Ben Frederickson <github@benfrederickson.com>
2023-01-05 10:08:26 -0800

    Add faiss license file

--- cpp/include/raft/spatial/knn/detail/faiss_select/MergeNetworkWarp.cuh
+++ cpp/include/raft/spatial/knn/detail/faiss_select/MergeNetworkWarp.cuh
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file thirdparty/LICENSES/LICENSE.faiss
  */
 
 #pragma once
