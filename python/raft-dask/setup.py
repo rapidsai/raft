ca3f91d7802fd2948206677c7703e9a97391547f
Vyas Ramasubramani <vyasr@nvidia.com>
2023-01-10 22:01:15 -0800

    Remove dask-cuda pinning.

--- python/raft-dask/setup.py
+++ python/raft-dask/setup.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020-2022, NVIDIA CORPORATION.
+# Copyright (c) 2020-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ install_requires = [
     "numpy",
     "numba>=0.49",
     "joblib>=0.11",
-    "dask-cuda>=23.02",
+    "dask-cuda",
     "dask>=2022.12.0",
     f"ucx-py{cuda_suffix}",
     "distributed>=2022.12.0",
