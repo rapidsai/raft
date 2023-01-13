264a9d27703b53fb6aed9a9fad2cbb9d70a011ee
Allard Hendriksen <ahendriksen@nvidia.com>
2023-01-13 11:58:43 +0100

    l1: Replace run-time -> compile-time dispatch

--- cpp/include/raft/distance/detail/l1.cuh
+++ cpp/include/raft/distance/detail/l1.cuh
@@ -15,182 +15,156 @@
  */
 
 #pragma once
+#include <raft/distance/detail/distance_operators.cuh>
 #include <raft/distance/detail/pairwise_distance_base.cuh>
 #include <raft/distance/detail/pairwise_distance_op.cuh>
-#include <raft/distance/detail/distance_operators.cuh>
 
 namespace raft {
 namespace distance {
 namespace detail {
 
-
 template <typename PCT>
-static void distance_matrix_launch(
-    typename PCT::opT distance_op,
-    typename PCT::FinOpT fin_op,
-    const typename PCT::DataT* x,
-    const typename PCT::DataT* y,
-    const typename PCT::DataT* _xn,
-    const typename PCT::DataT* _yn,
-    typename PCT::IdxT m,
-    typename PCT::IdxT n,
-    typename PCT::IdxT k,
-    typename PCT::IdxT lda,
-    typename PCT::IdxT ldb,
-    typename PCT::IdxT ldd,
-    typename PCT::OutT* dOutput,
-    cudaStream_t stream)
+static void distance_matrix_launch(typename PCT::opT distance_op,
+                                   typename PCT::FinOpT fin_op,
+                                   const typename PCT::DataT* x,
+                                   const typename PCT::DataT* y,
+                                   const typename PCT::DataT* _xn,
+                                   const typename PCT::DataT* _yn,
+                                   typename PCT::IdxT m,
+                                   typename PCT::IdxT n,
+                                   typename PCT::IdxT k,
+                                   typename PCT::IdxT lda,
+                                   typename PCT::IdxT ldb,
+                                   typename PCT::IdxT ldd,
+                                   typename PCT::OutT* dOutput,
+                                   cudaStream_t stream)
 {
   using Policy = typename PCT::PolicyT;
 
   dim3 blk(Policy::Nthreads);
   size_t smem_size = distance_op.template shared_mem_size<Policy>();
-  dim3 grid       = launchConfigGenerator<Policy>(m, n, smem_size, pairwiseDistanceOpKernel<PCT>);
+  dim3 grid        = launchConfigGenerator<Policy>(m, n, smem_size, pairwiseDistanceOpKernel<PCT>);
 
   pairwiseDistanceOpKernel<PCT><<<grid, blk, smem_size, stream>>>(
     x, y, _xn, _yn, m, n, k, lda, ldb, ldd, dOutput, distance_op, fin_op);
 
   RAFT_CUDA_TRY(cudaGetLastError());
-
 }
 
-/**
- * @brief the L1 distance matrix calculation implementer
- *  It computes the following equation: cij = op(ai-bj)
- * @tparam DataT          input data-type (for A and B matrices)
- * @tparam AccT           accumulation data-type
- * @tparam OutT           output data-type (for C and D matrices)
- * @tparam IdxT           index data-type
-
- * @tparam FinalLambda    final lambda called on final distance value
- * @tparam isRowMajor     true if input/output is row major,
-                          false for column major
- * @param[in]       x input matrix
- * @param[in]       y input matrix
- * @param[in]       m number of rows of A and C/D
- * @param[in]       n number of columns of B and C/D
- * @param[in]       k number of cols of A and rows of B
- * @param[in]       lda leading dimension of A
- * @param[in]       ldb leading dimension of B
- * @param[in]       ldd leading dimension of C/D
- * @param[output]   pD output matrix
- * @param fin_op    the final gemm epilogue lambda
- */
-template <typename DataT,
-          typename AccT,
-          typename OutT,
-          typename IdxT,
-          int VecLen,
-          typename FinalLambda,
-          bool isRowMajor>
-static void l1Impl(const DataT* x,
-                   const DataT* y,
-                   IdxT m,
-                   IdxT n,
-                   IdxT k,
-                   IdxT lda,
-                   IdxT ldb,
-                   IdxT ldd,
-                   OutT* dOutput,
-                   FinalLambda fin_op,
-                   cudaStream_t stream)
+// Determine the largest number of elements that can be loaded in one
+// instruction without causing misalignment errors.
+template <typename DataT>
+int max_aligned_load(const DataT* x, const DataT* y, int ldx, int ldy)
 {
-  typedef typename raft::linalg::Policy4x4<DataT, VecLen>::Policy RowPolicy;
-  typedef typename raft::linalg::Policy4x4<DataT, VecLen>::ColPolicy ColPolicy;
-  typedef typename std::conditional<isRowMajor, RowPolicy, ColPolicy>::type KPolicy;
+  auto base_x     = reinterpret_cast<uintptr_t>(x);
+  auto base_y     = reinterpret_cast<uintptr_t>(y);
+  size_t stride_X = sizeof(DataT) * ldx;  // stride in bytes
+  size_t stride_Y = sizeof(DataT) * ldy;  // stride in bytes
 
-  l1_distance_op distance_op{};
+  bool base_16B_aligned = base_x % 16 == 0 && base_y % 16 == 0;
+  bool base_8B_aligned  = base_x % 8 == 0 && base_y % 8 == 0;
 
-  using PCT = params_CT<DataT, AccT, OutT, IdxT, KPolicy, raft::distance::detail::l1_distance_op, FinalLambda, isRowMajor>;
+  bool stride_16B_aligned = stride_X % 16 == 0 && stride_Y % 16 == 0;
+  bool stride_8B_aligned  = stride_X % 8 == 0 && stride_Y % 8 == 0;
 
-  distance_matrix_launch<PCT>(
-    distance_op, fin_op,        // Operations
-    x, y, nullptr, nullptr,     // Input data
-    m, n, k, lda, ldb, ldd,     // Dimensions
-    dOutput,                    // Output data
-    stream);                    // CUDA stream
-}
-
-template <typename DataT,
-          typename AccT,
-          typename OutT,
-          typename IdxT,
-          typename FinalLambda,
-          bool isRowMajor>
-void l1(IdxT m,
-        IdxT n,
-        IdxT k,
-        IdxT lda,
-        IdxT ldb,
-        IdxT ldd,
-        const DataT* x,
-        const DataT* y,
-        OutT* dOutput,
-        FinalLambda fin_op,
-        cudaStream_t stream)
-{
-  size_t bytesA = sizeof(DataT) * lda;
-  size_t bytesB = sizeof(DataT) * ldb;
-  if (16 % sizeof(DataT) == 0 && bytesA % 16 == 0 && bytesB % 16 == 0) {
-    l1Impl<DataT, AccT, OutT, IdxT, 16 / sizeof(DataT), FinalLambda, isRowMajor>(
-      x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
-  } else if (8 % sizeof(DataT) == 0 && bytesA % 8 == 0 && bytesB % 8 == 0) {
-    l1Impl<DataT, AccT, OutT, IdxT, 8 / sizeof(DataT), FinalLambda, isRowMajor>(
-      x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
+  if (16 % sizeof(DataT) == 0 && base_16B_aligned && stride_16B_aligned) {
+    return 16 / sizeof(DataT);
+  } else if (8 % sizeof(DataT) == 0 && base_8B_aligned && stride_8B_aligned) {
+    return 8 / sizeof(DataT);
   } else {
-    l1Impl<DataT, AccT, OutT, IdxT, 1, FinalLambda, isRowMajor>(
-      x, y, m, n, k, lda, ldb, ldd, dOutput, fin_op, stream);
+    return 1;
   }
 }
 
-/**
- * @brief the L1 distance matrix calculation
- *  It computes the following equation: cij = op(ai-bj)
- * @tparam InType input data-type (for A and B matrices)
- * @tparam AccType accumulation data-type
- * @tparam OutType output data-type (for C and D matrices)
- * @tparam FinalLambda user-defined epilogue lamba
- * @tparam Index_ Index type
- * @param m number of rows of A and C/D
- * @param n number of columns of B and C/D
- * @param k number of cols of A and rows of B
- * @param pA input matrix
- * @param pB input matrix
- * @param pD output matrix
- * @param fin_op the final element-wise epilogue lambda
- * @param stream cuda stream where to launch work
- * @param isRowMajor whether the input and output matrices are row major
- */
-template <typename InType,
-          typename AccType,
-          typename OutType,
-          typename FinalLambda,
-          typename Index_ = int>
+template <typename opT,
+          typename DataT,
+          typename AccT,
+          typename OutT,
+          typename FinOpT,
+          typename IdxT = int>
+void distance_matrix_dispatch(opT distance_op,
+                              int m_,
+                              int n_,
+                              int k_,
+                              const DataT* x_,
+                              const DataT* y_,
+                              OutT* out,
+                              FinOpT fin_op,
+                              cudaStream_t stream,
+                              bool is_row_major)
+{
+  // Determine leading dimensions and possibly flip order of passing x and y if
+  // column_major.
+  //
+  // ldx, ldy, and ld_out are the leading dimensions of x, y, and out
+  const DataT* x;
+  const DataT* y;
+  int ldx, ldy, ld_out;
+  int m, n, k;
+  if (is_row_major) {
+    // Pass x, y, m, n, k in order
+    x = x_,   y = y_;
+    m = m_,   n = n_,   k = k_;
+    ldx = k_, ldy = k_, ld_out = n_;
+  } else {
+    // Flip x, y, and m, n, k.
+    x = y_,   y = x_;
+    m = n_,   n = m_,   k = k_;
+    ldx = n_, ldy = m_, ld_out = m_;
+  }
+
+  int vectorized_load_num_elem = max_aligned_load(x, y, ldx, ldy);
+
+  // We dispatch based on
+  // - vectorized_load_num_elem
+  // - is_row_major
+
+  // Create run-time parameter struct that does the dispatching
+  using PRT = params_RT<DataT, AccT, OutT, IdxT, decltype(distance_op), FinOpT>;
+  PRT run_time_params{vectorized_load_num_elem, is_row_major};
+
+  // Turn run-time parameters into compile-time parameters.
+  bool dispatch_success = run_time_params.dispatch_with_compile_time_params(
+    // We pass a lambda that receives the compile-time parameters and can use these
+    // to call the correct kernel.
+    [&](auto compile_time_params) {
+      // compile_time_params is an empty struct that we can convert back to a type
+      // using decltype.
+      return distance_matrix_launch<decltype(compile_time_params)>(
+        distance_op,
+        fin_op,
+        x,
+        y,
+        nullptr,
+        nullptr,  // TODO: use _xn, _yn for non-l1 distances
+        m,
+        n,
+        k,
+        ldx,
+        ldy,
+        ld_out,
+        out,
+        stream);
+    });
+}
+
+template <typename DataT, typename AccT, typename OutT, typename FinOpT, typename IdxT = int>
 void l1Impl(int m,
             int n,
             int k,
-            const InType* pA,
-            const InType* pB,
-            OutType* pD,
-            FinalLambda fin_op,
+            const DataT* x,
+            const DataT* y,
+            OutT* out,
+            FinOpT fin_op,
             cudaStream_t stream,
-            bool isRowMajor)
+            bool is_row_major)
 {
-  typedef std::is_same<OutType, bool> is_bool;
-  typedef typename std::conditional<is_bool::value, OutType, AccType>::type L1OutType;
-  Index_ lda, ldb, ldd;
-  L1OutType* pDcast = reinterpret_cast<L1OutType*>(pD);
-  if (isRowMajor) {
-    lda = k, ldb = k, ldd = n;
-    l1<InType, AccType, L1OutType, Index_, FinalLambda, true>(
-      m, n, k, lda, ldb, ldd, pA, pB, pDcast, fin_op, stream);
+  l1_distance_op distance_op{};
 
-  } else {
-    lda = n, ldb = m, ldd = m;
-    l1<InType, AccType, L1OutType, Index_, FinalLambda, false>(
-      n, m, k, lda, ldb, ldd, pB, pA, pDcast, fin_op, stream);
-  }
+  distance_matrix_dispatch<l1_distance_op, DataT, AccT, OutT, FinOpT, IdxT>(
+    distance_op, m, n, k, x, y, out, fin_op, stream, is_row_major);
 }
+
 }  // namespace detail
 }  // namespace distance
 }  // namespace raft
--- cpp/include/raft/distance/detail/pairwise_distance_op.cuh
+++ cpp/include/raft/distance/detail/pairwise_distance_op.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, NVIDIA CORPORATION.
+ * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,8 +25,6 @@
 
 namespace raft::distance::detail {
 
-
-
 template <typename data_type,
           typename accumulate_type,
           typename out_type,
@@ -39,58 +37,133 @@ template <typename data_type,
           bool row_major>
 struct params_CT {
   using DataT = data_type;
-  using AccT = accumulate_type;
-  using OutT = out_type;
-  using IdxT = index_type;
-
+  using AccT  = accumulate_type;
+  using OutT  = out_type;
+  using IdxT  = index_type;
   using PolicyT = policy;
-
-  using opT = op_type;
-  using FinOpT = final_op_type;
+  using opT                          = op_type;
+  using FinOpT                       = final_op_type;
   static constexpr bool is_row_major = row_major;
+};
+
+template <typename DataT,
+          typename AccT,
+          typename OutT,
+          typename IdxT,
+          typename opT,
+          typename FinOpT>
+struct params_RT {
+  int vectorized_load_num_elem = 1;
+  bool row_major               = true;
+
+  // Turn run-time parameters into compile-time parameters.
+  // Call the provided function f with these compile-time parameters.
+  // Returns false if dispatch fails, i.e., if there is no implementation
+  // for the given runtime parameters.
+  template <typename F>
+  bool dispatch_with_compile_time_params(F&& f) const
+  {
+    return convert_vectorized_load_num_elem(f);
+  }
+
+  // Step 1: convert alignment into a compile time constant
+  template <typename F>
+  bool convert_vectorized_load_num_elem(F&& f) const
+  {
+    bool fail = false;
+    switch (vectorized_load_num_elem) {
+      case 1: return layout<1>(f);
+      case 2: return layout<2>(f);
+      case 4:
+        // We need "if constexpr" here, to prevent the if else to be delegated
+        // to run time (in which case a kernel that loads 4 doubles is
+        // generated). This is especially important, because that leads to
+        // compilation errors (which we want to avoid).
+        if constexpr (sizeof(DataT) < 8) {
+          return layout<4>(f);
+        } else {
+          // For doubles, load at most 2 elements in one instruction.
+          return layout<2>(f);
+        }
+      default: return fail;
+    };
+  }
+
+  // Step 2: convert layout into a compile time constant
+  template <int vec_len, typename F>
+  bool layout(F&& f) const
+  {
+    if (row_major) {
+      return to_compile_time_params<vec_len, true>(f);
+    } else {
+      return to_compile_time_params<vec_len, false>(f);
+    }
+  }
+
+  // Step 3: convert compile-time constant into compile-time parameter struct and invoke
+  // function f with these compile time parameters.
+  template <int vec_len, bool is_row_major, typename F>
+  bool to_compile_time_params(F&& f) const
+  {
+    // Determine kernel policy using vec_len and layout
+    typedef typename raft::linalg::Policy4x4<DataT, vec_len>::Policy RowPolicy;
+    typedef typename raft::linalg::Policy4x4<DataT, vec_len>::ColPolicy ColPolicy;
+    typedef typename std::conditional<is_row_major, RowPolicy, ColPolicy>::type Policy;
+
+    // Create compile-time parameter type and instantiate a struct;
+    using PCT = params_CT<DataT, AccT, OutT, IdxT, Policy, opT, FinOpT, is_row_major>;
+    PCT compile_time_params{};
+
+    // Dispatch to f
+    f(compile_time_params);
+
+    bool dispatch_success = true;
+    return dispatch_success;
+  }
 };
 
 template <typename PCT>
 __global__ __launch_bounds__(PCT::PolicyT::Nthreads, 2)
 
-  void pairwiseDistanceOpKernel(
-    const typename PCT::DataT* x,
-    const typename PCT::DataT* y,
-    const typename PCT::DataT* _xn,
-    const typename PCT::DataT* _yn,
-    typename PCT::IdxT m,
-    typename PCT::IdxT n,
-    typename PCT::IdxT k,
-    typename PCT::IdxT lda,
-    typename PCT::IdxT ldb,
-    typename PCT::IdxT ldd,
-    typename PCT::OutT* dOutput,
-    typename PCT::opT distance_op,
-    typename PCT::FinOpT fin_op)
+  void pairwiseDistanceOpKernel(const typename PCT::DataT* x,
+                                const typename PCT::DataT* y,
+                                const typename PCT::DataT* _xn,
+                                const typename PCT::DataT* _yn,
+                                typename PCT::IdxT m,
+                                typename PCT::IdxT n,
+                                typename PCT::IdxT k,
+                                typename PCT::IdxT lda,
+                                typename PCT::IdxT ldb,
+                                typename PCT::IdxT ldd,
+                                typename PCT::OutT* dOutput,
+                                typename PCT::opT distance_op,
+                                typename PCT::FinOpT fin_op)
 {
-  using AccT = typename PCT::AccT;
+  using AccT  = typename PCT::AccT;
   using DataT = typename PCT::DataT;
-  using OutT = typename PCT::OutT;
-  using IdxT = typename PCT::IdxT;
+  using OutT  = typename PCT::OutT;
+  using IdxT  = typename PCT::IdxT;
 
   using Policy = typename PCT::PolicyT;
 
-  // Instantiate PCT to access constexpr members.
+  // Instantiate compile time parameters to access constexpr members.
   PCT compile_time_params{};
 
   extern __shared__ char smem[];
 
   // Wrap operator back into lambdas. This is temporary and should be removed. (TODO)
   auto core_op = [distance_op] __device__(AccT & acc, DataT & x, DataT & y) {
-    // use .template to disambiguate (See: https://en.cppreference.com/w/cpp/language/dependent_name)
+    // use .template to disambiguate (See:
+    // https://en.cppreference.com/w/cpp/language/dependent_name)
     distance_op.template core<AccT, DataT>(acc, x, y);
   };
   auto epilog_op = [distance_op] __device__(AccT acc[Policy::AccRowsPerTh][Policy::AccColsPerTh],
-                                     DataT * regxn,
-                                     DataT * regyn,
-                                     IdxT gridStrideX,
-                                     IdxT gridStrideY) {
-    distance_op.template epilog<Policy, AccT, DataT, IdxT>(acc, regxn, regyn, gridStrideX, gridStrideY);
+                                            DataT * regxn,
+                                            DataT * regyn,
+                                            IdxT gridStrideX,
+                                            IdxT gridStrideY) {
+    distance_op.template epilog<Policy, AccT, DataT, IdxT>(
+      acc, regxn, regyn, gridStrideX, gridStrideY);
   };
 
   // No support for row_epilog_op.
@@ -110,9 +183,23 @@ __global__ __launch_bounds__(PCT::PolicyT::Nthreads, 2)
                     decltype(row_epilog_op),
                     compile_time_params.is_row_major,
                     write_out>
-    obj(
-      x, y, m, n, k, lda, ldb, ldd, _xn, _yn, dOutput, smem, core_op, epilog_op, fin_op, row_epilog_op);
+    obj(x,
+        y,
+        m,
+        n,
+        k,
+        lda,
+        ldb,
+        ldd,
+        _xn,
+        _yn,
+        dOutput,
+        smem,
+        core_op,
+        epilog_op,
+        fin_op,
+        row_epilog_op);
   obj.run();
-
 }
-};  // namespace detail
+
+};  // namespace raft::distance::detail
