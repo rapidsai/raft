7afe6cc8219c4225442dbcbce4e8a28f22dbcb2f
Allard Hendriksen <ahendriksen@nvidia.com>
2023-01-13 16:35:29 +0100

    Add template for distance operator
    I wasted a lot of time because I had not replaced the op::core() method
    of the l2_exp_distance_op after I copied it from l2_unexp_distance_op...
    
    If I copy something from the template and forget to fill it in, I get a
    compile error.

---
+++ cpp/include/raft/distance/detail/distance_ops/template.cuh
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+namespace raft::distance::detail::ops {
+
+// Describes the computation the template distance
+//
+// Fill in the TODO items.
+
+struct template_op {
+  // Load norms of input data
+  static constexpr bool use_norms = TODO;
+
+  // Size of shared memory. This is normally decided by the kernel policy, but
+  // some ops such as correlation_distance_op use more.
+  template <typename Policy, typename DataT>
+  constexpr size_t shared_mem_size()
+  {
+    return Policy::SmemSize + TODO;
+  }
+
+  template <typename AccT, typename DataT>
+  DI void core(AccT& acc, DataT& x, DataT& y) const
+  {
+    TODO;
+  };
+
+  template <typename Policy, typename AccT, typename DataT, typename IdxT>
+  DI void epilog(AccT acc[Policy::AccRowsPerTh][Policy::AccColsPerTh],
+                 DataT* regxn,
+                 DataT* regyn,
+                 IdxT gridStrideX,
+                 IdxT gridStrideY) const
+  {
+    TODO;
+  };
+
+}  // namespace raft::distance::detail::ops
