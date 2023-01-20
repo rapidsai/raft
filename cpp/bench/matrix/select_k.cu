c78d9b0c0e876a0a56c13ed5d58310bab07f25d9
achirkin <achirkin@users.noreply.github.com>
2023-01-20 11:22:13 +0100

    Reference a TODO issue

--- cpp/bench/matrix/select_k.cu
+++ cpp/bench/matrix/select_k.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,11 @@
  * limitations under the License.
  */
 
+/**
+ * TODO: reconsider how to organize shared test+bench files better
+ *       Related Issue: https://github.com/rapidsai/raft/issues/1153
+ *       (although this header does not depend on any gtest headers)
+ */
 #include "../../test/matrix/select_k.cuh"
 
 #include <common/benchmark.hpp>
