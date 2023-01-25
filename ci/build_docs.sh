dae6cf1d6cbcb3a810e6d798cf0895717f9e8b3e
Jake Awe <jawe@nvidia.com>
2023-01-25 09:16:59 -0600

    fix typo

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -30,7 +30,7 @@ VERSION_NUMBER=$(rapids-get-rapids-version-from-git)
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
   --channel "${PYTHON_CHANNEL}" \
-  libraft \
+  libraft-split \
   pylibraft \
   raft-dask
 
