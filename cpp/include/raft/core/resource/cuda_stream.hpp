3e023929fb34ce559b84d8cfec37684fa83d2fe1
Corey J. Nolet <cjnolet@gmail.com>
2023-02-01 18:21:22 -0500

    Removing interruptible from basic handle sync.

--- cpp/include/raft/core/resource/cuda_stream.hpp
+++ cpp/include/raft/core/resource/cuda_stream.hpp
@@ -91,7 +91,7 @@ inline void set_cuda_stream(resources const& res, rmm::cuda_stream_view stream_v
  */
 inline void sync_stream(const resources& res, rmm::cuda_stream_view stream)
 {
-  interruptible::synchronize(stream);
+  RAFT_CUDA_TRY(cudaStreamSynchronize(stream));
 }
 
 /**
