d175ed6203de923f32cf60322fd68af1c2ff96d4
Allard Hendriksen <ahendriksen@nvidia.com>
2023-02-10 10:19:36 +0100

    build.sh: Add --time option to log nvcc compile time
    The time option is disabled by default. When enabled, writes a log of
    compilation times to
    cpp/build/nvcc_compile_log.csv.
    
    This is not supported in CI, as it leads to seg faults.

--- cpp/cmake/modules/ConfigureCUDA.cmake
+++ cpp/cmake/modules/ConfigureCUDA.cmake
@@ -21,7 +21,10 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   list(APPEND RAFT_CXX_FLAGS -Wall -Werror -Wno-unknown-pragmas -Wno-error=deprecated-declarations)
 endif()
 
-list(APPEND RAFT_CUDA_FLAGS "--time=CMakeFiles/nvcc_compile_log.csv")
+if(CUDA_LOG_COMPILE_TIME)
+  list(APPEND RAFT_CUDA_FLAGS "--time=nvcc_compile_log.csv")
+endif()
+
 list(APPEND RAFT_CUDA_FLAGS --expt-extended-lambda --expt-relaxed-constexpr)
 list(APPEND RAFT_CXX_FLAGS "-DCUDA_API_PER_THREAD_DEFAULT_STREAM")
 list(APPEND RAFT_CUDA_FLAGS "-DCUDA_API_PER_THREAD_DEFAULT_STREAM")
