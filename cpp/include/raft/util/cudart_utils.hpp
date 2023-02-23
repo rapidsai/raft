b085085121fe23b5bb1fea8f2939ab46598a88e6
Corey J. Nolet <cjnolet@gmail.com>
2023-02-22 19:40:43 -0500

    Adding util to get the device id for a pointer address and associated test

--- cpp/include/raft/util/cudart_utils.hpp
+++ cpp/include/raft/util/cudart_utils.hpp
@@ -25,6 +25,7 @@
 #pragma once
 
 #include <raft/core/error.hpp>
+#include <raft/core/logger.hpp>
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/mr/device/managed_memory_resource.hpp>
 #include <rmm/mr/device/per_device_resource.hpp>
@@ -331,6 +332,32 @@ void print_vector(const char* variable_name, const T* ptr, size_t componentsCoun
   }
 }
 /** @} */
+
+/**
+ * Returns the id of the device for which the pointer is located
+ * @param p pointer to check
+ * @return id of device for which pointer is located, otherwise -1.
+ */
+template <typename T>
+int get_device_for_address(const T* p)
+{
+  if (!p) { return -1; }
+
+  cudaPointerAttributes att;
+  cudaError_t err = cudaPointerGetAttributes(&att, p);
+  if (err == cudaErrorInvalidValue) {
+    // Make sure the current thread error status has been reset
+    err = cudaGetLastError();
+    return -1;
+  }
+
+  // memoryType is deprecated for CUDA 10.0+
+  if (att.type == cudaMemoryTypeDevice) {
+    return att.device;
+  } else {
+    return -1;
+  }
+}
 
 /** helper method to get max usable shared mem per block parameter */
 inline int getSharedMemPerBlock()
