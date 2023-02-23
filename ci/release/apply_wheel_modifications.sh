53d4b570fe5109c9b84fd738552fc64a4b6aa47c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-23 11:52:34 -0800

    Add pyproject.toml change to apply_wheel_modifications.sh.

--- ci/release/apply_wheel_modifications.sh
+++ ci/release/apply_wheel_modifications.sh
@@ -8,11 +8,11 @@ CUDA_SUFFIX=${2}
 
 # __init__.py versions
 sed -i "s/__version__ = .*/__version__ = \"${VERSION}\"/g" python/pylibraft/pylibraft/__init__.py
-sed -i "s/__version__ = .*/__version__ = \"${VERSION}\"/g" python/raft_dask/raft_dask/__init__.py
+sed -i "s/__version__ = .*/__version__ = \"${VERSION}\"/g" python/raft-dask/raft_dask/__init__.py
 
 # setup.py versions
 sed -i "s/version=.*,/version=\"${VERSION}\",/g" python/pylibraft/pylibraft/__init__.py
-sed -i "s/version=.*,/version=\"${VERSION}\",/g" python/raft_dask/raft_dask/__init__.py
+sed -i "s/version=.*,/version=\"${VERSION}\",/g" python/raft-dask/raft_dask/__init__.py
 
 # pylibraft setup.py cuda suffixes
 sed -i "s/name=\"pylibraft\"/name=\"pylibraft${CUDA_SUFFIX}\"/g" python/pylibraft/setup.py
@@ -22,3 +22,6 @@ sed -i "s/rmm/rmm${CUDA_SUFFIX}/g" python/pylibraft/setup.py
 sed -i "s/name=\"raft-dask\"/name=\"raft-dask${CUDA_SUFFIX}\"/g" python/raft-dask/setup.py
 sed -i "s/ucx-py/ucx-py${CUDA_SUFFIX}/g" python/raft-dask/setup.py
 sed -i "s/pylibraft/pylibraft${CUDA_SUFFIX}/g" python/raft-dask/setup.py
+
+# Dependency versions in pyproject.toml
+sed -i "s/rmm/rmm${CUDA_SUFFIX}/g" python/pylibraft/pyproject.toml
