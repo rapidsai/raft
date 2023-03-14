7e328ff8eed6410fea3c79db8bfca6dd07050c66
Corey J. Nolet <cjnolet@users.noreply.github.com>
2023-03-13 20:54:27 -0400

    Revert "Generic linalg::map (#1329)" (#1336)
    The change to the public API was siginficant enough here that it's going to require updates on the cuml side which invoke `map_k` with very different arguments (e.g. mdspan, different order). For now, it's best we revert this commmit to unblock cuml and then we can proceed by keeping the old APIs and deprecating them until we change cuml.
    
    Authors:
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Ben Frederickson (https://github.com/benfred)
    
    URL: https://github.com/rapidsai/raft/pull/1336

--- cpp/include/raft/linalg/detail/init.hpp
+++ cpp/include/raft/linalg/detail/init.hpp
@@ -13,31 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __INIT_H
-#define __INIT_H
 
 #pragma once
 
-#include "detail/init.hpp"
-#include <raft/util/cudart_utils.hpp>
+#include <rmm/exec_policy.hpp>
+#include <thrust/copy.h>
+#include <thrust/device_ptr.h>
+#include <thrust/iterator/counting_iterator.h>
 
 namespace raft {
 namespace linalg {
+namespace detail {
 
-/**
- * @brief Like Python range.
- *
- * Fills the output as out[i] = i.
- *
- * \param [out] out device array, size [end-start]
- * \param [in] start of the range
- * \param [in] end of range (exclusive)
- * \param [in] stream cuda stream
- */
 template <typename T>
 void range(T* out, int start, int end, cudaStream_t stream)
 {
-  detail::range(out, start, end, stream);
+  thrust::counting_iterator<int> first(start);
+  thrust::counting_iterator<int> last = first + (end - start);
+  thrust::device_ptr<T> ptr(out);
+  thrust::copy(rmm::exec_policy(stream), first, last, ptr);
 }
 
 /**
@@ -52,23 +46,9 @@ void range(T* out, int start, int end, cudaStream_t stream)
 template <typename T, int TPB = 256>
 void range(T* out, int n, cudaStream_t stream)
 {
-  detail::range(out, n, stream);
+  range(out, 0, n, stream);
 }
 
-/**
- * @brief Zeros the output.
- *
- * \param [out] out device array, size [n]
- * \param [in] n length of the array
- * \param [in] stream cuda stream
- */
-template <typename T>
-void zero(T* out, int n, cudaStream_t stream)
-{
-  RAFT_CUDA_TRY(cudaMemsetAsync(static_cast<void*>(out), 0, n * sizeof(T), stream));
-}
-
+}  // namespace detail
 }  // namespace linalg
 }  // namespace raft
-
-#endif
--- cpp/include/raft/linalg/unary_op.cuh
+++ cpp/include/raft/linalg/unary_op.cuh
@@ -18,9 +18,11 @@
 
 #pragma once
 
+#include "detail/unary_op.cuh"
+
 #include <raft/core/device_mdspan.hpp>
 #include <raft/core/device_resources.hpp>
-#include <raft/linalg/map.cuh>
+#include <raft/util/input_validation.hpp>
 
 namespace raft {
 namespace linalg {
@@ -46,7 +48,7 @@ template <typename InType,
           int TPB          = 256>
 void unaryOp(OutType* out, const InType* in, IdxType len, Lambda op, cudaStream_t stream)
 {
-  return detail::map<false>(stream, out, len, op, in);
+  detail::unaryOpCaller(out, in, len, op, stream);
 }
 
 /**
@@ -69,11 +71,7 @@ void unaryOp(OutType* out, const InType* in, IdxType len, Lambda op, cudaStream_
 template <typename OutType, typename Lambda, typename IdxType = int, int TPB = 256>
 void writeOnlyUnaryOp(OutType* out, IdxType len, Lambda op, cudaStream_t stream)
 {
-  return detail::map<true>(stream, out, len, [op] __device__(IdxType offset) {
-    OutType r;
-    op(&r, offset);
-    return r;
-  });
+  detail::writeOnlyUnaryOpCaller(out, len, op, stream);
 }
 
 /**
@@ -99,7 +97,20 @@ template <typename InType,
           typename = raft::enable_if_output_device_mdspan<OutType>>
 void unary_op(raft::device_resources const& handle, InType in, OutType out, Lambda op)
 {
-  return map(handle, in, out, op);
+  RAFT_EXPECTS(raft::is_row_or_column_major(out), "Output must be contiguous");
+  RAFT_EXPECTS(raft::is_row_or_column_major(in), "Input must be contiguous");
+  RAFT_EXPECTS(out.size() == in.size(), "Size mismatch between Output and Input");
+
+  using in_value_t  = typename InType::value_type;
+  using out_value_t = typename OutType::value_type;
+
+  if (out.size() <= std::numeric_limits<std::uint32_t>::max()) {
+    unaryOp<in_value_t, Lambda, std::uint32_t, out_value_t>(
+      out.data_handle(), in.data_handle(), out.size(), op, handle.get_stream());
+  } else {
+    unaryOp<in_value_t, Lambda, std::uint64_t, out_value_t>(
+      out.data_handle(), in.data_handle(), out.size(), op, handle.get_stream());
+  }
 }
 
 /**
@@ -119,7 +130,17 @@ template <typename OutType,
           typename = raft::enable_if_output_device_mdspan<OutType>>
 void write_only_unary_op(const raft::device_resources& handle, OutType out, Lambda op)
 {
-  return writeOnlyUnaryOp(out.data_handle(), out.size(), op, handle.get_stream());
+  RAFT_EXPECTS(raft::is_row_or_column_major(out), "Output must be contiguous");
+
+  using out_value_t = typename OutType::value_type;
+
+  if (out.size() <= std::numeric_limits<std::uint32_t>::max()) {
+    writeOnlyUnaryOp<out_value_t, Lambda, std::uint32_t>(
+      out.data_handle(), out.size(), op, handle.get_stream());
+  } else {
+    writeOnlyUnaryOp<out_value_t, Lambda, std::uint64_t>(
+      out.data_handle(), out.size(), op, handle.get_stream());
+  }
 }
 
 /** @} */  // end of group unary_op
