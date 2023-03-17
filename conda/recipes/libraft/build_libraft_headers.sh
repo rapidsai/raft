6a3ddaf13d790530ada1334503b9d09cb61f7f3c
Bradley Dice <bdice@bradleydice.com>
2023-03-17 16:41:36 -0500

    Update conda recipes for CUDA 12 (nvidia channel).

--- conda/recipes/libraft/build_libraft_headers.sh
+++ conda/recipes/libraft/build_libraft_headers.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh libraft --allgpuarch --no-nvtx
