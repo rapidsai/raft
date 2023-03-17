92d82db893feca721e33babee7c600b065f1c71e
Ben Frederickson <ben@benfrederickson.com>
2023-03-16 20:07:36 -0700

    one more try with cublasSetStream

--- cpp/include/raft/linalg/detail/transpose.cuh
+++ cpp/include/raft/linalg/detail/transpose.cuh
@@ -37,6 +37,7 @@ void transpose(raft::device_resources const& handle,
                cudaStream_t stream)
 {
   cublasHandle_t cublas_h = handle.get_cublas_handle();
+  RAFT_CUBLAS_TRY(cublasSetStream(cublas_h, stream));
 
   int out_n_rows = n_cols;
   int out_n_cols = n_rows;
@@ -90,7 +91,11 @@ void transpose_row_major_impl(
   auto out_n_cols   = in.extent(0);
   T constexpr kOne  = 1;
   T constexpr kZero = 0;
-  CUBLAS_TRY(cublasgeam(handle.get_cublas_handle(),
+
+  cublasHandle_t cublas_h = handle.get_cublas_handle();
+  RAFT_CUBLAS_TRY(cublasSetStream(cublas_h, stream));
+
+  CUBLAS_TRY(cublasgeam(cublas_h,
                         CUBLAS_OP_T,
                         CUBLAS_OP_N,
                         out_n_cols,
@@ -112,11 +117,14 @@ void transpose_col_major_impl(
   raft::mdspan<T, raft::matrix_extent<IndexType>, LayoutPolicy, AccessorPolicy> in,
   raft::mdspan<T, raft::matrix_extent<IndexType>, LayoutPolicy, AccessorPolicy> out)
 {
-  auto out_n_rows   = in.extent(1);
-  auto out_n_cols   = in.extent(0);
-  T constexpr kOne  = 1;
-  T constexpr kZero = 0;
-  CUBLAS_TRY(cublasgeam(handle.get_cublas_handle(),
+  auto out_n_rows         = in.extent(1);
+  auto out_n_cols         = in.extent(0);
+  T constexpr kOne        = 1;
+  T constexpr kZero       = 0;
+  cublasHandle_t cublas_h = handle.get_cublas_handle();
+  RAFT_CUBLAS_TRY(cublasSetStream(cublas_h, stream));
+
+  CUBLAS_TRY(cublasgeam(cublas_h,
                         CUBLAS_OP_T,
                         CUBLAS_OP_N,
                         out_n_rows,
