17af88db9c548a109ed19c35ca5e62af1a13f02d
Allard Hendriksen <ahendriksen@nvidia.com>
2023-03-20 14:04:57 +0100

    Fix ivf flat specialization header

--- cpp/include/raft/neighbors/specializations/ivf_flat.cuh
+++ cpp/include/raft/neighbors/specializations/ivf_flat.cuh
@@ -46,9 +46,9 @@ namespace raft::neighbors::ivf_flat {
                               raft::device_matrix_view<IdxT, IdxT, row_major>,               \
                               raft::device_matrix_view<float, IdxT, row_major>);
 
-RAFT_INST(float, uint64_t);
-RAFT_INST(int8_t, uint64_t);
-RAFT_INST(uint8_t, uint64_t);
+RAFT_INST(float, int64_t);
+RAFT_INST(int8_t, int64_t);
+RAFT_INST(uint8_t, int64_t);
 
 #undef RAFT_INST
 }  // namespace raft::neighbors::ivf_flat
