a435e19dba0b00224bf8e3af180d44d249e96305
Allard Hendriksen <ahendriksen@nvidia.com>
2023-03-20 14:08:28 +0100

    ivf_flat::build: Use template type IdxT
    Instead of hardcoding uint64_t

--- cpp/include/raft/neighbors/specializations/ivf_flat.cuh
+++ cpp/include/raft/neighbors/specializations/ivf_flat.cuh
@@ -20,30 +20,30 @@
 
 namespace raft::neighbors::ivf_flat {
 
-#define RAFT_INST(T, IdxT)                                                                   \
-  extern template auto build(raft::device_resources const& handle,                           \
-                             const index_params& params,                                     \
-                             raft::device_matrix_view<const T, uint64_t, row_major> dataset) \
-    ->index<T, IdxT>;                                                                        \
-                                                                                             \
-  extern template auto extend(                                                               \
-    raft::device_resources const& handle,                                                    \
-    raft::device_matrix_view<const T, IdxT, row_major> new_vectors,                          \
-    std::optional<raft::device_vector_view<const IdxT, IdxT>> new_indices,                   \
-    const index<T, IdxT>& orig_index)                                                        \
-    ->index<T, IdxT>;                                                                        \
-                                                                                             \
-  extern template void extend(                                                               \
-    raft::device_resources const& handle,                                                    \
-    raft::device_matrix_view<const T, IdxT, row_major> new_vectors,                          \
-    std::optional<raft::device_vector_view<const IdxT, IdxT>> new_indices,                   \
-    raft::neighbors::ivf_flat::index<T, IdxT>* idx);                                         \
-                                                                                             \
-  extern template void search(raft::device_resources const&,                                 \
-                              raft::neighbors::ivf_flat::search_params const&,               \
-                              const raft::neighbors::ivf_flat::index<T, IdxT>&,              \
-                              raft::device_matrix_view<const T, IdxT, row_major>,            \
-                              raft::device_matrix_view<IdxT, IdxT, row_major>,               \
+#define RAFT_INST(T, IdxT)                                                               \
+  extern template auto build(raft::device_resources const& handle,                       \
+                             const index_params& params,                                 \
+                             raft::device_matrix_view<const T, IdxT, row_major> dataset) \
+    ->index<T, IdxT>;                                                                    \
+                                                                                         \
+  extern template auto extend(                                                           \
+    raft::device_resources const& handle,                                                \
+    raft::device_matrix_view<const T, IdxT, row_major> new_vectors,                      \
+    std::optional<raft::device_vector_view<const IdxT, IdxT>> new_indices,               \
+    const index<T, IdxT>& orig_index)                                                    \
+    ->index<T, IdxT>;                                                                    \
+                                                                                         \
+  extern template void extend(                                                           \
+    raft::device_resources const& handle,                                                \
+    raft::device_matrix_view<const T, IdxT, row_major> new_vectors,                      \
+    std::optional<raft::device_vector_view<const IdxT, IdxT>> new_indices,               \
+    raft::neighbors::ivf_flat::index<T, IdxT>* idx);                                     \
+                                                                                         \
+  extern template void search(raft::device_resources const&,                             \
+                              raft::neighbors::ivf_flat::search_params const&,           \
+                              const raft::neighbors::ivf_flat::index<T, IdxT>&,          \
+                              raft::device_matrix_view<const T, IdxT, row_major>,        \
+                              raft::device_matrix_view<IdxT, IdxT, row_major>,           \
                               raft::device_matrix_view<float, IdxT, row_major>);
 
 RAFT_INST(float, int64_t);
