44bc7422b980b6e453fd2b47de3d6745c1a025fa
Allard Hendriksen <ahendriksen@nvidia.com>
2023-03-21 18:51:31 +0100

    Add extern template for ivfflat_interleaved_scan
    This cuts compilation time for refine_d_int64_t_float.cu.o from ~900
    seconds to 29 seconds.

--- cpp/include/raft/neighbors/specializations/ivf_flat.cuh
+++ cpp/include/raft/neighbors/specializations/ivf_flat.cuh
@@ -44,7 +44,23 @@ namespace raft::neighbors::ivf_flat {
                               const raft::neighbors::ivf_flat::index<T, IdxT>&,          \
                               raft::device_matrix_view<const T, IdxT, row_major>,        \
                               raft::device_matrix_view<IdxT, IdxT, row_major>,           \
-                              raft::device_matrix_view<float, IdxT, row_major>);
+                              raft::device_matrix_view<float, IdxT, row_major>);         \
+                                                                                         \
+  extern template void raft::neighbors::ivf_flat::detail::ivfflat_interleaved_scan<      \
+    T,                                                                                   \
+    typename raft::spatial::knn::detail::utils::config<T>::value_t,                      \
+    IdxT>(const index<T, IdxT>& index,                                                   \
+          const T* queries,                                                              \
+          const uint32_t* coarse_query_results,                                          \
+          const uint32_t n_queries,                                                      \
+          const raft::distance::DistanceType metric,                                     \
+          const uint32_t n_probes,                                                       \
+          const uint32_t k,                                                              \
+          const bool select_min,                                                         \
+          IdxT* neighbors,                                                               \
+          float* distances,                                                              \
+          uint32_t& grid_dim_x,                                                          \
+          rmm::cuda_stream_view stream);
 
 RAFT_INST(float, int64_t);
 RAFT_INST(int8_t, int64_t);
