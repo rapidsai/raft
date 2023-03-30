d88346ec589301adfca477d8fa0c32001d6d450a
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-30 16:32:56 -0700

    Force build and test scripts to install rmm from the PR.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -7,10 +7,13 @@ source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
 
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1223/72e0c74/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+
 rapids-print-env
 
 rapids-logger "Begin cpp build"
 
-rapids-mamba-retry mambabuild conda/recipes/libraft
+rapids-mamba-retry mambabuild --channel "${LIBRMM_CHANNEL}" conda/recipes/libraft
 
 rapids-upload-conda-to-s3 cpp
