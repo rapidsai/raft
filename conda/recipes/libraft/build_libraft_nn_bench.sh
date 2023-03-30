50d1576a10106c94e8414394d5a3c3fe7721afd7
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-30 09:49:56 -0700

    Add nvcc flags needed to work with broken nvidia packages

--- conda/recipes/libraft/build_libraft_nn_bench.sh
+++ conda/recipes/libraft/build_libraft_nn_bench.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh tests bench-ann --allgpuarch --no-nvtx
 cmake --install cpp/build --component ann_bench
--- conda/recipes/libraft/build_libraft_template.sh
+++ conda/recipes/libraft/build_libraft_template.sh
@@ -2,4 +2,5 @@
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 # Just building template so we verify it uses libraft.so and fail if it doesn't build
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh template
