cdee126bf2f2d9076926642da638dc383c16dba1
Corey J. Nolet <cjnolet@gmail.com>
2023-03-30 20:48:29 -0400

    Need to add ctk deps explicitly to executables

--- cpp/include/raft/linalg/detail/cublas_wrappers.hpp
+++ cpp/include/raft/linalg/detail/cublas_wrappers.hpp
@@ -967,6 +967,8 @@ inline cublasStatus_t cublasdot(cublasHandle_t handle,
   RAFT_CUBLAS_TRY(cublasSetStream(handle, stream));
   return cublasDotEx(
     handle, n, x, CUDA_R_32F, incx, y, CUDA_R_32F, incy, result, CUDA_R_32F, CUDA_R_32F);
+
+  //  return cublasSdot(handle, n, x, incx, y, incy, result);
 }
 
 template <>
@@ -982,6 +984,7 @@ inline cublasStatus_t cublasdot(cublasHandle_t handle,
   RAFT_CUBLAS_TRY(cublasSetStream(handle, stream));
   return cublasDotEx(
     handle, n, x, CUDA_R_64F, incx, y, CUDA_R_64F, incy, result, CUDA_R_64F, CUDA_R_64F);
+  //  return cublasDdot(handle, n, x, incx, y, incy, result);
 }
 /** @} */
 
