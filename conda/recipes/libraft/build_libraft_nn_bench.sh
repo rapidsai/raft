242931b37a9f696532599be1f03f12eb332424b7
Robert Maynard <rmaynard@nvidia.com>
2023-04-07 09:02:25 -0400

    Simplify building conda packages for libraft

--- conda/recipes/libraft/build_libraft_nn_bench.sh
+++ conda/recipes/libraft/build_libraft_nn_bench.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # Copyright (c) 2023, NVIDIA CORPORATION.
-
-./build.sh tests bench-ann --allgpuarch --no-nvtx
 cmake --install cpp/build --component ann_bench
--- conda/recipes/libraft/build_libraft_tests.sh
+++ conda/recipes/libraft/build_libraft_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
-./build.sh tests bench-prims --allgpuarch --no-nvtx
+./build.sh tests bench-prims bench-ann --allgpuarch --no-nvtx
 cmake --install cpp/build --component testing
+cmake --install cpp/build --component bench-prims
