8dd386101d51cfa9a45fb2cb06b0132aee31fea3
Allard Hendriksen <ahendriksen@nvidia.com>
2023-04-20 16:53:32 +0200

    Remove pairwise_matrix_instantiation_point

--- cpp/include/raft/distance/detail/pairwise_matrix/dispatch-inl.cuh
+++ cpp/include/raft/distance/detail/pairwise_matrix/dispatch-inl.cuh
@@ -24,7 +24,7 @@
  *    architectures.
  *
  * 2. Provide concise function templates that can be instantiated in
- *    src/distance/distance/specializations/detail/. Previously,
+ *    src/distance/detail/pairwise_matrix/. Previously,
  *    raft::distance::detail::distance was instantiated. The function
  *    necessarily required a large set of include files, which slowed down the
  *    build. The raft::distance::detail::pairwise_matrix_arch_dispatch functions
@@ -40,13 +40,13 @@
 // Including dispatch_sm80.cuh can slow down compile times (due to CUTLASS).
 // Therefore, it is the including file's responsibility to include the correct
 // dispatch_smXX.cuh headers, as is done in raft/distance/detail/distance.cuh
-// and the specializations in src/distance/distance/specializations/detail/.
+// and src/distance/detail/pairwise_matrix/dispatch_*.cu.
 
 namespace raft::distance::detail {
 
 // This forward-declaration ensures that we do not need to include
 // dispatch_sm80.cuh if we are not calling it in practice. This makes compiling
-// all the non-CUTLASS based distance specializations faster. For CUTLASS-based
+// all the non-CUTLASS based distance instantiations faster. For CUTLASS-based
 // distances, dispatch_sm80.cuh has to be included by the file including this
 // file.
 template <typename OpT,
@@ -60,11 +60,35 @@ void pairwise_matrix_sm80_dispatch(OpT,
                                    SM_compat_t,
                                    cudaStream_t);
 
-template <typename OpT, typename IdxT, typename DataT, typename OutT, typename FinOpT>
-void pairwise_matrix_instantiation_point(OpT distance_op,
-                                         pairwise_matrix_params<IdxT, DataT, OutT, FinOpT> params,
-                                         cudaStream_t stream)
+template <typename OpT,
+          typename DataT,
+          typename AccT,
+          typename OutT,
+          typename FinOpT,
+          typename IdxT = int>
+void pairwise_matrix_dispatch(OpT distance_op,
+                              IdxT m,
+                              IdxT n,
+                              IdxT k,
+                              const DataT* x,
+                              const DataT* y,
+                              const DataT* x_norm,
+                              const DataT* y_norm,
+                              OutT* out,
+                              FinOpT fin_op,
+                              cudaStream_t stream,
+                              bool is_row_major)
 {
+  // Create kernel parameter struct. Flip x and y if column major.
+  IdxT ldx    = is_row_major ? k : m;
+  IdxT ldy    = is_row_major ? k : n;
+  IdxT ld_out = is_row_major ? n : m;
+
+  pairwise_matrix_params<IdxT, DataT, OutT, FinOpT> params{
+    m, n, k, ldx, ldy, ld_out, x, y, x_norm, y_norm, out, fin_op, is_row_major};
+
+  if (!params.is_row_major) { params.flip_x_and_y(); }
+
   // On CUDA 12:
   // - always execute normal kernel
   //
@@ -101,37 +125,6 @@ void pairwise_matrix_instantiation_point(OpT distance_op,
       sm60_wrapper.launch(distance_op, params, stream);
     }
   }
-}
-
-template <typename OpT,
-          typename DataT,
-          typename AccT,
-          typename OutT,
-          typename FinOpT,
-          typename IdxT = int>
-void pairwise_matrix_dispatch(OpT distance_op,
-                              IdxT m,
-                              IdxT n,
-                              IdxT k,
-                              const DataT* x,
-                              const DataT* y,
-                              const DataT* x_norm,
-                              const DataT* y_norm,
-                              OutT* out,
-                              FinOpT fin_op,
-                              cudaStream_t stream,
-                              bool is_row_major)
-{
-  // Create kernel parameter struct. Flip x and y if column major.
-  IdxT ldx    = is_row_major ? k : m;
-  IdxT ldy    = is_row_major ? k : n;
-  IdxT ld_out = is_row_major ? n : m;
-
-  pairwise_matrix_params<IdxT, DataT, OutT, FinOpT> params{
-    m, n, k, ldx, ldy, ld_out, x, y, x_norm, y_norm, out, fin_op, is_row_major};
-
-  if (!params.is_row_major) { params.flip_x_and_y(); }
-  pairwise_matrix_instantiation_point(distance_op, params, stream);
 }
 
 };  // namespace raft::distance::detail
