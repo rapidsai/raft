a2e5608493dbec6b7924bb6ff39d716fb34f7167
akifcorduk <akifcorduk@gmail.com>
2023-04-28 02:10:46 -0700

    fix style include order

--- cpp/include/raft/util/device_loads_stores.cuh
+++ cpp/include/raft/util/device_loads_stores.cuh
@@ -17,8 +17,8 @@
 #pragma once
 
 #include <cstdint>                            // uintX_t
-#include <raft/util/cuda_dev_essentials.cuh>  // DI
 #include <raft/core/device_span.hpp>
+#include <raft/util/cuda_dev_essentials.cuh>  // DI
 
 namespace raft {
 
