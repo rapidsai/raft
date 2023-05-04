3fec2355008b6113c8bc3f096042e6dc60de85cc
akifcorduk <akifcorduk@gmail.com>
2023-05-04 07:24:51 -0700

    Add detailed comment on blockWeightedSelect

--- cpp/include/raft/util/reduction.cuh
+++ cpp/include/raft/util/reduction.cuh
@@ -132,6 +132,15 @@ DI T warpWeightedSelect(rng_t& rng, T& weight, i_t& idx)
  * @brief 1-D block-level weighted selection of an index.
  * It selects an index with the given discrete probability
  * distribution(represented by weights of each index)
+ *
+ * Let w_i be the weight stored on thread i.  We calculate the cumulative distribution function
+ * F_i = \sum_{k=0..i} weight_i.
+ * Sequentially, we could select one of the elements with with the desired probability using the
+ * following method. We can consider that each element has a subinterval assigned: [F_{i-1}, F_i).
+ * We generate a uniform random number in the [0, F_i) range, and check which subinterval it falls.
+ * We return idx corresponding to the selected subinterval.
+ * In parallel, we do a tree reduction and make a selection at every step when we combine two
+ * values.
  * @param rng random number generator, must have next_u32() function
  * @param shbuf shared memory region needed for storing intermediate results. It
  *             must alteast be of size: `(sizeof(T) + sizeof(i_t)) * WarpSize`
