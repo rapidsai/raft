0aa4d26ad113f880b1cac66223329d766a18dae6
Alexandr Guzhva <alexanderguzhva@gmail.com>
2023-05-17 21:59:40 -0400

    fix code style

--- cpp/include/raft/neighbors/detail/ivf_pq_compute_similarity-ext.cuh
+++ cpp/include/raft/neighbors/detail/ivf_pq_compute_similarity-ext.cuh
@@ -180,16 +180,27 @@ auto compute_similarity_select(const cudaDeviceProp& dev_props,
 
 #define COMMA ,
 instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
-  half, raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA false>, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+  half,
+  raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA false>,
+  raft::neighbors::ivf_pq::detail::NoneSampleFilter);
 instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
-  half, raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA true>, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
-instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(half, half, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
-instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(float, half, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
-instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(float, float, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+  half,
+  raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA true>,
+  raft::neighbors::ivf_pq::detail::NoneSampleFilter);
 instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
-  float, raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA false>, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+  half, half, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
 instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
-  float, raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA true>, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+  float, half, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
+  float, float, raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
+  float,
+  raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA false>,
+  raft::neighbors::ivf_pq::detail::NoneSampleFilter);
+instantiate_raft_neighbors_ivf_pq_detail_compute_similarity_select(
+  float,
+  raft::neighbors::ivf_pq::detail::fp_8bit<5u COMMA true>,
+  raft::neighbors::ivf_pq::detail::NoneSampleFilter);
 
 #undef COMMA
 
