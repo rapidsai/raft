14d505adce5128adc8310291e86fa564c6e750ab
Bradley Dice <bdice@bradleydice.com>
2023-05-30 14:33:58 -0500

    Add dash with arbitrary characters before space to allow YAML anchors.

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -61,7 +61,7 @@ DEPENDENCIES=(
 )
 for FILE in dependencies.yaml conda/environments/*.yaml; do
   for DEP in "${DEPENDENCIES[@]}"; do
-    sed_runner "/ ${DEP}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}\.*/g" ${FILE};
+    sed_runner "/-.* ${DEP}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}\.*/g" ${FILE};
   done
   sed_runner "/ ucx-py==/ s/==.*/==${NEXT_UCX_PY_SHORT_TAG_PEP440}\.*/g" ${FILE};
 done
