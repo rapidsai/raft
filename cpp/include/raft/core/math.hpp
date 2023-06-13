9f7a0736b581a6d299a60f38d2c3811e7bacd006
Nicolas Blin <nicolas.blin7@hotmail.fr>
2023-06-13 07:49:00 -0700

    add missing _RAFT_HAS_CUDA

--- cpp/include/raft/core/math.hpp
+++ cpp/include/raft/core/math.hpp
@@ -49,18 +49,21 @@ RAFT_INLINE_FUNCTION auto abs(T x)
 template <typename T>
 constexpr RAFT_INLINE_FUNCTION auto abs(T x)
   -> std::enable_if_t<!std::is_same_v<float, T> && !std::is_same_v<double, T> &&
+#if defined(_RAFT_HAS_CUDA)
                         !std::is_same_v<__half, T> && !std::is_same_v<nv_bfloat16, T> &&
+#endif
                         !std::is_same_v<int, T> && !std::is_same_v<long int, T> &&
                         !std::is_same_v<long long int, T>,
                       T>
 {
   return x < T{0} ? -x : x;
 }
+#if defined(_RAFT_HAS_CUDA)
 template <typename T>
 RAFT_DEVICE_INLINE_FUNCTION typename std::enable_if_t<std::is_same_v<T, __half>, __half> abs(T x)
 {
 #if (__CUDA_ARCH__ >= 530)
-  return __habs(x);
+  return ::__habs(x);
 #else
   // Fail during template instantiation if the compute capability doesn't support this operation
   static_assert(sizeof(T) != sizeof(T), "__half is only supported on __CUDA_ARCH__ >= 530");
@@ -73,13 +76,14 @@ RAFT_DEVICE_INLINE_FUNCTION typename std::enable_if_t<std::is_same_v<T, nv_bfloa
 abs(T x)
 {
 #if (__CUDA_ARCH__ >= 800)
-  return __habs(x);
+  return ::__habs(x);
 #else
   // Fail during template instantiation if the compute capability doesn't support this operation
   static_assert(sizeof(T) != sizeof(T), "nv_bfloat16 is only supported on __CUDA_ARCH__ >= 800");
   return T{};
 #endif
 }
+#endif
 /** @} */
 
 /**
