40ea1ca25cc4b8cd7662933cb029575be2da3492
Vibhu Jawa <vibhujawa@gmail.com>
2023-06-13 12:36:34 -0700

    Add scheduler_file argument to support MNMG setup

--- python/raft-dask/raft_dask/test/conftest.py
+++ python/raft-dask/raft_dask/test/conftest.py
@@ -1,54 +1,54 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 import os
 
 import pytest
 
 from dask.distributed import Client
-from dask_cuda import LocalCUDACluster, initialize
+from dask_cuda import LocalCUDACluster
 
 os.environ["UCX_LOG_LEVEL"] = "error"
 
 
-enable_tcp_over_ucx = True
-enable_nvlink = False
-enable_infiniband = False
-
-
 @pytest.fixture(scope="session")
 def cluster():
-    cluster = LocalCUDACluster(protocol="tcp", scheduler_port=0)
-    yield cluster
-    cluster.close()
+    scheduler_file = os.environ.get("SCHEDULER_FILE")
+    if scheduler_file:
+        return scheduler_file
+    else:
+        cluster = LocalCUDACluster(protocol="tcp", scheduler_port=0)
+        yield cluster
+        cluster.close()
 
 
 @pytest.fixture(scope="session")
 def ucx_cluster():
-    initialize.initialize(
-        create_cuda_context=True,
-        enable_tcp_over_ucx=enable_tcp_over_ucx,
-        enable_nvlink=enable_nvlink,
-        enable_infiniband=enable_infiniband,
-    )
-    cluster = LocalCUDACluster(
-        protocol="ucx",
-        enable_tcp_over_ucx=enable_tcp_over_ucx,
-        enable_nvlink=enable_nvlink,
-        enable_infiniband=enable_infiniband,
-    )
-    yield cluster
-    cluster.close()
+    scheduler_file = os.environ.get("SCHEDULER_FILE")
+    if scheduler_file:
+        return scheduler_file
+    else:
+        cluster = LocalCUDACluster(
+            protocol="ucx",
+        )
+        yield cluster
+        cluster.close()
 
 
 @pytest.fixture(scope="session")
 def client(cluster):
-    client = Client(cluster)
+    if isinstance(cluster, LocalCUDACluster):
+        client = Client(cluster)
+    else:
+        client = Client(scheduler_file=cluster)
     yield client
     client.close()
 
 
 @pytest.fixture()
 def ucx_client(ucx_cluster):
-    client = Client(cluster)
+    if isinstance(ucx_cluster, LocalCUDACluster):
+        client = Client(ucx_cluster)
+    else:
+        client = Client(scheduler_file=cluster)
     yield client
     client.close()
