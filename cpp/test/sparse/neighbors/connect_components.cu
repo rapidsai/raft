d9293785dcf3018acaafda8fc482a0794b242940
Tarang Jain <jaintarang2015@gmail.com>
2023-06-20 17:42:20 -0700

    Update todo

--- cpp/test/sparse/neighbors/connect_components.cu
+++ cpp/test/sparse/neighbors/connect_components.cu
@@ -14,13 +14,15 @@
  * limitations under the License.
  */
 
-// XXX: We allow the instantiation of fused_l2_nn here:
-// raft::linkage::FixConnectivitiesRedOp<value_idx, value_t> red_op(colors.data(), params.n_row);
+// XXX: We allow the instantiation of masked_l2_nn here:
+// raft::linkage::FixConnectivitiesRedOp<value_idx, value_t> red_op(params.n_row);
 // raft::linkage::connect_components<value_idx, value_t>(
 //   handle, out_edges, data.data(), colors.data(), params.n_row, params.n_col, red_op);
 //
 // TODO: consider adding this to libraft.so or creating an instance in a
 // separate translation unit for this test.
+//
+// TODO: edge case testing
 #undef RAFT_EXPLICIT_INSTANTIATE_ONLY
 
 #include <gtest/gtest.h>
