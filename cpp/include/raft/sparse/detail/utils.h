735174e2b424c8f2255bc4840e3192ea15c65d93
viclafargue <viclafargue@nvidia.com>
2023-07-07 14:13:16 +0200

    Fix sparse KNN for large batches

--- cpp/include/raft/sparse/detail/utils.h
+++ cpp/include/raft/sparse/detail/utils.h
@@ -90,7 +90,8 @@ __global__ void iota_fill_block_kernel(value_idx* indices, value_idx ncols)
   int tid = threadIdx.x;
 
   for (int i = tid; i < ncols; i += blockDim.x) {
-    indices[row * ncols + i] = i;
+    uint64_t idx = (uint64_t)row * (uint64_t)ncols;
+    indices[idx + i] = i;
   }
 }
 
