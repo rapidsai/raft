24223ed8721a118fc2b106c00e604bc90fdfb9d5
William Hicks <whicks@nvidia.com>
2023-07-11 11:46:32 -0400

    Add infrastructure for CUDA-free build

--- cpp/include/raft/core/stream_view.hpp
+++ cpp/include/raft/core/stream_view.hpp
@@ -13,9 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <raft/core/logger.hpp>
+#include <raft/core/cuda_support.hpp>
 #include <raft/core/error.hpp>
+#include <raft/core/logger.hpp>
 #ifndef RAFT_DISABLE_CUDA
+#include <raft/core/interruptible.hpp>
 #include <rmm/cuda_stream_view.hpp>
 #endif
 
@@ -23,30 +25,21 @@ namespace raft {
 
 namespace detail {
 struct fail_stream_view {
-  constexpr fail_stream_view(fail_stream_view const&) = default;
-  constexpr fail_stream_view(fail_stream_view&&) = default;
+  constexpr fail_stream_view()                                           = default;
+  constexpr fail_stream_view(fail_stream_view const&)                    = default;
+  constexpr fail_stream_view(fail_stream_view&&)                         = default;
   auto constexpr operator=(fail_stream_view const&) -> fail_stream_view& = default;
-  auto constexpr operator=(fail_stream_view&&) -> fail_stream_view& = default;
-  auto value() {
-    throw non_cuda_build_error{
-      "Attempted to access CUDA stream in non-CUDA build"
-    };
+  auto constexpr operator=(fail_stream_view&&) -> fail_stream_view&      = default;
+  auto value() { throw non_cuda_build_error{"Attempted to access CUDA stream in non-CUDA build"}; }
+  [[nodiscard]] auto is_per_thread_default() const { return false; }
+  [[nodiscard]] auto is_default() const { return false; }
+  void synchronize() const
+  {
+    throw non_cuda_build_error{"Attempted to sync CUDA stream in non-CUDA build"};
   }
-  [[nodiscard]] auto is_per_thread_default() const {
-    return false;
-  }
-  [[nodiscard]] auto is_default() const {
-    return false;
-  }
-  void synchronize() const {
-    throw non_cuda_build_error{
-      "Attempted to sync CUDA stream in non-CUDA build"
-    };
-  }
-  void synchronize_no_throw() const {
-    RAFT_LOG_ERROR(
-      "Attempted to sync CUDA stream in non-CUDA build"
-    );
+  void synchronize_no_throw() const
+  {
+    RAFT_LOG_ERROR("Attempted to sync CUDA stream in non-CUDA build");
   }
 };
 }  // namespace detail
@@ -66,39 +59,51 @@ struct stream_view {
 #else
   using underlying_view_type = detail::fail_stream_view;
 #endif
-  constexpr stream_view(stream_view const&) = default;
-  constexpr stream_view(stream_view&&) = default;
-  auto operator=(stream_view const&) -> stream_view& = default;
-  auto operator=(stream_view&&) -> stream_view& = default;
-  auto value() {
-    return base_view_.value();
-  }
-  operator underlying_view_type() const noexcept {
-    return base_view_;
-  }
-  [[nodiscard]] auto is_per_thread_default() const {
-    return base_view_.is_per_thread_default();
-  }
-  [[nodiscard]] auto is_default() const {
-    return base_view_.is_default();
-  }
-  void synchronize() const {
-    base_view_.synchronize();
-  }
-  void synchronize_no_throw() const {
-    base_view_.synchronize_no_throw();
-  }
 
-  auto underlying() {
-    return base_view_;
+  constexpr stream_view(underlying_view_type base_view = stream_view::get_underlying_per_thread_default())
+    : base_view_{base_view}
+  {
   }
-  void synchronize_if_cuda() {
+  constexpr stream_view(stream_view const&)          = default;
+  constexpr stream_view(stream_view&&)               = default;
+  auto operator=(stream_view const&) -> stream_view& = default;
+  auto operator=(stream_view&&) -> stream_view&      = default;
+  auto value() { return base_view_.value(); }
+  operator underlying_view_type() const noexcept { return base_view_; }
+  [[nodiscard]] auto is_per_thread_default() const { return base_view_.is_per_thread_default(); }
+  [[nodiscard]] auto is_default() const { return base_view_.is_default(); }
+  void synchronize() const { base_view_.synchronize(); }
+  void synchronize_no_throw() const { base_view_.synchronize_no_throw(); }
+  void interruptible_synchronize() const
+  {
 #ifndef RAFT_DISABLE_CUDA
-    base_view_.synchronize();
+    interruptible::synchronize(base_view_);
+#else
+    synchronize();
 #endif
   }
+
+  auto underlying() { return base_view_; }
+  void synchronize_if_cuda_enabled()
+  {
+    if constexpr (raft::CUDA_ENABLED) {
+      base_view_.synchronize();
+    }
+  }
+
  private:
   underlying_view_type base_view_;
+  auto static get_underlying_per_thread_default() -> underlying_view_type
+  {
+#ifndef RAFT_DISABLE_CUDA
+    return rmm::cuda_stream_per_thread;
+#else
+    auto static constexpr const default_fail_stream = underlying_view_type{};
+    return default_fail_stream;
+#endif
+  }
 };
+
+auto static const stream_view_per_thread = stream_view{};
 
 }  // namespace raft
