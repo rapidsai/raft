c9c8f523016d6ad668b19bc5edb4e3ec9f89d61d
Divye Gala <divyegala@gmail.com>
2023-07-13 12:35:30 -0400

    Remove guard for uploading wheels

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -53,7 +53,4 @@ if [[ ! -d "/tmp/gha-tools" ]]; then
   git clone https://github.com/divyegala/gha-tools.git -b wheel-local-runs /tmp/gha-tools
 fi
 
-# do not upload wheels to S3 in local runs
-if [ "${CI:-false}" = "false" ]; then
-  /tmp/gha-tools/tools/rapids-upload-wheels-to-s3 final_dist
-fi
+/tmp/gha-tools/tools/rapids-upload-wheels-to-s3 final_dist
