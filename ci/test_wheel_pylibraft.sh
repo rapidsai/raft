ead668df67b078b500e114a9f780b56af0614fb3
Divye Gala <divyegala@gmail.com>
2023-07-20 19:38:47 -0400

    Apply suggestions from code review
    Co-authored-by: AJ Schmidt <ajschmidt8@users.noreply.github.com>

--- ci/test_wheel_pylibraft.sh
+++ ci/test_wheel_pylibraft.sh
@@ -8,7 +8,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="pylibraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install -v $(echo ./dist/pylibraft*.whl)[test]
+python -m pip install $(echo ./dist/pylibraft*.whl)[test]
 
 # Run smoke tests for aarch64 pull requests
 if [[ "$(arch)" == "aarch64" && "${RAPIDS_BUILD_TYPE}" == "pull-request" ]]; then
