4f33ba84831626feb3da3cd50cf51c9937e6cef1
Corey J. Nolet <cjnolet@gmail.com>
2023-08-29 15:19:09 -0400

    Static library codna recipe

--- conda/recipes/libraft/build_libraft_static.sh
+++ conda/recipes/libraft/build_libraft_static.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
-./build.sh libraft --allgpuarch --compile-lib --build-metrics=compile_lib_static --incl-cache-stats --no-nvtx --cmake-args=\"-DBUILD_SHARED_LIBS=OFF\"
+./build.sh libraft --allgpuarch --compile-static-lib --build-metrics=compile_lib_static --incl-cache-stats --no-nvtx
