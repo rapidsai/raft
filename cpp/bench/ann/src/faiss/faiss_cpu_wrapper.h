bded6749736d6de3daaba265d5da20946be1a80e
Corey J. Nolet <cjnolet@gmail.com>
2023-09-11 16:21:01 -0400

    Adding FAISS cpu to raft-ann-bench

--- cpp/bench/ann/src/faiss/faiss_cpu_wrapper.h
+++ cpp/bench/ann/src/faiss/faiss_cpu_wrapper.h
@@ -14,22 +14,16 @@
  * limitations under the License.
  */
 #pragma once
+
 #include "../common/ann_types.hpp"
 
 #include <raft/core/logger.hpp>
-#include <raft/util/cudart_utils.hpp>
 
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFFlat.h>
 #include <faiss/IndexIVFPQ.h>
 #include <faiss/IndexRefine.h>
 #include <faiss/IndexScalarQuantizer.h>
-#include <faiss/gpu/GpuIndexFlat.h>
-#include <faiss/gpu/GpuIndexIVFFlat.h>
-#include <faiss/gpu/GpuIndexIVFPQ.h>
-#include <faiss/gpu/GpuIndexIVFScalarQuantizer.h>
-#include <faiss/gpu/StandardGpuResources.h>
-#include <faiss/impl/ScalarQuantizer.h>
 #include <faiss/index_io.h>
 #include <omp.h>
 
@@ -76,7 +70,7 @@ class OmpSingleThreadScope {
 namespace raft::bench::ann {
 
 template <typename T>
-class FaissGpu : public ANN<T> {
+class Faiss : public ANN<T> {
  public:
   using typename ANN<T>::AnnSearchParam;
   struct SearchParam : public AnnSearchParam {
@@ -89,19 +83,16 @@ class FaissGpu : public ANN<T> {
     int ratio = 2;
   };
 
-  FaissGpu(Metric metric, int dim, const BuildParam& param)
+  Faiss(Metric metric, int dim, const BuildParam& param)
     : ANN<T>(metric, dim),
       metric_type_(parse_metric_type(metric)),
       nlist_{param.nlist},
       training_sample_fraction_{1.0 / double(param.ratio)}
   {
     static_assert(std::is_same_v<T, float>, "faiss support only float type");
-    RAFT_CUDA_TRY(cudaGetDevice(&device_));
-    RAFT_CUDA_TRY(cudaEventCreate(&sync_, cudaEventDisableTiming));
-    faiss_default_stream_ = gpu_resource_.getDefaultStream(device_);
   }
 
-  virtual ~FaissGpu() noexcept { RAFT_CUDA_TRY_NO_THROW(cudaEventDestroy(sync_)); }
+  virtual ~Faiss() noexcept {}
 
   void build(const T* dataset, size_t nrow, cudaStream_t stream = 0) final;
 
@@ -119,41 +110,31 @@ class FaissGpu : public ANN<T> {
   AlgoProperty get_preference() const override
   {
     AlgoProperty property;
-    // to enable building big dataset which is larger than GPU memory
+    // to enable building big dataset which is larger than  memory
     property.dataset_memory_type = MemoryType::Host;
-    property.query_memory_type   = MemoryType::Device;
+    property.query_memory_type   = MemoryType::Host;
     return property;
   }
 
  protected:
-  template <typename GpuIndex, typename CpuIndex>
+  template <typename Index, typename CpuIndex>
   void save_(const std::string& file) const;
 
-  template <typename GpuIndex, typename CpuIndex>
+  template <typename Index, typename CpuIndex>
   void load_(const std::string& file);
 
-  void stream_wait(cudaStream_t stream) const
-  {
-    RAFT_CUDA_TRY(cudaEventRecord(sync_, faiss_default_stream_));
-    RAFT_CUDA_TRY(cudaStreamWaitEvent(stream, sync_));
-  }
-
-  mutable faiss::gpu::StandardGpuResources gpu_resource_;
-  std::unique_ptr<faiss::gpu::GpuIndex> index_;
+  std::unique_ptr<faiss::Index> index_;
   std::unique_ptr<faiss::IndexRefineFlat> index_refine_;
   faiss::MetricType metric_type_;
   int nlist_;
-  int device_;
-  cudaEvent_t sync_{nullptr};
-  cudaStream_t faiss_default_stream_{nullptr};
   double training_sample_fraction_;
 };
 
 template <typename T>
-void FaissGpu<T>::build(const T* dataset, size_t nrow, cudaStream_t stream)
+void Faiss<T>::build(const T* dataset, size_t nrow, cudaStream_t stream)
 {
   OmpSingleThreadScope omp_single_thread;
-  auto index_ivf = dynamic_cast<faiss::gpu::GpuIndexIVF*>(index_.get());
+  auto index_ivf = dynamic_cast<faiss::IndexIVF*>(index_.get());
   if (index_ivf != nullptr) {
     // set the min/max training size for clustering to use the whole provided training set.
     double trainset_size       = training_sample_fraction_ * static_cast<double>(nrow);
@@ -175,19 +156,19 @@ void FaissGpu<T>::build(const T* dataset, size_t nrow, cudaStream_t stream)
     index_ivf->cp.max_points_per_centroid = max_ppc;
     index_ivf->cp.min_points_per_centroid = min_ppc;
   }
-  index_->train(nrow, dataset);  // faiss::gpu::GpuIndexFlat::train() will do nothing
+  index_->train(nrow, dataset);  // faiss::IndexFlat::train() will do nothing
   assert(index_->is_trained);
   index_->add(nrow, dataset);
   stream_wait(stream);
 }
 
 template <typename T>
-void FaissGpu<T>::set_search_param(const AnnSearchParam& param)
+void Faiss<T>::set_search_param(const AnnSearchParam& param)
 {
   auto search_param = dynamic_cast<const SearchParam&>(param);
   int nprobe        = search_param.nprobe;
   assert(nprobe <= nlist_);
-  dynamic_cast<faiss::gpu::GpuIndexIVF*>(index_.get())->setNumProbes(nprobe);
+  dynamic_cast<faiss::IndexIVF*>(index_.get())->setNumProbes(nprobe);
 
   if (search_param.refine_ratio > 1.0) {
     this->index_refine_ = std::make_unique<faiss::IndexRefineFlat>(this->index_.get());
@@ -196,108 +177,104 @@ void FaissGpu<T>::set_search_param(const AnnSearchParam& param)
 }
 
 template <typename T>
-void FaissGpu<T>::search(const T* queries,
-                         int batch_size,
-                         int k,
-                         size_t* neighbors,
-                         float* distances,
-                         cudaStream_t stream) const
+void Faiss<T>::search(const T* queries,
+                      int batch_size,
+                      int k,
+                      size_t* neighbors,
+                      float* distances,
+                      cudaStream_t stream) const
 {
   static_assert(sizeof(size_t) == sizeof(faiss::Index::idx_t),
                 "sizes of size_t and faiss::Index::idx_t are different");
   index_->search(
     batch_size, queries, k, distances, reinterpret_cast<faiss::Index::idx_t*>(neighbors));
-  stream_wait(stream);
 }
 
 template <typename T>
-template <typename GpuIndex, typename CpuIndex>
-void FaissGpu<T>::save_(const std::string& file) const
+template <typename Index, typename CpuIndex>
+void Faiss<T>::save_(const std::string& file) const
 {
   OmpSingleThreadScope omp_single_thread;
 
   auto cpu_index = std::make_unique<CpuIndex>();
-  dynamic_cast<GpuIndex*>(index_.get())->copyTo(cpu_index.get());
+  dynamic_cast<Index*>(index_.get())->copyTo(cpu_index.get());
   faiss::write_index(cpu_index.get(), file.c_str());
 }
 
 template <typename T>
-template <typename GpuIndex, typename CpuIndex>
-void FaissGpu<T>::load_(const std::string& file)
+template <typename Index>
+void Faiss<T>::load_(const std::string& file)
 {
   OmpSingleThreadScope omp_single_thread;
 
-  std::unique_ptr<CpuIndex> cpu_index(dynamic_cast<CpuIndex*>(faiss::read_index(file.c_str())));
-  assert(cpu_index);
-  dynamic_cast<GpuIndex*>(index_.get())->copyFrom(cpu_index.get());
+  index_ = std::make_unique<Index>(dynamic_cast<CpuIndex*>(faiss::read_index(file.c_str())));
 }
 
 template <typename T>
-class FaissGpuIVFFlat : public FaissGpu<T> {
+class FaissIVFFlat : public Faiss<T> {
  public:
-  using typename FaissGpu<T>::BuildParam;
+  using typename Faiss<T>::BuildParam;
 
-  FaissGpuIVFFlat(Metric metric, int dim, const BuildParam& param) : FaissGpu<T>(metric, dim, param)
+  FaissIVFFlat(Metric metric, int dim, const BuildParam& param) : Faiss<T>(metric, dim, param)
   {
-    faiss::gpu::GpuIndexIVFFlatConfig config;
+    faiss::IndexIVFFlatConfig config;
     config.device = this->device_;
-    this->index_  = std::make_unique<faiss::gpu::GpuIndexIVFFlat>(
-      &(this->gpu_resource_), dim, param.nlist, this->metric_type_, config);
+    this->index_  = std::make_unique<faiss::IndexIVFFlat>(
+      &(this->_resource_), dim, param.nlist, this->metric_type_, config);
   }
 
   void save(const std::string& file) const override
   {
-    this->template save_<faiss::gpu::GpuIndexIVFFlat, faiss::IndexIVFFlat>(file);
+    this->template save_<faiss::IndexIVFFlat, faiss::IndexIVFFlat>(file);
   }
   void load(const std::string& file) override
   {
-    this->template load_<faiss::gpu::GpuIndexIVFFlat, faiss::IndexIVFFlat>(file);
+    this->template load_<faiss::IndexIVFFlat, faiss::IndexIVFFlat>(file);
   }
 };
 
 template <typename T>
-class FaissGpuIVFPQ : public FaissGpu<T> {
+class FaissIVFPQ : public Faiss<T> {
  public:
-  struct BuildParam : public FaissGpu<T>::BuildParam {
+  struct BuildParam : public Faiss<T>::BuildParam {
     int M;
     bool useFloat16;
     bool usePrecomputed;
   };
 
-  FaissGpuIVFPQ(Metric metric, int dim, const BuildParam& param) : FaissGpu<T>(metric, dim, param)
+  FaissIVFPQ(Metric metric, int dim, const BuildParam& param) : Faiss<T>(metric, dim, param)
   {
-    faiss::gpu::GpuIndexIVFPQConfig config;
+    faiss::IndexIVFPQConfig config;
     config.useFloat16LookupTables = param.useFloat16;
     config.usePrecomputedTables   = param.usePrecomputed;
     config.device                 = this->device_;
-    this->index_ =
-      std::make_unique<faiss::gpu::GpuIndexIVFPQ>(&(this->gpu_resource_),
-                                                  dim,
-                                                  param.nlist,
-                                                  param.M,
-                                                  8,  // FAISS only supports bitsPerCode=8
-                                                  this->metric_type_,
-                                                  config);
+    this->index_                  = std::make_unique<faiss::IndexIVFPQ>(&(this->_resource_),
+                                                       dim,
+                                                       param.nlist,
+                                                       param.M,
+                                                       8,  // FAISS only supports bitsPerCode=8
+                                                       this->metric_type_,
+                                                       config);
   }
 
   void save(const std::string& file) const override
   {
-    this->template save_<faiss::gpu::GpuIndexIVFPQ, faiss::IndexIVFPQ>(file);
+    this->template save_<faiss::IndexIVFPQ, faiss::IndexIVFPQ>(file);
   }
   void load(const std::string& file) override
   {
-    this->template load_<faiss::gpu::GpuIndexIVFPQ, faiss::IndexIVFPQ>(file);
+    this->template load_<faiss::IndexIVFPQ, faiss::IndexIVFPQ>(file);
   }
 };
 
 template <typename T>
-class FaissGpuIVFSQ : public FaissGpu<T> {
+class FaissIVFSQ : public Faiss<T> {
  public:
-  struct BuildParam : public FaissGpu<T>::BuildParam {
+  struct BuildParam : public Faiss<T>::BuildParam {
     std::string quantizer_type;
   };
 
-  FaissGpuIVFSQ(Metric metric, int dim, const BuildParam& param) : FaissGpu<T>(metric, dim, param)
+  FaissIVFSQ(Metric metric, int dim, const BuildParam& param) : Faiss<T>(metric, dim, param)
   {
     faiss::ScalarQuantizer::QuantizerType qtype;
     if (param.quantizer_type == "fp16") {
@@ -305,50 +282,47 @@ class FaissGpuIVFSQ : public FaissGpu<T> {
     } else if (param.quantizer_type == "int8") {
       qtype = faiss::ScalarQuantizer::QT_8bit;
     } else {
-      throw std::runtime_error("FaissGpuIVFSQ supports only fp16 and int8 but got " +
+      throw std::runtime_error("FaissIVFSQ supports only fp16 and int8 but got " +
                                param.quantizer_type);
     }
 
-    faiss::gpu::GpuIndexIVFScalarQuantizerConfig config;
+    faiss::IndexIVFScalarQuantizerConfig config;
     config.device = this->device_;
-    this->index_  = std::make_unique<faiss::gpu::GpuIndexIVFScalarQuantizer>(
-      &(this->gpu_resource_), dim, param.nlist, qtype, this->metric_type_, true, config);
+    this->index_  = std::make_unique<faiss::IndexIVFScalarQuantizer>(
+      &(this->_resource_), dim, param.nlist, qtype, this->metric_type_, true, config);
   }
 
   void save(const std::string& file) const override
   {
-    this->template save_<faiss::gpu::GpuIndexIVFScalarQuantizer, faiss::IndexIVFScalarQuantizer>(
-      file);
+    this->template save_<faiss::IndexIVFScalarQuantizer, faiss::IndexIVFScalarQuantizer>(file);
   }
   void load(const std::string& file) override
   {
-    this->template load_<faiss::gpu::GpuIndexIVFScalarQuantizer, faiss::IndexIVFScalarQuantizer>(
-      file);
+    this->template load_<faiss::IndexIVFScalarQuantizer, faiss::IndexIVFScalarQuantizer>(file);
   }
 };
 
 template <typename T>
-class FaissGpuFlat : public FaissGpu<T> {
+class FaissFlat : public Faiss<T> {
  public:
-  FaissGpuFlat(Metric metric, int dim)
-    : FaissGpu<T>(metric, dim, typename FaissGpu<T>::BuildParam{})
+  FaissFlat(Metric metric, int dim) : Faiss<T>(metric, dim, typename Faiss<T>::BuildParam{})
   {
-    faiss::gpu::GpuIndexFlatConfig config;
+    faiss::IndexFlatConfig config;
     config.device = this->device_;
-    this->index_  = std::make_unique<faiss::gpu::GpuIndexFlat>(
-      &(this->gpu_resource_), dim, this->metric_type_, config);
+    this->index_ =
+      std::make_unique<faiss::IndexFlat>(&(this->_resource_), dim, this->metric_type_, config);
   }
 
-  // class FaissGpu is more like a IVF class, so need special treating here
+  // class Faiss is more like a IVF class, so need special treating here
   void set_search_param(const typename ANN<T>::AnnSearchParam&) override{};
 
   void save(const std::string& file) const override
   {
-    this->template save_<faiss::gpu::GpuIndexFlat, faiss::IndexFlat>(file);
+    this->template save_<faiss::IndexFlat, faiss::IndexFlat>(file);
   }
   void load(const std::string& file) override
   {
-    this->template load_<faiss::gpu::GpuIndexFlat, faiss::IndexFlat>(file);
+    this->template load_<faiss::IndexFlat, faiss::IndexFlat>(file);
   }
 };
 
