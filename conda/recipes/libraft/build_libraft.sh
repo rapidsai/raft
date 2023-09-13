9585f20e6ab3560c70c1d49a7e05512dc26b06ba
Corey J. Nolet <cjnolet@gmail.com>
2023-09-13 17:04:00 -0400

    Using selectors for faiss

--- conda/recipes/libraft/build_libraft.sh
+++ conda/recipes/libraft/build_libraft.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
-./build.sh libraft -v --allgpuarch --compile-lib --build-metrics=compile_lib --incl-cache-stats --no-nvtx
+./build.sh libraft --allgpuarch --compile-lib --build-metrics=compile_lib --incl-cache-stats --no-nvtx
