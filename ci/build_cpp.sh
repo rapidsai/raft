7785f9649c71342790e9530beba4308c985e9484
Bradley Dice <bdice@bradleydice.com>
2023-09-13 15:02:56 -0500

    Attempt removing dependency on conda-forge channel.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 set -euo pipefail
 
 source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
+
+grep -v "conda-forge" /opt/conda/.condarc > /opt/conda/.condarc.tmp
+mv /opt/conda/.condarc.tmp /opt/conda/.condarc
+echo "Contents of /opt/conda/.condarc:"
+cat /opt/conda/.condarc
 
 rapids-print-env
 
