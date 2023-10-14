355d9a2f1dc3a766f45c702e35b1c9b325b6859c
Bradley Dice <bdice@bradleydice.com>
2023-10-13 22:48:41 -0500

    Build with libcudacxx 2.1.0.

--- cpp/template/cmake/thirdparty/fetch_rapids.cmake
+++ cpp/template/cmake/thirdparty/fetch_rapids.cmake
@@ -14,6 +14,8 @@
 # Use this variable to update RAPIDS and RAFT versions
 set(RAPIDS_VERSION "23.12")
 
+set(rapids-cmake-repo bdice/rapids-cmake)
+set(rapids-cmake-branch libcudacxx-2.1.0)
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/RAFT_RAPIDS.cmake)
     file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_VERSION}/RAPIDS.cmake
             ${CMAKE_CURRENT_BINARY_DIR}/RAFT_RAPIDS.cmake)
