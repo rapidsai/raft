0d6597d425ca673d358c045241fa722a47807b10
Divye Gala <divyegala@gmail.com>
2023-10-31 09:18:14 -0400

    Add YAML config files to run parameter sweeps for ANN benchmarks (#1929)
    Also generate CUDA 12.0 development environments for `bench-ann`
    
    Authors:
      - Divye Gala (https://github.com/divyegala)
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Corey J. Nolet (https://github.com/cjnolet)
      - Ray Douglass (https://github.com/raydouglass)
    
    URL: https://github.com/rapidsai/raft/pull/1929

---
+++ python/raft-ann-bench/src/raft-ann-bench/validators/__init__.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2023, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DTYPE_SIZES = {"float": 4, "half": 2, "fp8": 1}
+
+
+def raft_ivf_pq_build_validator(params, dims):
+    if "pq_dim" in params:
+        return params["pq_dim"] <= dims
+    return True
+
+
+def raft_ivf_pq_search_validator(params, build_params, k, batch_size):
+    ret = True
+    if "internalDistanceDtype" in params and "smemLutDtype" in params:
+        ret = (
+            DTYPE_SIZES[params["smemLutDtype"]]
+            < DTYPE_SIZES[params["internalDistanceDtype"]]
+        )
+
+    if "nlist" in build_params and "nprobe" in params:
+        ret = build_params["nlist"] <= params["nprobe"]
+    return ret
+
+
+def raft_cagra_search_validator(params, build_params, k, batch_size):
+    if "itopk" in params:
+        return params["itopk"] >= k
