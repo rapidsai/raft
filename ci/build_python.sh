75cdb5e755efba7564f0f1da78ddc271a0449119
Mark Harris <783069+harrism@users.noreply.github.com>
2023-11-07 00:45:54 +0000

    Use RMM artifacts in build_python.sh

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -13,13 +13,21 @@ rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
+REPO="rmm"
+PR_NUMBER="1095"
+COMMIT=$(git ls-remote https://github.com/rapidsai/${REPO}.git refs/heads/pull-request/${PR_NUMBER} | cut -c1-7)
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+PYTHON_MINOR_VERSION=$(python --version | sed -E 's/Python [0-9]+\.([0-9]+)\.[0-9]+/\1/g')
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/${REPO}/pull-request/${PR_NUMBER}/${COMMIT}/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+RMM_CHANNEL=$(rapids-get-artifact ci/${REPO}/pull-request/${PR_NUMBER}/${COMMIT}/rmm_conda_python_cuda${RAPIDS_CUDA_MAJOR}_3${PYTHON_MINOR_VERSION}_$(arch).tar.gz)
+
 version=$(rapids-generate-version)
 git_commit=$(git rev-parse HEAD)
-export RAPIDS_PACKAGE_VERSION=${version} 
+export RAPIDS_PACKAGE_VERSION=${version}
 echo "${version}" > VERSION
 
 package_dir="python"
-for package_name in pylibraft raft-dask; do 
+for package_name in pylibraft raft-dask; do
   underscore_package_name=$(echo "${package_name}" | tr "-" "_")
   sed -i "/^__git_commit__/ s/= .*/= \"${git_commit}\"/g" "${package_dir}/${package_name}/${underscore_package_name}/_version.py"
 done
@@ -29,12 +37,16 @@ done
 rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
   conda/recipes/pylibraft
 
 rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
   conda/recipes/raft-dask
 
 # Build ann-bench for each cuda and python version
@@ -42,6 +54,8 @@ rapids-conda-retry mambabuild \
 --no-test \
 --channel "${CPP_CHANNEL}" \
 --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+--channel "${RMM_CHANNEL}" \
+--channel "${LIBRMM_CHANNEL}" \
 conda/recipes/raft-ann-bench
 
 # Build ann-bench-cpu only in CUDA 11 jobs since it only depends on python
@@ -52,6 +66,8 @@ if [[ ${RAPIDS_CUDA_MAJOR} == "11" ]]; then
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
   conda/recipes/raft-ann-bench-cpu
 fi
 
