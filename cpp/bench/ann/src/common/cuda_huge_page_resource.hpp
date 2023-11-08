e355ff0bd1c28f6f3ebb9637228ad7afc9262219
Tamas Bela Feher <tfeher@nvidia.com>
2023-11-08 13:56:28 +0100

    update tuning guide

--- cpp/bench/ann/src/common/cuda_huge_page_resource.hpp
+++ cpp/bench/ann/src/common/cuda_huge_page_resource.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 #pragma once
-
-// #include <raft/core/logger.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/detail/error.hpp>
