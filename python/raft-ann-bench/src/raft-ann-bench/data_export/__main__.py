356eefe9fea4592c924597e8b19dcb0df55cbc76
Corey J. Nolet <cjnolet@gmail.com>
2023-11-13 19:31:04 -0500

    Fixing error in data_export (not sure how this ended up getting into the code)

--- python/raft-ann-bench/src/raft-ann-bench/data_export/__main__.py
+++ python/raft-ann-bench/src/raft-ann-bench/data_export/__main__.py
@@ -114,7 +114,7 @@ def convert_json_to_csv_search(dataset, dataset_path):
                 write["build cpu_time"] = None
                 write["build GPU"] = None
 
-                for col_idx in range(5, len(build_df.columns)):
+                for col_idx in range(6, len(build_df.columns)):
                     col_name = build_df.columns[col_idx]
                     write[col_name] = None
 
