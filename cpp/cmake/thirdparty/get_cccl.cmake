c8cda27dca54fccaacba9986d27978c41fe9d8fb
Bradley Dice <bdice@bradleydice.com>
2023-12-18 11:16:14 -0800

    Remove thrust_create_target.

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -15,9 +15,7 @@
 # Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
         include(${rapids-cmake-dir}/cpm/cccl.cmake)
-
-        rapids_cpm_cccl( BUILD_EXPORT_SET raft-exports
-                         INSTALL_EXPORT_SET raft-exports)
+        rapids_cpm_cccl(BUILD_EXPORT_SET raft-exports INSTALL_EXPORT_SET raft-exports)
 endfunction()
 
 find_and_configure_cccl()
