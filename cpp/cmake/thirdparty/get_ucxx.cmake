8c6f0cc424db39760871d73dff1cb33f663de10e
Peter Andreas Entschev <peter@entschev.com>
2024-01-09 06:52:36 -0800

    CMake find UCXX

---
+++ cpp/cmake/thirdparty/get_ucxx.cmake
@@ -0,0 +1,39 @@
+#=============================================================================
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
+function(find_and_configure_ucxx)
+    set(oneValueArgs VERSION FORK PINNED_TAG EXCLUDE_FROM_ALL)
+    cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
+            "${multiValueArgs}" ${ARGN} )
+
+    rapids_cpm_find(ucxx ${PKG_VERSION}
+            GLOBAL_TARGETS         ucxx::ucxx
+            BUILD_EXPORT_SET       raft-exports
+            INSTALL_EXPORT_SET     raft-exports
+            CPM_ARGS
+            GIT_REPOSITORY         https://github.com/rapidsai/ucxx.git
+            GIT_TAG                ${PKG_PINNED_TAG}
+            EXCLUDE_FROM_ALL       ${PKG_EXCLUDE_FROM_ALL})
+
+endfunction()
+
+# Change pinned tag here to test a commit in CI
+# To use a different RAFT locally, set the CMake variable
+# CPM_raft_SOURCE=/path/to/local/raft
+find_and_configure_ucxx(VERSION  0.36
+        FORK             ucxx
+        PINNED_TAG       v0.36.00a
+        EXCLUDE_FROM_ALL YES)
