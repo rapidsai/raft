a93e3ca7711914610aef03db2180508ea59495e0
Vyas Ramasubramani <vyasr@nvidia.com>
2024-01-19 14:23:38 +0000

    Switch to my ucxx fork for conda build support

--- python/raft-dask/cmake/thirdparty/get_ucxx.cmake
+++ python/raft-dask/cmake/thirdparty/get_ucxx.cmake
@@ -48,8 +48,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_ucxx(VERSION  0.36
-        FORK             rapidsai
-        PINNED_TAG       branch-0.36
+        FORK             vyasr
+        PINNED_TAG       fix/static_build_conda_env
         EXCLUDE_FROM_ALL YES
         UCXX_STATIC      ${RAFT_DASK_UCXX_STATIC}
     )
