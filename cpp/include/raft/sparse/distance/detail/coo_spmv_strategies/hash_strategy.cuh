3334de7c9688b2558176ba82f05325f5d69ece4e
Yunsong Wang <yunsongw@nvidia.com>
2024-02-05 15:48:47 -0800

    Fix typos

--- cpp/include/raft/sparse/distance/detail/coo_spmv_strategies/hash_strategy.cuh
+++ cpp/include/raft/sparse/distance/detail/coo_spmv_strategies/hash_strategy.cuh
@@ -43,11 +43,11 @@ namespace detail {
 template <typename value_idx, typename value_t, int tpb>
 class hash_strategy : public coo_spmv_strategy<value_idx, value_t, tpb> {
  public:
-  using insert_type = typename cuco::experimental::
+  using insert_type = typename cuco::legacy::
     static_map<value_idx, value_t, cuda::thread_scope_block>::device_mutable_view;
   using smem_type = typename insert_type::slot_type*;
-  using find_type = typename cuco::experimental::
-    static_map<value_idx, value_t, cuda::thread_scope_block>::device_view;
+  using find_type =
+    typename cuco::legacy::static_map<value_idx, value_t, cuda::thread_scope_block>::device_view;
 
   hash_strategy(const distances_config_t<value_idx, value_t>& config_,
                 float capacity_threshold_ = 0.5,
