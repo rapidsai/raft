2e5ab617cfcddba0ebb1822d651aeea2d56b9459
Malte Foerster <mfoerster@nvidia.com>
2024-02-07 16:51:30 +0000

    review suggestion

--- cpp/include/raft/sparse/linalg/spmm.hpp
+++ cpp/include/raft/sparse/linalg/spmm.hpp
@@ -61,11 +61,10 @@ void spmm(raft::resources const& handle,
   bool is_row_major = detail::is_row_major(y, z);
 
   auto z_tmp_view =
-    is_row_major
-      ? raft::make_device_strided_matrix_view<ValueType, IndexType, layout_c_contiguous>(
-          z.data_handle(), z.extent(0), z.extent(1), is_row_major ? z.stride(0) : z.stride(1))
-      : raft::make_device_strided_matrix_view<ValueType, IndexType, layout_f_contiguous>(
-          z.data_handle(), z.extent(0), z.extent(1), is_row_major ? z.stride(0) : z.stride(1));
+    is_row_major ? raft::make_device_strided_matrix_view<ValueType, IndexType, layout_c_contiguous>(
+                     z.data_handle(), z.extent(0), z.extent(1), z.stride(0))
+                 : raft::make_device_strided_matrix_view<ValueType, IndexType, layout_f_contiguous>(
+                     z.data_handle(), z.extent(0), z.extent(1), z.stride(1));
 
   auto descr_x = detail::create_descriptor(x);
   auto descr_y = detail::create_descriptor(y);
