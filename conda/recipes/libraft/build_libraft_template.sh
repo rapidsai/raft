b669135241a492040acd92742ff2766269236145
Mickael Ide <mide@nvidia.com>
2024-03-14 21:08:06 +0100

    Update copyright

--- conda/recipes/libraft/build_libraft_template.sh
+++ conda/recipes/libraft/build_libraft_template.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
 
 # Just building template so we verify it uses libraft.so and fail if it doesn't build
 ./build.sh template --no-nvtx
