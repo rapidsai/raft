daf4f084532265bb1d89786fb7abce5b9df54efc
Hiroyuki Ootomo <hootomo@nvidia.com>
2024-03-20 23:17:18 +0900

    Fix VPQ similarity computation for large dim

--- cpp/include/raft/neighbors/detail/cagra/compute_distance_vpq.cuh
+++ cpp/include/raft/neighbors/detail/cagra/compute_distance_vpq.cuh
@@ -91,10 +91,6 @@ struct cagra_q_dataset_descriptor_t : public dataset_descriptor_base_t<half, DIS
       pq_code_book_ptr(pq_code_book_ptr),
       pq_scale(pq_scale)
   {
-    if (dim > DATASET_BLOCK_DIM) {
-      RAFT_FAIL(
-        "`dim` must be smaller or equal to 512. Support for larger dimension is coming soon.");
-    }
   }
 
   __device__ DISTANCE_T compute_similarity(const QUERY_T* const query_ptr,
@@ -107,95 +103,98 @@ struct cagra_q_dataset_descriptor_t : public dataset_descriptor_base_t<half, DIS
       const uint32_t vq_code = *(reinterpret_cast<const std::uint32_t*>(
         encoded_dataset_ptr + (static_cast<std::uint64_t>(encoded_dataset_dim) * node_id)));
       if (PQ_BITS == 8) {
-        constexpr unsigned vlen  = 4;  // **** DO NOT CHANGE ****
-        constexpr unsigned nelem = raft::div_rounding_up_unsafe<unsigned>(
-          DATASET_BLOCK_DIM / PQ_CODE_BOOK_DIM, TEAM_SIZE * vlen);
-        // Loading PQ codes
-        uint32_t pq_codes[nelem];
-#pragma unroll
-        for (std::uint32_t e = 0; e < nelem; e++) {
-          const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen;
-          if (k >= n_subspace) break;
-          // Loading 4 x 8-bit PQ-codes using 32-bit load ops (from device memory)
-          pq_codes[e] = *(reinterpret_cast<const std::uint32_t*>(
-            encoded_dataset_ptr + (static_cast<std::uint64_t>(encoded_dataset_dim) * node_id) + 4 +
-            k));
-        }
-        //
-        if constexpr ((std::is_same<CODE_BOOK_T, half>::value) && (PQ_CODE_BOOK_DIM % 2 == 0)) {
-          // **** Use half2 for distance computation ****
-          half2 norm2{0, 0};
+        for (uint32_t elem_offset = 0; elem_offset < dim; elem_offset += DATASET_BLOCK_DIM) {
+          constexpr unsigned vlen  = 4;  // **** DO NOT CHANGE ****
+          constexpr unsigned nelem = raft::div_rounding_up_unsafe<unsigned>(
+            DATASET_BLOCK_DIM / PQ_CODE_BOOK_DIM, TEAM_SIZE * vlen);
+          // Loading PQ codes
+          uint32_t pq_codes[nelem];
 #pragma unroll
           for (std::uint32_t e = 0; e < nelem; e++) {
-            const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen;
+            const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen + elem_offset;
             if (k >= n_subspace) break;
-            // Loading VQ code-book
-            raft::TxN_t<half2, vlen / 2> vq_vals[PQ_CODE_BOOK_DIM];
-#pragma unroll
-            for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m += 1) {
-              const uint32_t d = (vlen * m) + (PQ_CODE_BOOK_DIM * k);
-              if (d >= dim) break;
-              vq_vals[m].load(
-                reinterpret_cast<const half2*>(vq_code_book_ptr + d + (dim * vq_code)), 0);
-            }
-            // Compute distance
-            std::uint32_t pq_code = pq_codes[e];
-#pragma unroll
-            for (std::uint32_t v = 0; v < vlen; v++) {
-              if (PQ_CODE_BOOK_DIM * (v + k) >= dim) break;
-#pragma unroll
-              for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m += 2) {
-                const std::uint32_t d1 = m + (PQ_CODE_BOOK_DIM * v);
-                const std::uint32_t d  = d1 + (PQ_CODE_BOOK_DIM * k);
-                // Loading query vector in smem
-                half2 diff2 = (reinterpret_cast<const half2*>(
-                  query_ptr))[device::swizzling<std::uint32_t, DATASET_BLOCK_DIM / 2>(d / 2)];
-                // Loading PQ code book in smem
-                diff2 -= *(reinterpret_cast<half2*>(
-                  smem_pq_code_book_ptr + (1 << PQ_BITS) * 2 * (m / 2) + (2 * (pq_code & 0xff))));
-                diff2 -= vq_vals[d1 / vlen].val.data[(d1 % vlen) / 2];
-                norm2 += diff2 * diff2;
-              }
-              pq_code >>= 8;
-            }
+            // Loading 4 x 8-bit PQ-codes using 32-bit load ops (from device memory)
+            pq_codes[e] = *(reinterpret_cast<const std::uint32_t*>(
+              encoded_dataset_ptr + (static_cast<std::uint64_t>(encoded_dataset_dim) * node_id) +
+              4 + k));
           }
-          norm = static_cast<float>(norm2.x + norm2.y);
-        } else {
-          // **** Use float for distance computation ****
+          //
+          if constexpr ((std::is_same<CODE_BOOK_T, half>::value) && (PQ_CODE_BOOK_DIM % 2 == 0)) {
+            // **** Use half2 for distance computation ****
+            half2 norm2{0, 0};
 #pragma unroll
-          for (std::uint32_t e = 0; e < nelem; e++) {
-            const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen;
-            if (k >= n_subspace) break;
-            // Loading VQ code-book
-            raft::TxN_t<CODE_BOOK_T, vlen> vq_vals[PQ_CODE_BOOK_DIM];
+            for (std::uint32_t e = 0; e < nelem; e++) {
+              const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen;
+              if (k >= n_subspace) break;
+              // Loading VQ code-book
+              raft::TxN_t<half2, vlen / 2> vq_vals[PQ_CODE_BOOK_DIM];
 #pragma unroll
-            for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m++) {
-              const std::uint32_t d = (vlen * m) + (PQ_CODE_BOOK_DIM * k);
-              if (d >= dim) break;
-              // Loading 4 x 8/16-bit VQ-values using 32/64-bit load ops (from L2$ or device memory)
-              vq_vals[m].load(
-                reinterpret_cast<const half2*>(vq_code_book_ptr + d + (dim * vq_code)), 0);
+              for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m += 1) {
+                const uint32_t d = (vlen * m) + (PQ_CODE_BOOK_DIM * k) + elem_offset;
+                if (d >= dim) break;
+                vq_vals[m].load(
+                  reinterpret_cast<const half2*>(vq_code_book_ptr + d + (dim * vq_code)), 0);
+              }
+              // Compute distance
+              std::uint32_t pq_code = pq_codes[e];
+#pragma unroll
+              for (std::uint32_t v = 0; v < vlen; v++) {
+                if (PQ_CODE_BOOK_DIM * (v + k) >= dim) break;
+#pragma unroll
+                for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m += 2) {
+                  const std::uint32_t d1 = m + (PQ_CODE_BOOK_DIM * v);
+                  const std::uint32_t d  = d1 + (PQ_CODE_BOOK_DIM * k);
+                  // Loading query vector in smem
+                  half2 diff2 = (reinterpret_cast<const half2*>(
+                    query_ptr))[device::swizzling<std::uint32_t, DATASET_BLOCK_DIM / 2>(d / 2)];
+                  // Loading PQ code book in smem
+                  diff2 -= *(reinterpret_cast<half2*>(
+                    smem_pq_code_book_ptr + (1 << PQ_BITS) * 2 * (m / 2) + (2 * (pq_code & 0xff))));
+                  diff2 -= vq_vals[d1 / vlen].val.data[(d1 % vlen) / 2];
+                  norm2 += diff2 * diff2;
+                }
+                pq_code >>= 8;
+              }
             }
-            // Compute distance
-            std::uint32_t pq_code = pq_codes[e];
+            norm += static_cast<float>(norm2.x + norm2.y);
+          } else {
+            // **** Use float for distance computation ****
 #pragma unroll
-            for (std::uint32_t v = 0; v < vlen; v++) {
-              if (PQ_CODE_BOOK_DIM * (v + k) >= dim) break;
-              raft::TxN_t<CODE_BOOK_T, PQ_CODE_BOOK_DIM> pq_vals;
-              pq_vals.load(reinterpret_cast<const half2*>(smem_pq_code_book_ptr +
-                                                          PQ_CODE_BOOK_DIM * (pq_code & 0xff)),
-                           0);  // (from L1$ or smem)
+            for (std::uint32_t e = 0; e < nelem; e++) {
+              const std::uint32_t k = (lane_id + (TEAM_SIZE * e)) * vlen + elem_offset;
+              if (k >= n_subspace) break;
+              // Loading VQ code-book
+              raft::TxN_t<CODE_BOOK_T, vlen> vq_vals[PQ_CODE_BOOK_DIM];
 #pragma unroll
               for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m++) {
-                const std::uint32_t d1 = m + (PQ_CODE_BOOK_DIM * v);
-                const std::uint32_t d  = d1 + (PQ_CODE_BOOK_DIM * k);
-                // if (d >= dataset_dim) break;
-                DISTANCE_T diff = query_ptr[d];  // (from smem)
-                diff -= pq_scale * static_cast<float>(pq_vals.data[m]);
-                diff -= vq_scale * static_cast<float>(vq_vals[d1 / vlen].val.data[d1 % vlen]);
-                norm += diff * diff;
+                const std::uint32_t d = (vlen * m) + (PQ_CODE_BOOK_DIM * k) + elem_offset;
+                if (d >= dim) break;
+                // Loading 4 x 8/16-bit VQ-values using 32/64-bit load ops (from L2$ or device
+                // memory)
+                vq_vals[m].load(
+                  reinterpret_cast<const half2*>(vq_code_book_ptr + d + (dim * vq_code)), 0);
               }
-              pq_code >>= 8;
+              // Compute distance
+              std::uint32_t pq_code = pq_codes[e];
+#pragma unroll
+              for (std::uint32_t v = 0; v < vlen; v++) {
+                if (PQ_CODE_BOOK_DIM * (v + k) >= dim) break;
+                raft::TxN_t<CODE_BOOK_T, PQ_CODE_BOOK_DIM> pq_vals;
+                pq_vals.load(reinterpret_cast<const half2*>(smem_pq_code_book_ptr +
+                                                            PQ_CODE_BOOK_DIM * (pq_code & 0xff)),
+                             0);  // (from L1$ or smem)
+#pragma unroll
+                for (std::uint32_t m = 0; m < PQ_CODE_BOOK_DIM; m++) {
+                  const std::uint32_t d1 = m + (PQ_CODE_BOOK_DIM * v);
+                  const std::uint32_t d  = d1 + (PQ_CODE_BOOK_DIM * k);
+                  // if (d >= dataset_dim) break;
+                  DISTANCE_T diff = query_ptr[d];  // (from smem)
+                  diff -= pq_scale * static_cast<float>(pq_vals.data[m]);
+                  diff -= vq_scale * static_cast<float>(vq_vals[d1 / vlen].val.data[d1 % vlen]);
+                  norm += diff * diff;
+                }
+                pq_code >>= 8;
+              }
             }
           }
         }
