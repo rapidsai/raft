59033c7af6a2ea785d43b7fed876050827109dd7
achirkin <achirkin@users.noreply.github.com>
2024-03-20 17:16:57 +0100

    Fix the instances in the tests as well

--- cpp/test/neighbors/ann_cagra/search_kernel_uint64_t.cuh
+++ cpp/test/neighbors/ann_cagra/search_kernel_uint64_t.cuh
@@ -21,37 +21,36 @@
 namespace raft::neighbors::cagra::detail {
 
 namespace multi_cta_search {
-#define instantiate_kernel_selection(                                                           \
-  DATASET_DESCRIPTOR, TEAM_SIZE, MAX_DATASET_DIM, DATA_T, INDEX_T, DISTANCE_T, SAMPLE_FILTER_T) \
-  extern template void select_and_run<                                                          \
-    TEAM_SIZE,                                                                                  \
-    MAX_DATASET_DIM,                                                                            \
-    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, 0, 0, DISTANCE_T>,      \
-    SAMPLE_FILTER_T>(                                                                           \
-    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, 0, 0, DISTANCE_T>       \
-      dataset_desc,                                                                             \
-    raft::device_matrix_view<const INDEX_T, int64_t, row_major> graph,                          \
-    INDEX_T* const topk_indices_ptr,                                                            \
-    DISTANCE_T* const topk_distances_ptr,                                                       \
-    const DATA_T* const queries_ptr,                                                            \
-    const uint32_t num_queries,                                                                 \
-    const INDEX_T* dev_seed_ptr,                                                                \
-    uint32_t* const num_executed_iterations,                                                    \
-    uint32_t topk,                                                                              \
-    uint32_t block_size,                                                                        \
-    uint32_t result_buffer_size,                                                                \
-    uint32_t smem_size,                                                                         \
-    int64_t hash_bitlen,                                                                        \
-    INDEX_T* hashmap_ptr,                                                                       \
-    uint32_t num_cta_per_query,                                                                 \
-    uint32_t num_random_samplings,                                                              \
-    uint64_t rand_xor_mask,                                                                     \
-    uint32_t num_seeds,                                                                         \
-    size_t itopk_size,                                                                          \
-    size_t search_width,                                                                        \
-    size_t min_iterations,                                                                      \
-    size_t max_iterations,                                                                      \
-    SAMPLE_FILTER_T sample_filter,                                                              \
+#define instantiate_kernel_selection(                                                             \
+  DATASET_DESCRIPTOR, TEAM_SIZE, MAX_DATASET_DIM, DATA_T, INDEX_T, DISTANCE_T, SAMPLE_FILTER_T)   \
+  extern template void                                                                            \
+  select_and_run<TEAM_SIZE,                                                                       \
+                 MAX_DATASET_DIM,                                                                 \
+                 raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, DISTANCE_T>, \
+                 SAMPLE_FILTER_T>(                                                                \
+    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, DISTANCE_T> dataset_desc, \
+    raft::device_matrix_view<const INDEX_T, int64_t, row_major> graph,                            \
+    INDEX_T* const topk_indices_ptr,                                                              \
+    DISTANCE_T* const topk_distances_ptr,                                                         \
+    const DATA_T* const queries_ptr,                                                              \
+    const uint32_t num_queries,                                                                   \
+    const INDEX_T* dev_seed_ptr,                                                                  \
+    uint32_t* const num_executed_iterations,                                                      \
+    uint32_t topk,                                                                                \
+    uint32_t block_size,                                                                          \
+    uint32_t result_buffer_size,                                                                  \
+    uint32_t smem_size,                                                                           \
+    int64_t hash_bitlen,                                                                          \
+    INDEX_T* hashmap_ptr,                                                                         \
+    uint32_t num_cta_per_query,                                                                   \
+    uint32_t num_random_samplings,                                                                \
+    uint64_t rand_xor_mask,                                                                       \
+    uint32_t num_seeds,                                                                           \
+    size_t itopk_size,                                                                            \
+    size_t search_width,                                                                          \
+    size_t min_iterations,                                                                        \
+    size_t max_iterations,                                                                        \
+    SAMPLE_FILTER_T sample_filter,                                                                \
     cudaStream_t stream);
 
 instantiate_kernel_selection(standard_dataset_descriptor_t,
@@ -88,38 +87,37 @@ instantiate_kernel_selection(standard_dataset_descriptor_t,
 
 namespace single_cta_search {
 
-#define instantiate_single_cta_select_and_run(                                                  \
-  DATASET_DESCRIPTOR, TEAM_SIZE, MAX_DATASET_DIM, DATA_T, INDEX_T, DISTANCE_T, SAMPLE_FILTER_T) \
-  extern template void select_and_run<                                                          \
-    TEAM_SIZE,                                                                                  \
-    MAX_DATASET_DIM,                                                                            \
-    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, 0, 0, DISTANCE_T>,      \
-    SAMPLE_FILTER_T>(                                                                           \
-    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, 0, 0, DISTANCE_T>       \
-      dataset_desc,                                                                             \
-    raft::device_matrix_view<const INDEX_T, int64_t, row_major> graph,                          \
-    INDEX_T* const topk_indices_ptr,                                                            \
-    DISTANCE_T* const topk_distances_ptr,                                                       \
-    const DATA_T* const queries_ptr,                                                            \
-    const uint32_t num_queries,                                                                 \
-    const INDEX_T* dev_seed_ptr,                                                                \
-    uint32_t* const num_executed_iterations,                                                    \
-    uint32_t topk,                                                                              \
-    uint32_t num_itopk_candidates,                                                              \
-    uint32_t block_size,                                                                        \
-    uint32_t smem_size,                                                                         \
-    int64_t hash_bitlen,                                                                        \
-    INDEX_T* hashmap_ptr,                                                                       \
-    size_t small_hash_bitlen,                                                                   \
-    size_t small_hash_reset_interval,                                                           \
-    uint32_t num_random_samplings,                                                              \
-    uint64_t rand_xor_mask,                                                                     \
-    uint32_t num_seeds,                                                                         \
-    size_t itopk_size,                                                                          \
-    size_t search_width,                                                                        \
-    size_t min_iterations,                                                                      \
-    size_t max_iterations,                                                                      \
-    SAMPLE_FILTER_T sample_filter,                                                              \
+#define instantiate_single_cta_select_and_run(                                                    \
+  DATASET_DESCRIPTOR, TEAM_SIZE, MAX_DATASET_DIM, DATA_T, INDEX_T, DISTANCE_T, SAMPLE_FILTER_T)   \
+  extern template void                                                                            \
+  select_and_run<TEAM_SIZE,                                                                       \
+                 MAX_DATASET_DIM,                                                                 \
+                 raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, DISTANCE_T>, \
+                 SAMPLE_FILTER_T>(                                                                \
+    raft::neighbors::cagra::detail::DATASET_DESCRIPTOR<DATA_T, INDEX_T, DISTANCE_T> dataset_desc, \
+    raft::device_matrix_view<const INDEX_T, int64_t, row_major> graph,                            \
+    INDEX_T* const topk_indices_ptr,                                                              \
+    DISTANCE_T* const topk_distances_ptr,                                                         \
+    const DATA_T* const queries_ptr,                                                              \
+    const uint32_t num_queries,                                                                   \
+    const INDEX_T* dev_seed_ptr,                                                                  \
+    uint32_t* const num_executed_iterations,                                                      \
+    uint32_t topk,                                                                                \
+    uint32_t num_itopk_candidates,                                                                \
+    uint32_t block_size,                                                                          \
+    uint32_t smem_size,                                                                           \
+    int64_t hash_bitlen,                                                                          \
+    INDEX_T* hashmap_ptr,                                                                         \
+    size_t small_hash_bitlen,                                                                     \
+    size_t small_hash_reset_interval,                                                             \
+    uint32_t num_random_samplings,                                                                \
+    uint64_t rand_xor_mask,                                                                       \
+    uint32_t num_seeds,                                                                           \
+    size_t itopk_size,                                                                            \
+    size_t search_width,                                                                          \
+    size_t min_iterations,                                                                        \
+    size_t max_iterations,                                                                        \
+    SAMPLE_FILTER_T sample_filter,                                                                \
     cudaStream_t stream);
 
 instantiate_single_cta_select_and_run(standard_dataset_descriptor_t,
