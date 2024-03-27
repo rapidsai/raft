6f52b9bb91e77fa691b4dd216856354124b21873
galipremsagar <sagarprem75@gmail.com>
2024-03-27 18:27:28 +0000

    Enable all pytests for arm jobs

--- ci/test_wheel_pylibraft.sh
+++ ci/test_wheel_pylibraft.sh
@@ -10,9 +10,4 @@ RAPIDS_PY_WHEEL_NAME="pylibraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/pylibraft*.whl)[test]
 
-# Run smoke tests for aarch64 pull requests
-if [[ "$(arch)" == "aarch64" && "${RAPIDS_BUILD_TYPE}" == "pull-request" ]]; then
-    python ./ci/wheel_smoke_test_pylibraft.py
-else
-    python -m pytest ./python/pylibraft/pylibraft/test
-fi
+python -m pytest ./python/pylibraft/pylibraft/test
