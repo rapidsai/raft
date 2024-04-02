4b882551ecdf14149686a7b2c53b509446145385
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-02 16:46:41 +0000

    Also download libraft wheel

--- ci/test_wheel_pylibraft.sh
+++ ci/test_wheel_pylibraft.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+RAPIDS_PY_WHEEL_NAME="libraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 RAPIDS_PY_WHEEL_NAME="pylibraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/pylibraft*.whl)[test]
+python -m pip install $(echo ./dist/pylibraft*.whl)[test] --find-links ./dist
 
 python -m pytest ./python/pylibraft/pylibraft/test
