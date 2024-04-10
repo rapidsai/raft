7a5f87628471e1f8c6285e389d05af3d56471d7f
Tarang Jain <jaintarang2015@gmail.com>
2024-04-10 13:35:26 -0700

    update testing

--- cpp/test/neighbors/ann_cagra/test_half_uint32_t.cu
+++ cpp/test/neighbors/ann_cagra/test_half_uint32_t.cu
@@ -23,19 +23,19 @@ namespace raft::neighbors::cagra {
 typedef AnnCagraTest<float, half, std::uint32_t> AnnCagraTestH_U32;
 TEST_P(AnnCagraTestH_U32, AnnCagra) { this->testCagra(); }
 
-// typedef AnnCagraSortTest<float, half, std::uint32_t> AnnCagraSortTestH_U32;
-// TEST_P(AnnCagraSortTestH_U32, AnnCagraSort) { this->testCagraSort(); }
+typedef AnnCagraSortTest<float, half, std::uint32_t> AnnCagraSortTestH_U32;
+TEST_P(AnnCagraSortTestH_U32, AnnCagraSort) { this->testCagraSort(); }
 
-// typedef AnnCagraFilterTest<float, half, std::uint32_t> AnnCagraFilterTestH_U32;
-// TEST_P(AnnCagraFilterTestH_U32, AnnCagraFilter)
-// {
-//   this->testCagraFilter();
-//   this->testCagraRemoved();
-// }
+typedef AnnCagraFilterTest<float, half, std::uint32_t> AnnCagraFilterTestH_U32;
+TEST_P(AnnCagraFilterTestH_U32, AnnCagraFilter)
+{
+  this->testCagraFilter();
+  this->testCagraRemoved();
+}
 
 INSTANTIATE_TEST_CASE_P(AnnCagraTest, AnnCagraTestH_U32, ::testing::ValuesIn(inputs));
-// INSTANTIATE_TEST_CASE_P(AnnCagraSortTest, AnnCagraSortTestH_U32, ::testing::ValuesIn(inputs));
-// INSTANTIATE_TEST_CASE_P(AnnCagraFilterTest, AnnCagraFilterTestH_U32,
-// ::testing::ValuesIn(inputs));
+INSTANTIATE_TEST_CASE_P(AnnCagraSortTest, AnnCagraSortTestH_U32, ::testing::ValuesIn(inputs));
+INSTANTIATE_TEST_CASE_P(AnnCagraFilterTest, AnnCagraFilterTestH_U32,
+::testing::ValuesIn(inputs));
 
 }  // namespace raft::neighbors::cagra
