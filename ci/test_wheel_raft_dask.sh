e2e0902fd3d3c8592decec88cda829a9bae386e0
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-17 15:22:02 +0000

    Standardize test scripts

--- ci/test_wheel_raft_dask.sh
+++ ci/test_wheel_raft_dask.sh
@@ -4,13 +4,12 @@
 set -euo pipefail
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="libraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp ./dist
-RAPIDS_PY_WHEEL_NAME="pylibraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
-RAPIDS_PY_WHEEL_NAME="raft_dask_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
+WHEELHOUSE="${PWD}/dist/"
+RAPIDS_PY_WHEEL_NAME="${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp "${WHEELHOUSE}"
+RAPIDS_PY_WHEEL_NAME="${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python "${WHEELHOUSE}"
 
 librmm_wheelhouse=$(RAPIDS_PY_WHEEL_NAME="librmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-wheel-artifact rmm 1529 cpp)
 
-# echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/raft_dask*.whl)[test] --find-links dist/ --find-links ${librmm_wheelhouse}
+python -m pip install "raft_dask[test]>=0.0.0a0" --find-links "${WHEELHOUSE}" --find-links ${librmm_wheelhouse}
 
 python -m pytest ./python/raft-dask/raft_dask/test
