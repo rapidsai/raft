a358b52bd894b51f233f28724190e579aa643198
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-17 01:35:16 +0000

    Remove more clones

--- ci/test_wheel_raft_dask.sh
+++ ci/test_wheel_raft_dask.sh
@@ -3,12 +3,6 @@
 
 set -euo pipefail
 
-if [[ ! -d "/tmp/gha-tools" ]]; then
-    git clone https://github.com/msarahan/gha-tools.git -b get-pr-wheel-artifact /tmp/gha-tools
-fi
-export PATH="/tmp/gha-tools/tools:${PATH}"
-
-mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="libraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp ./dist
 RAPIDS_PY_WHEEL_NAME="pylibraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
