d1218164a50c3dd5aacd7166078ced65f2d70f62
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2024-05-10 10:38:54 -0700

    Revert "Revert "Build C++ wheel (#2264)" (#2305)"
    This reverts commit ea54c792b2b415b238cecf238f418aabb37836a2.

--- ci/build_wheel_pylibraft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
-
-set -euo pipefail
-
-# Set up skbuild options. Enable sccache in skbuild config options
-export SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;-DFIND_RAFT_CPP=OFF"
-
-ci/build_wheel.sh pylibraft python/pylibraft
