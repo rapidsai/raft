9e24c5a0ac51623c7b774829d291da9f4580ed6a
hrong <hrong@nvidia.com>
2024-05-22 13:38:23 -0700

    Move sparse distance API utils to cuvs and split the bitmap

--- cpp/include/raft/sparse/distance/detail/utils.cuh
+++ cpp/include/raft/sparse/distance/detail/utils.cuh
@@ -41,75 +41,6 @@ inline int max_cols_per_block()
          sizeof(value_t);
 }
 
-template <typename value_idx, typename value_t, typename expansion_f>
-RAFT_KERNEL epilogue_on_csr_kernel(value_t* __restrict__ compressed_C,
-                                   const value_idx* __restrict__ rows,
-                                   const value_idx* __restrict__ cols,
-                                   const value_t* __restrict__ Q_sq_norms,
-                                   const value_t* __restrict__ R_sq_norms,
-                                   value_idx nnz,
-                                   expansion_f expansion_func)
-{
-  auto tid = blockDim.x * blockIdx.x + threadIdx.x;
-
-  if (tid >= nnz) return;
-  const value_idx i = rows[tid];
-  const value_idx j = cols[tid];
-
-  compressed_C[tid] = expansion_func(compressed_C[tid], Q_sq_norms[i], R_sq_norms[j]);
-}
-
-template <typename value_idx, typename value_t, int tpb = 256>
-void epilogue_on_csr(raft::resources const& handle,
-                     value_t* compressed_C,
-                     const value_idx nnz,
-                     const value_idx* rows,
-                     const value_idx* cols,
-                     const value_t* Q_sq_norms,
-                     const value_t* R_sq_norms,
-                     raft::distance::DistanceType metric)
-{
-  if (nnz == 0) return;
-  auto stream = resource::get_cuda_stream(handle);
-
-  int blocks = raft::ceildiv<size_t>((size_t)nnz, tpb);
-  if (metric == raft::distance::DistanceType::L2Expanded) {
-    epilogue_on_csr_kernel<<<blocks, tpb, 0, stream>>>(
-      compressed_C,
-      rows,
-      cols,
-      Q_sq_norms,
-      R_sq_norms,
-      nnz,
-      [] __device__ __host__(value_t dot, value_t q_norm, value_t r_norm) -> value_t {
-        return value_t(-2.0) * dot + q_norm + r_norm;
-      });
-  } else if (metric == raft::distance::DistanceType::L2SqrtExpanded) {
-    epilogue_on_csr_kernel<<<blocks, tpb, 0, stream>>>(
-      compressed_C,
-      rows,
-      cols,
-      Q_sq_norms,
-      R_sq_norms,
-      nnz,
-      [] __device__ __host__(value_t dot, value_t q_norm, value_t r_norm) -> value_t {
-        return raft::sqrt(value_t(-2.0) * dot + q_norm + r_norm);
-      });
-  } else if (metric == raft::distance::DistanceType::CosineExpanded) {
-    epilogue_on_csr_kernel<<<blocks, tpb, 0, stream>>>(
-      compressed_C,
-      rows,
-      cols,
-      Q_sq_norms,
-      R_sq_norms,
-      nnz,
-      [] __device__ __host__(value_t dot, value_t q_norm, value_t r_norm) -> value_t {
-        return value_t(1.0) - dot / (q_norm * r_norm);
-      });
-  }
-  RAFT_CUDA_TRY(cudaPeekAtLastError());
-}
-
 template <typename value_idx, typename value_t>
 RAFT_KERNEL faster_dot_on_csr_kernel(value_t* __restrict__ dot,
                                      const value_idx* __restrict__ indptr,
@@ -234,6 +165,7 @@ void faster_dot_on_csr(raft::resources const& handle,
 
   RAFT_CUDA_TRY(cudaPeekAtLastError());
 }
+
 }  // namespace detail
 }  // namespace distance
 }  // namespace sparse
