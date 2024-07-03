c593f4e8b485974951e02c5934954dc5c08dddd2
Julio Perez <jperez@nvidia.com>
2024-07-02 23:40:18 -0400

    add knn to preprocess tests

--- cpp/test/sparse/preprocess.cu
+++ cpp/test/sparse/preprocess.cu
@@ -36,6 +36,8 @@ namespace sparse {
 
 template <typename Type_f, typename Index_>
 struct SparsePreprocessInputs {
+  int n_rows;
+  int n_cols;
   std::vector<Index_> rows_h;
   std::vector<Index_> columns_h;
   std::vector<Type_f> values_h;
@@ -47,6 +49,8 @@ class SparseTest : public ::testing::TestWithParam<SparsePreprocessInputs<Type_f
   SparseTest()
     : params(::testing::TestWithParam<SparsePreprocessInputs<Type_f, Index_>>::GetParam()),
       stream(resource::get_cuda_stream(handle)),
+      n_rows(params.n_rows),
+      n_cols(params.n_cols),
       rows(params.rows_h.size(), stream),
       columns(params.columns_h.size(), stream),
       values(params.values_h.size(), stream),
@@ -60,34 +64,44 @@ class SparseTest : public ::testing::TestWithParam<SparsePreprocessInputs<Type_f
 
   void Run(bool bm25_on)
   {
+    int k               = 3;
     cudaStream_t stream = raft::resource::get_cuda_stream(handle);
-
-    auto rows    = raft::make_device_vector<int, int64_t>(handle, params.rows_h.size());
-    auto columns = raft::make_device_vector<int, int64_t>(handle, params.columns_h.size());
-    auto values  = raft::make_device_vector<float, int64_t>(handle, params.values_h.size());
-    auto result  = raft::make_device_vector<float, int64_t>(handle, params.values_h.size());
+    auto rows           = raft::make_device_vector<int, int64_t>(handle, params.rows_h.size());
+    auto columns        = raft::make_device_vector<int, int64_t>(handle, params.columns_h.size());
+    auto values         = raft::make_device_vector<float, int64_t>(handle, params.values_h.size());
+    auto result         = raft::make_device_vector<float, int64_t>(handle, params.values_h.size());
 
     raft::copy(rows.data_handle(), params.rows_h.data(), params.rows_h.size(), stream);
     raft::copy(columns.data_handle(), params.columns_h.data(), params.columns_h.size(), stream);
 
     raft::copy(values.data_handle(), params.values_h.data(), params.values_h.size(), stream);
 
-    auto coo_structure = raft::make_device_coordinate_structure_view(rows.data_handle(),
-                                                                     columns.data_handle(),
-                                                                     int(rows.size()),
-                                                                     int(columns.size()),
-                                                                     int(values.size()));
-
-    auto coo_view = raft::make_device_coo_matrix_view(values.data_handle(), coo_structure);
-
-    raft::update_device(
-      coo_view.get_elements().data(), values.data_handle(), values.size(), stream);
+    auto coo_struct_view = raft::make_device_coordinate_structure_view(
+      rows.data_handle(), columns.data_handle(), params.n_rows, params.n_cols, int(values.size()));
+    auto coo_matrix = raft::make_device_coo_matrix<float, int, int, int>(handle, coo_struct_view);
+    raft::update_device<float>(
+      coo_matrix.view().get_elements().data(), values.data_handle(), values.size(), stream);
 
     if (bm25_on) {
-      sparse::matrix::encode_bm25<int, float>(handle, coo_view, result.view());
+      sparse::matrix::encode_bm25<int, float>(handle, coo_matrix.view(), result.view());
     } else {
-      sparse::matrix::encode_tfidf<int, float>(handle, coo_view, result.view());
+      sparse::matrix::encode_tfidf<int, float>(handle, coo_matrix.view(), result.view());
     }
+
+    auto out_rows_coo =
+      raft::make_device_vector<int, int64_t>(handle, coo_matrix.structure_view().get_n_rows() * k);
+    auto out_dists_coo = raft::make_device_vector<float, int64_t>(
+      handle, coo_matrix.structure_view().get_n_rows() * k);
+
+    raft::sparse::neighbors::brute_force_knn<int, float>(coo_matrix,
+                                                         coo_matrix,
+                                                         out_rows_coo.data_handle(),
+                                                         out_dists_coo.data_handle(),
+                                                         k,
+                                                         handle,
+                                                         coo_matrix.structure_view().get_n_rows(),
+                                                         coo_matrix.structure_view().get_n_rows(),
+                                                         raft::distance::DistanceType::L1);
 
     RAFT_CUDA_TRY(cudaStreamSynchronize(stream));
 
@@ -100,6 +114,7 @@ class SparseTest : public ::testing::TestWithParam<SparsePreprocessInputs<Type_f
   cudaStream_t stream;
 
   SparsePreprocessInputs<Type_f, Index_> params;
+  int n_rows, n_cols;
   rmm::device_uvector<Index_> rows, columns;
   rmm::device_uvector<Type_f> values, result;
   bool bm25;
@@ -112,9 +127,11 @@ using SparseTestFT = SparseTest<float, int>;
 TEST_P(SparseTestFT, Result) { Run(true); }
 
 const std::vector<SparsePreprocessInputs<float, int>> sparse_preprocess_inputs = {
-  {{0, 3, 4, 5, 6, 7, 8, 9, 10, 11},
-   {0, 0, 1, 2, 2, 1, 1, 3, 2, 1},
-   {1.0, 2.0, 2.0, 1.0, 1.0, 3.0, 4.0, 2.0, 1.0, 3.0}},
+  {9,                                                    // n_rows
+   4,                                                    // n_cols
+   {0, 3, 4, 5, 6, 7, 8, 9, 10, 11},                     // rows
+   {0, 0, 1, 2, 2, 1, 1, 3, 2, 1},                       // cols
+   {1.0, 2.0, 2.0, 1.0, 1.0, 3.0, 4.0, 2.0, 1.0, 3.0}},  // vals
 };
 
 INSTANTIATE_TEST_CASE_P(SparseTest, SparseTestFF, ::testing::ValuesIn(sparse_preprocess_inputs));
