d4ef11a485a2e643492bb43e9f8a771eaf8ce7d5
James Lamb <jlamb@nvidia.com>
2024-09-16 19:59:16 -0500

    rapids-get-pr-wheel-artifact was missing RAPIDS_PY_WHEEL_NAME

--- ci/test_wheel_raft_dask.sh
+++ ci/test_wheel_raft_dask.sh
@@ -7,8 +7,12 @@ mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="raft_dask_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-LIBRMM_CHANNEL=$(rapids-get-artifact "ci/rmm/pull-request/1678/4127d9c/rmm_wheel_cpp_rmm_${RAPIDS_PY_CUDA_SUFFIX}_x86_64.tar.gz")
-RMM_CHANNEL=$(rapids-get-artifact "ci/rmm/pull-request/1678/4127d9c/rmm_wheel_python_rmm_${RAPIDS_PY_CUDA_SUFFIX}_py${RAPIDS_PY_VERSION//./}_x86_64.tar.gz")
+LIBRMM_CHANNEL=$(
+  RAPIDS_PY_WHEEL_NAME=rmm_${RAPIDS_PY_CUDA_SUFFIX} rapids-get-pr-wheel-artifact rmm 1678 cpp
+)
+RMM_CHANNEL=$(
+  RAPIDS_PY_WHEEL_NAME=rmm_${RAPIDS_PY_CUDA_SUFFIX}_py${RAPIDS_PY_VERSION//./} rapids-get-pr-wheel-artifact rmm 1678 python
+)
 
 echo "rmm-${RAPIDS_PY_CUDA_SUFFIX} @ $(echo ${RMM_CHANNEL}/rmm*.whl)" > /tmp/constraints.txt
 echo "librmm-${RAPIDS_PY_CUDA_SUFFIX} @ $(echo ${LIBRMM_CHANNEL}/librmm*.whl)" >> /tmp/constraints.txt
