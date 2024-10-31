41ddbfefa5058d206cc0aeed3f9667782869a94e
James Lamb <jlamb@nvidia.com>
2024-10-31 17:14:51 -0500

    print sccache stats in builds (#2470)
    Contributes to https://github.com/rapidsai/build-planning/issues/111
    
    Proposes some small packaging/CI changes, matching similar changes being made across RAPIDS.
    
    * printing `sccache` stats to CI logs
    * reducing `pip`'s verbosity in wheel building scripts
    * updating to the latest `rapids-dependency-file-generator` (v1.16.0)
    * always explicitly specifying `cpp` / `python` in calls to `rapids-upload-wheels-to-s3`
    
    ## Notes for Reviewers
    
    This originally also ran wheel builds with `--no-build-isolation`, but I reverted that based on https://github.com/rapidsai/build-planning/issues/108#issuecomment-2436764212.
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/raft/pull/2470

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -22,6 +22,8 @@ git_commit=$(git rev-parse HEAD)
 export RAPIDS_PACKAGE_VERSION=${version}
 echo "${version}" > VERSION
 
+sccache --zero-stats
+
 # TODO: Remove `--no-test` flags once importing on a CPU
 # node works correctly
 rapids-conda-retry mambabuild \
@@ -29,11 +31,17 @@ rapids-conda-retry mambabuild \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/pylibraft
 
+sccache --show-adv-stats
+sccache --zero-stats
+
 rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/raft-dask
+
+sccache --show-adv-stats
+sccache --zero-stats
 
 # Build ann-bench for each cuda and python version
 rapids-conda-retry mambabuild \
@@ -42,15 +50,21 @@ rapids-conda-retry mambabuild \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/raft-ann-bench
 
+sccache --show-adv-stats
+
 # Build ann-bench-cpu only in CUDA 11 jobs since it only depends on python
 # version
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 if [[ ${RAPIDS_CUDA_MAJOR} == "11" ]]; then
+  sccache --zero-stats
+
   rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/raft-ann-bench-cpu
+
+  sccache --show-adv-stats
 fi
 
 rapids-upload-conda-to-s3 python
