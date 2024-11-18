64969380971073495e9b6224591a693f2f925bde
Corey J. Nolet <cjnolet@gmail.com>
2024-11-18 13:07:25 -0500

    Removing remaining instances of implicit instantiations

--- cpp/include/raft/linalg/detail/coalesced_reduction.cuh
+++ cpp/include/raft/linalg/detail/coalesced_reduction.cuh
@@ -16,11 +16,4 @@
 
 #pragma once
 
-// Always include inline definitions of coalesced reduction, because we do not
-// force explicit instantion.
 #include "coalesced_reduction-inl.cuh"
-
-// Do include the extern template instantiations when possible.
-#ifdef RAFT_COMPILED
-#include "coalesced_reduction-ext.cuh"
-#endif
