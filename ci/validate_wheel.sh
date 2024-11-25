cc8aad278ddb373c6c1e55309633a93cdcebbd31
Bradley Dice <bdice@bradleydice.com>
2024-11-25 15:44:48 -0600

    Apply tighter limit to pylibraft wheels for CUDA 12.

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -6,12 +6,26 @@ set -euo pipefail
 package_dir=$1
 wheel_dir_relative_path=$2
 
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+
+# some packages are much larger on CUDA 11 than on CUDA 12
+if [[ "${RAPIDS_CUDA_MAJOR}" == "11" ]]; then
+    PYDISTCHECK_ARGS=(
+        --max-allowed-size-compressed '600M'
+    )
+else
+    PYDISTCHECK_ARGS=(
+        --max-allowed-size-compressed '100M'
+    )
+fi
+
 cd "${package_dir}"
 
 rapids-logger "validate packages with 'pydistcheck'"
 
 pydistcheck \
     --inspect \
+    "${PYDISTCHECK_ARGS[@]}" \
     "$(echo ${wheel_dir_relative_path}/*.whl)"
 
 rapids-logger "validate packages with 'twine'"
