679d2890c1718bb4ad41a339233a3c2d696eab6b
James Lamb <jlamb@nvidia.com>
2024-12-17 09:12:21 -0800

    more changes

--- ci/build_wheel_raft_dask.sh
+++ ci/build_wheel_raft_dask.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 package_dir="python/raft-dask"
 
 # Set up skbuild options. Enable sccache in skbuild config options
-export SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;-DFIND_RAFT_CPP=OFF"
+export SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;"
 
 ci/build_wheel.sh raft-dask ${package_dir} python
 ci/validate_wheel.sh ${package_dir} final_dist raft-dask
