11168a1ed9441f128b9e4916b9c58d8985ba4e9c
James Lamb <jlamb@nvidia.com>
2024-12-17 10:45:30 -0800

    install libraft wheels

--- ci/build_wheel_raft_dask.sh
+++ ci/build_wheel_raft_dask.sh
@@ -5,6 +5,17 @@ set -euo pipefail
 
 package_dir="python/raft-dask"
 
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+
+# Downloads libraft wheels from this current build,
+# then ensures 'raft-dask' wheel builds always use the 'libraft' just built in the same CI run.
+#
+# Using env variable PIP_CONSTRAINT is necessary to ensure the constraints
+# are used when creating the isolated build environment.
+RAPIDS_PY_WHEEL_NAME="libraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libraft_dist
+echo "libraft-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo /tmp/libraft_dist/libraft_*.whl)" > /tmp/constraints.txt
+export PIP_CONSTRAINT="/tmp/constraints.txt"
+
 # Set up skbuild options. Enable sccache in skbuild config options
 export SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;"
 
