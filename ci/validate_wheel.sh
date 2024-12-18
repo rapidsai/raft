978324442bd2347bee78acee1ad5463a9e99ded7
James Lamb <jlamb@nvidia.com>
2024-12-18 13:32:33 -0800

    update thresholds

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -10,30 +10,18 @@ package_name=$3
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 
 # some packages are much larger on CUDA 11 than on CUDA 12
-# TODO(jameslamb): adjust these thresholds
 if [[ "${package_name}" == "libraft" ]]; then
     if [[ "${RAPIDS_CUDA_MAJOR}" == "11" ]]; then
         PYDISTCHECK_ARGS=(
-            --max-allowed-size-compressed '5.0G'
+            --max-allowed-size-compressed '750M'
         )
     else
         PYDISTCHECK_ARGS=(
-            --max-allowed-size-compressed '5.0G'
-        )
-    fi
-elif [[ "${package_name}" == "pylibraft" ]] || [[ "${package_name}" == "raft-dask" ]]; then
-    if [[ "${RAPIDS_CUDA_MAJOR}" == "11" ]]; then
-        PYDISTCHECK_ARGS=(
-            --max-allowed-size-compressed '600M'
-        )
-    else
-        PYDISTCHECK_ARGS=(
-            --max-allowed-size-compressed '400M'
+            --max-allowed-size-compressed '100M'
         )
     fi
 else
-    echo "Unsupported package name: ${package_name}"
-    exit 1
+    PYDISTCHECK_ARGS=()
 fi
 
 cd "${package_dir}"
