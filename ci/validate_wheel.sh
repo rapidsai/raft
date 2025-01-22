9b154a12752f451cebf056ac47759caf98da5283
James Lamb <jlamb@nvidia.com>
2025-01-21 17:17:13 -0800

    fix wheel-size thresholds

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -9,28 +9,12 @@ package_name=$3
 
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
 
-# some packages are much larger on CUDA 11 than on CUDA 12
-PYDISTCHECK_ARGS=()
-if [[ "${package_name}" == "libraft" ]]; then
-    # TODO(jameslamb): revise these thresholds
-    if [[ "${RAPIDS_CUDA_MAJOR}" == "11" ]]; then
-        PYDISTCHECK_ARGS+=(
-            --max-allowed-size-compressed '750M'
-        )
-    else
-        PYDISTCHECK_ARGS+=(
-            --max-allowed-size-compressed '100M'
-        )
-    fi
-fi
-
 cd "${package_dir}"
 
 rapids-logger "validate packages with 'pydistcheck'"
 
 pydistcheck \
     --inspect \
-    "${PYDISTCHECK_ARGS[@]}" \
     "$(echo ${wheel_dir_relative_path}/*.whl)"
 
 rapids-logger "validate packages with 'twine'"
