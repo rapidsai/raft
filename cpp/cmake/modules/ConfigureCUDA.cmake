15074686532c5faaa93b30ecd90d910eddc3b5ac
Robert Maynard <rmaynard@nvidia.com>
2025-01-23 10:46:12 -0500

    Allow invalid CUDA kernels in the short term

--- cpp/cmake/modules/ConfigureCUDA.cmake
+++ cpp/cmake/modules/ConfigureCUDA.cmake
@@ -29,6 +29,12 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   if(CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 11.2.0)
     list(APPEND RAFT_CUDA_FLAGS -Werror=all-warnings)
   endif()
+
+  # Allow invalid CUDA kernels in the short term
+  if(CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 12.8.0)
+    list(APPEND RAFT_CUDA_FLAGS -static-global-template-stub=false )
+  endif()
+
 endif()
 
 if(CUDA_LOG_COMPILE_TIME)
