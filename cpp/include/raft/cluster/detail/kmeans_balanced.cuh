b218c9405e2f3dee41ebf70d5ec23d8f6db9ac28
Simon Adorf <sadorf@nvidia.com>
2025-02-05 07:30:09 -0800

    Revert "Use current device resource instead of explicit managed memory resource."
    This reverts commit be4586dcc466942649d4399c77e9e992365abe98.

--- cpp/include/raft/cluster/detail/kmeans_balanced.cuh
+++ cpp/include/raft/cluster/detail/kmeans_balanced.cuh
@@ -967,7 +967,8 @@ void build_hierarchical(const raft::resources& handle,
   IdxT n_mesoclusters = std::min(n_clusters, static_cast<IdxT>(std::sqrt(n_clusters) + 0.5));
   RAFT_LOG_DEBUG("build_hierarchical: n_mesoclusters: %u", n_mesoclusters);
 
-  rmm::device_async_resource_ref managed_memory = rmm::mr::get_current_device_resource();
+  // TODO: Remove the explicit managed memory- we shouldn't be creating this on the user's behalf.
+  rmm::mr::managed_memory_resource managed_memory;
   rmm::device_async_resource_ref device_memory = resource::get_large_workspace_resource(handle);
   auto [max_minibatch_size, mem_per_row] =
     calc_minibatch_size<MathT>(n_clusters, n_rows, dim, params.metric, std::is_same_v<T, MathT>);
@@ -997,8 +998,8 @@ void build_hierarchical(const raft::resources& handle,
     CounterT;
 
   // build coarse clusters (mesoclusters)
-  rmm::device_uvector<LabelT> mesocluster_labels_buf(n_rows, stream, managed_memory);
-  rmm::device_uvector<CounterT> mesocluster_sizes_buf(n_mesoclusters, stream, managed_memory);
+  rmm::device_uvector<LabelT> mesocluster_labels_buf(n_rows, stream, &managed_memory);
+  rmm::device_uvector<CounterT> mesocluster_sizes_buf(n_mesoclusters, stream, &managed_memory);
   {
     rmm::device_uvector<MathT> mesocluster_centers_buf(n_mesoclusters * dim, stream, device_memory);
     build_clusters(handle,
@@ -1054,7 +1055,7 @@ void build_hierarchical(const raft::resources& handle,
                                              fine_clusters_nums_max,
                                              cluster_centers,
                                              mapping_op,
-                                             managed_memory,
+                                             &managed_memory,
                                              device_memory);
   RAFT_EXPECTS(n_clusters_done == n_clusters, "Didn't process all clusters.");
 
