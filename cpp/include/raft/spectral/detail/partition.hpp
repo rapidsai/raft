a319a9fc2aa8ee18f47dfee4f47f1dd4f8ef646a
William Hicks <whicks@nvidia.com>
2025-02-13 15:59:18 -0800

    Update style

--- cpp/include/raft/spectral/detail/partition.hpp
+++ cpp/include/raft/spectral/detail/partition.hpp
@@ -18,10 +18,10 @@
 #include <raft/core/resource/cublas_handle.hpp>
 #include <raft/core/resource/cuda_stream.hpp>
 #include <raft/linalg/detail/cublas_wrappers.hpp>
+#include <raft/sparse/linalg/laplacian.cuh>
 #include <raft/spectral/cluster_solvers.cuh>
 #include <raft/spectral/detail/spectral_util.cuh>
 #include <raft/spectral/eigen_solvers.cuh>
-#include <raft/sparse/linalg/laplacian.cuh>
 #include <raft/spectral/matrix_wrappers.hpp>
 
 #include <cuda.h>
