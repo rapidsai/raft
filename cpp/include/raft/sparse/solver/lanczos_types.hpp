3d5823cb61a006296dfdd2d1e816b8b5ecf8c62b
aamijar <aamijar230@gmail.com>
2025-05-07 05:11:47 +0000

    doxygen format

--- cpp/include/raft/sparse/solver/lanczos_types.hpp
+++ cpp/include/raft/sparse/solver/lanczos_types.hpp
@@ -35,19 +35,42 @@ enum LANCZOS_WHICH {
   SM
 };
 
+/**
+ * @brief Configuration parameters for the Lanczos eigensolver
+ *
+ * This structure encapsulates all configuration parameters needed to run the
+ * Lanczos algorithm for computing eigenvalues and eigenvectors of large sparse matrices.
+ *
+ * @tparam ValueTypeT Data type for values (float or double)
+ */
 template <typename ValueTypeT>
 struct lanczos_solver_config {
-  /** The number of eigenvalues and eigenvectors to compute. Must be 1 <= k < n.*/
+  /** @brief The number of eigenvalues and eigenvectors to compute
+   *  @note Must be 1 <= n_components < n, where n is the matrix dimension
+   */
   int n_components;
-  /** Maximum number of iteration. */
+
+  /** @brief Maximum number of iterations allowed for the algorithm to converge */
   int max_iterations;
-  /** The number of Lanczos vectors generated. Must be k + 1 < ncv < n. */
+
+  /** @brief The number of Lanczos vectors to generate
+   *  @note Must satisfy n_components + 1 < ncv < n, where n is the matrix dimension
+   */
   int ncv;
-  /** Tolerance for residuals ``||Ax - wx||`` */
+
+  /** @brief Convergence tolerance for residuals
+   *  @note Used to determine when to stop iteration based on ||Ax - wx|| < tolerance
+   */
   ValueTypeT tolerance;
-  /** Enumeration specifying which eigenvalues to compute in the Lanczos algorithm. */
+
+  /** @brief Specifies which eigenvalues to compute in the Lanczos algorithm
+   *  @see LANCZOS_WHICH for possible values (SA, LA, SM, LM)
+   */
   LANCZOS_WHICH which;
-  /** random seed */
+
+  /** @brief Random seed for initialization of the algorithm
+   *  @note Controls reproducibility of results
+   */
   uint64_t seed;
 };
 
