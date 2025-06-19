5a026847a50bce452df3c543850405538262c648
Vitor Sessak <vitor.sessak@m4x.org>
2025-06-19 20:43:25 +0200

    Fix several issues that breaks LLVM (#2698)
    Fix several issues that breaks LLVM:
    - do not declare a function _RAFT_HOST_DEVICE if it has no host implementation;
    - use std::min() instead of min();
    - avoid usign non-existent methods on templated class;
    - add missing include.
    
    Authors:
      - Vitor Sessak (https://github.com/vitor1001)
    
    Approvers:
      - Micka (https://github.com/lowener)
      - Anupam (https://github.com/aamijar)
    
    URL: https://github.com/rapidsai/raft/pull/2698

--- cpp/include/raft/distance/detail/pairwise_matrix/dispatch_layout.cuh
+++ cpp/include/raft/distance/detail/pairwise_matrix/dispatch_layout.cuh
@@ -57,7 +57,7 @@ int determine_vec_len(pairwise_matrix_params<IdxT, DataT, OutT, FinOpT> params)
 {
   size_t align_x        = alignment_of_2d_array(params.x, params.ldx);
   size_t align_y        = alignment_of_2d_array(params.y, params.ldy);
-  size_t byte_alignment = min(align_x, align_y);
+  size_t byte_alignment = std::min(align_x, align_y);
 
   // Since alignment is in bytes, it could be smaller than sizeof(DataT).
   // Handle this (unlikely) case here.
