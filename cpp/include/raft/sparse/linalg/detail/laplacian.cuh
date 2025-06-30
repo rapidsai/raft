3656bdcb587ebeff8bdc81029d38f49deb1e94bd
aamijar <aamijar230@gmail.com>
2025-06-30 22:39:07 +0000

    fix-laplacian-cast

--- cpp/include/raft/sparse/linalg/detail/laplacian.cuh
+++ cpp/include/raft/sparse/linalg/detail/laplacian.cuh
@@ -149,7 +149,7 @@ auto laplacian_normalized(
     res, raft::make_const_mdspan(diagonal.view()), diagonal.view(), raft::sqrt_op());
 
   raft::sparse::matrix::scale_by_diagonal_symmetric(res, diagonal.view(), laplacian.view());
-  raft::sparse::matrix::set_diagonal(res, laplacian.view(), 1.0f);
+  raft::sparse::matrix::set_diagonal(res, laplacian.view(), static_cast<ElementType>(1.0));
 
   auto stream = resource::get_cuda_stream(res);
 
