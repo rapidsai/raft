94a28c9c7ca9c56750c3603375e99986219ad186
Jake Awe <jawe@nvidia.com>
2025-09-30 12:51:27 -0500

    rm other unused vars

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -13,8 +13,6 @@ NEXT_FULL_TAG=$1
 
 # Get current version
 CURRENT_TAG=$(git tag --merged HEAD | grep -xE '^v.*' | sort --version-sort | tail -n 1 | tr -d 'v')
-CURRENT_MAJOR=$(echo "$CURRENT_TAG" | awk '{split($0, a, "."); print a[1]}')
-CURRENT_MINOR=$(echo "$CURRENT_TAG" | awk '{split($0, a, "."); print a[2]}')
 
 # Get <major>.<minor> for next version
 NEXT_MAJOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[1]}')
