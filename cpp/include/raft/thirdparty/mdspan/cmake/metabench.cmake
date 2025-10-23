32501e64e5bf0d0b83c57ee7aeeecdee1e19f179
Kyle Edwards <kyedwards@nvidia.com>
2025-10-23 11:40:18 -0400

    metabench

--- cpp/include/raft/thirdparty/mdspan/cmake/metabench.cmake
+++ cpp/include/raft/thirdparty/mdspan/cmake/metabench.cmake
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: Copyright Louis Dionne 2017
 # SPDX-FileCopyrightText: Copyright Bruno Dutra 2016
 # SPDX-License-Identifier: BSL-1.0
+
+# Copyright Louis Dionne 2017
+# Copyright Bruno Dutra 2016
 #
 # Distributed under the Boost Software License, Version 1.0.
 # See https://github.com/ldionne/metabench/blob/master/LICENSE.md.
