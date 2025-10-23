92daba0d76ab288b48803255604aaf410c9c7997
Kyle Edwards <kyedwards@nvidia.com>
2025-10-23 11:38:06 -0400

    More

--- cpp/include/raft/solver/detail/lap_functions.cuh
+++ cpp/include/raft/solver/detail/lap_functions.cuh
@@ -1,6 +1,9 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2024, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright 2020 KETAN DATE & RAKESH NAGI
+ * SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
+ */
+/*
  * Copyright 2020 KETAN DATE & RAKESH NAGI
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
