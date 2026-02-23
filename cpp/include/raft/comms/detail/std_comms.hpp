52e0d2a170e794d529dfbe4beb04ea7f4ac6e875
Michael Schellenberger Costa <miscco@nvidia.com>
2026-02-23 13:15:41 +0100

    Drop `thrust::zip_iterator` in favor of `cuda::zip_iterator`

--- cpp/include/raft/comms/detail/std_comms.hpp
+++ cpp/include/raft/comms/detail/std_comms.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -16,7 +16,6 @@
 #include <rmm/device_uvector.hpp>
 
 #include <cuda_runtime.h>
-#include <thrust/iterator/zip_iterator.h>
 
 #include <nccl.h>
 #include <stdlib.h>
