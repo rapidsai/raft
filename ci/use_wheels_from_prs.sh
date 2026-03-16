ffec9aa7df7c06625c33d769492bd13d371382f0
James Lamb <jlamb@nvidia.com>
2026-03-15 21:21:46 -0500

    new RMM and UCXX commits

--- ci/use_wheels_from_prs.sh
+++ ci/use_wheels_from_prs.sh
@@ -8,7 +8,7 @@ source rapids-init-pip
 RAPIDS_PY_CUDA_SUFFIX=$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")
 
 # download wheels, store the directories holding them in variables
-RMM_COMMIT=0465f7cbc3acfa95dc5b83bdc6682db108e8b1b2
+RMM_COMMIT=e36df7200d5c05302c8fefbcb94a89aa634ef2b7
 LIBRMM_WHEELHOUSE=$(
   RAPIDS_PY_WHEEL_NAME="librmm_${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-artifact rmm 2270 cpp wheel "${RMM_COMMIT}"
 )
@@ -16,7 +16,7 @@ RMM_WHEELHOUSE=$(
   rapids-get-pr-artifact rmm 2270 python wheel --pkg_name rmm --stable "${RMM_COMMIT}"
 )
 
-UCXX_COMMIT=8b6e2e45b2c1962dd58fd95e1f5e43db5d953a41
+UCXX_COMMIT=d673fd753706d396a5efb5fe0e3913192ebb0c05
 DISTRIBUTED_UCXX_WHEELHOUSE=$(
   RAPIDS_PY_WHEEL_NAME="${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-get-pr-artifact --pkg_name distributed-ucxx ucxx 604 python wheel
 )
